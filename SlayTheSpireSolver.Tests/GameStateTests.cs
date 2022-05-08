908862f2b36126ff79b5693a8259d5174772c88f
cvennevik <cvennevik@gmail.com>
2022-05-08 20:54:39 +0200

    Add GameState.Enemies and value equality for GameState

--- SlayTheSpireSolver.Tests/GameStateTests.cs
+++ SlayTheSpireSolver.Tests/GameStateTests.cs
@@ -5,9 +5,34 @@ namespace SlayTheSpireSolver.Tests;
 public class GameStateTests
 {
     [Test]
-    public void Test()
+    public void TestEquality1()
     {
-        var gameState = new GameState();
-        Assert.NotNull(gameState);
+        var gameState1 = new GameState();
+        var gameState2 = new GameState();
+        Assert.AreEqual(gameState1, gameState2);
+    }
+
+    [Test]
+    public void TestEquality2()
+    {
+        var gameState1 = new GameState { Enemies = new Enemy[] { } };
+        var gameState2 = new GameState { Enemies = new Enemy[] { } };
+        Assert.AreEqual(gameState1, gameState2);
+    }
+
+    [Test]
+    public void TestEquality3()
+    {
+        var gameState1 = new GameState { Enemies = new[] { new Enemy() } };
+        var gameState2 = new GameState { Enemies = new Enemy[] { } };
+        Assert.AreNotEqual(gameState1, gameState2);
+    }
+
+    [Test]
+    public void TestEquality4()
+    {
+        var gameState1 = new GameState { Enemies = new[] { new Enemy() } };
+        var gameState2 = new GameState { Enemies = new[] { new Enemy() } };
+        Assert.AreEqual(gameState1, gameState2);
     }
 }
--- SlayTheSpireSolver/GameState.cs
+++ SlayTheSpireSolver/GameState.cs
@@ -2,4 +2,22 @@
 
 public class GameState
 {
+    public IReadOnlyList<Enemy> Enemies { get; init; } = new List<Enemy>();
+
+    public override bool Equals(object? obj)
+    {
+        var gameState = obj as GameState;
+        if (obj == null)
+        {
+            return false;
+        }
+
+        return Enemies.SequenceEqual(gameState.Enemies);
+    }
+
+    public override int GetHashCode()
+    {
+        // Uhhh let's hope I never need this lol
+        return 0;
+    }
 }
