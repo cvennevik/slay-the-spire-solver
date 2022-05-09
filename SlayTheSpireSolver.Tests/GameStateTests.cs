e9b9297210d6c34d2bd858ab22e8a5fb4a606246
cvennevik <cvennevik@gmail.com>
2022-05-09 17:47:18 +0200

    Group tests in EqualityTests subclass

--- SlayTheSpireSolver.Tests/GameStateTests.cs
+++ SlayTheSpireSolver.Tests/GameStateTests.cs
@@ -2,37 +2,41 @@ using NUnit.Framework;
 
 namespace SlayTheSpireSolver.Tests;
 
+[TestFixture]
 public class GameStateTests
 {
-    [Test]
-    public void TestEquality1()
+    public class EqualityTests : GameStateTests
     {
-        var gameState1 = new GameState();
-        var gameState2 = new GameState();
-        Assert.AreEqual(gameState1, gameState2);
-    }
+        [Test]
+        public void TestEquality1()
+        {
+            var gameState1 = new GameState();
+            var gameState2 = new GameState();
+            Assert.AreEqual(gameState1, gameState2);
+        }
 
-    [Test]
-    public void TestEquality2()
-    {
-        var gameState1 = new GameState { Enemies = new Enemy[] { } };
-        var gameState2 = new GameState { Enemies = new Enemy[] { } };
-        Assert.AreEqual(gameState1, gameState2);
-    }
+        [Test]
+        public void TestEquality2()
+        {
+            var gameState1 = new GameState { Enemies = new Enemy[] { } };
+            var gameState2 = new GameState { Enemies = new Enemy[] { } };
+            Assert.AreEqual(gameState1, gameState2);
+        }
 
-    [Test]
-    public void TestEquality3()
-    {
-        var gameState1 = new GameState { Enemies = new[] { new Enemy() } };
-        var gameState2 = new GameState { Enemies = new Enemy[] { } };
-        Assert.AreNotEqual(gameState1, gameState2);
-    }
+        [Test]
+        public void TestEquality3()
+        {
+            var gameState1 = new GameState { Enemies = new[] { new Enemy() } };
+            var gameState2 = new GameState { Enemies = new Enemy[] { } };
+            Assert.AreNotEqual(gameState1, gameState2);
+        }
 
-    [Test]
-    public void TestEquality4()
-    {
-        var gameState1 = new GameState { Enemies = new[] { new Enemy() } };
-        var gameState2 = new GameState { Enemies = new[] { new Enemy() } };
-        Assert.AreEqual(gameState1, gameState2);
+        [Test]
+        public void TestEquality4()
+        {
+            var gameState1 = new GameState { Enemies = new[] { new Enemy() } };
+            var gameState2 = new GameState { Enemies = new[] { new Enemy() } };
+            Assert.AreEqual(gameState1, gameState2);
+        }
     }
 }
