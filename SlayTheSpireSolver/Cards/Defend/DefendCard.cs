36dd379ff3a36182d99b75ed37de75ff2c722032
cvennevik <cvennevik@gmail.com>
2022-05-14 14:17:40 +0200

    Add DefendCard

---
+++ SlayTheSpireSolver/Cards/Defend/DefendCard.cs
@@ -0,0 +1,9 @@
+﻿namespace SlayTheSpireSolver.Cards.Defend;
+
+public record DefendCard : ICard
+{
+    public IEnumerable<IAction> GetLegalActions(GameState gameState)
+    {
+        throw new NotImplementedException();
+    }
+}
