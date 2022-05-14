380c4062600f3ed25bee52127c701c3db1da11ab
cvennevik <cvennevik@gmail.com>
2022-05-14 15:25:21 +0200

    Add DefendAction with Resolve() unimplemented

---
+++ SlayTheSpireSolver.Tests/Cards/Defend/DefendActionTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using SlayTheSpireSolver.Cards.Defend;
+using System;
+
+namespace SlayTheSpireSolver.Tests.Cards.Defend;
+
+[TestFixture]
+public class DefendActionTests
+{
+    [Test]
+    public void MustHaveDefendCardInHand()
+    {
+        var gameState = new GameState();
+        Assert.Throws<ArgumentException>(() => new DefendAction(gameState));
+    }
+
+    [Test]
+    public void ActionsWithSameGameStatesAreEqual()
+    {
+        var gameState = new GameState { Hand = new Hand(new DefendCard()) };
+        Assert.AreEqual(new DefendAction(gameState), new DefendAction(gameState));
+    }
+
+    [Test]
+    public void ActionsWithDifferentGameStatesAreNotEqual()
+    {
+        var gameState1 = new GameState
+        {
+            Hand = new Hand(new DefendCard()),
+            Turn = new Turn(1)
+        };
+        var gameState2 = new GameState
+        {
+            Hand = new Hand(new DefendCard()),
+            Turn = new Turn(2)
+        };
+        Assert.AreNotEqual(new DefendAction(gameState1), new DefendAction(gameState2));
+    }
+}
---
+++ SlayTheSpireSolver/Cards/Defend/DefendAction.cs
@@ -0,0 +1,17 @@
+﻿namespace SlayTheSpireSolver.Cards.Defend;
+
+public record DefendAction : IAction
+{
+    public GameState GameState { get; }
+
+    public DefendAction(GameState gameState)
+    {
+        if (!gameState.Hand.Cards.Contains(new DefendCard())) throw new ArgumentException("No Defend card in hand");
+        GameState = gameState;
+    }
+
+    public GameState Resolve()
+    {
+        throw new NotImplementedException();
+    }
+}
