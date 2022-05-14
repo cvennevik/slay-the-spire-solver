4f22b590ceb83fd072c52dd3e3e3b7e4990e2f92
cvennevik <cvennevik@gmail.com>
2022-05-14 08:12:11 +0200

    Test EndTurnAction equality

--- SlayTheSpireSolver.Tests/EndTurnActionTests.cs
+++ SlayTheSpireSolver.Tests/EndTurnActionTests.cs
@@ -16,4 +16,33 @@ public class EndTurnActionTests
         GameState newGameState = endTurnAction.Resolve();
         Assert.AreEqual(expectedTurnNumber, newGameState.TurnNumber);
     }
+
+    [Test]
+    public void TestEquality1()
+    {
+        var gameState = new GameState();
+        var action1 = new EndTurnAction(gameState);
+        var action2 = new EndTurnAction(gameState);
+        Assert.AreEqual(action1, action2);
+    }
+
+    [Test]
+    public void TestEquality2()
+    {
+        var gameState = new GameState();
+        var identicalGameState = new GameState();
+        var action1 = new EndTurnAction(gameState);
+        var action2 = new EndTurnAction(identicalGameState);
+        Assert.AreEqual(action1, action2);
+    }
+
+    [Test]
+    public void TestEquality3()
+    {
+        var gameState = new GameState { TurnNumber = 1 };
+        var differentGameState = new GameState { TurnNumber = 2 };
+        var action1 = new EndTurnAction(gameState);
+        var action2 = new EndTurnAction(differentGameState);
+        Assert.AreNotEqual(action1, action2);
+    }
 }
