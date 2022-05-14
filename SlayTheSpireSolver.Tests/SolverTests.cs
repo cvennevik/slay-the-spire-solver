12a453eaa9e89debd6b256a3263f1d9bb2d2d404
cvennevik <cvennevik@gmail.com>
2022-05-14 15:11:51 +0200

    Improve Solver

--- SlayTheSpireSolver.Tests/SolverTests.cs
+++ SlayTheSpireSolver.Tests/SolverTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using SlayTheSpireSolver.Cards.Strike;
 using SlayTheSpireSolver.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.Tests;
@@ -16,5 +17,18 @@ public class SolverTests
         };
         var bestAction = Solver.GetBestAction(gameState);
         Assert.AreEqual(new EndTurnAction(gameState), bestAction);
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var gameState = new GameState
+        {
+            Player = new Player { Health = new Health(50) },
+            Enemy = new JawWorm { Health = new Health(6) },
+            Hand = new Hand(new StrikeCard())
+        };
+        var bestAction = Solver.GetBestAction(gameState);
+        Assert.AreEqual(new StrikeAction(gameState), bestAction);
     }
 }
--- SlayTheSpireSolver/Solver.cs
+++ SlayTheSpireSolver/Solver.cs
@@ -4,6 +4,13 @@ public class Solver
 {
     public static IAction GetBestAction(GameState gameState)
     {
-        return new EndTurnAction(gameState);
+        var legalActions = gameState.GetLegalActions();
+        var endTurnAction = new EndTurnAction(gameState);
+        var actionsExceptEndTurn = legalActions.Except(new[] { endTurnAction });
+        if (actionsExceptEndTurn.Any())
+        {
+            return actionsExceptEndTurn.First();
+        }
+        return endTurnAction;
     }
 }
