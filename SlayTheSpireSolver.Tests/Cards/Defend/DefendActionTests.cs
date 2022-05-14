391a04a2e38a6c77c8e4ea6d0d93047b71a8a909
cvennevik <cvennevik@gmail.com>
2022-05-14 15:47:38 +0200

    Implement DefendAction.Resolve() + require an enemy

--- SlayTheSpireSolver.Tests/Cards/Defend/DefendActionTests.cs
+++ SlayTheSpireSolver.Tests/Cards/Defend/DefendActionTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using SlayTheSpireSolver.Cards.Defend;
+using SlayTheSpireSolver.Enemies.JawWorms;
 using System;
 
 namespace SlayTheSpireSolver.Tests.Cards.Defend;
@@ -10,14 +11,25 @@ public class DefendActionTests
     [Test]
     public void MustHaveDefendCardInHand()
     {
-        var gameState = new GameState();
+        var gameState = new GameState { Enemy = new JawWorm() };
+        Assert.Throws<ArgumentException>(() => new DefendAction(gameState));
+    }
+
+    [Test]
+    public void MustHaveEnemy()
+    {
+        var gameState = new GameState { Hand = new Hand(new DefendCard()) };
         Assert.Throws<ArgumentException>(() => new DefendAction(gameState));
     }
 
     [Test]
     public void ActionsWithSameGameStatesAreEqual()
     {
-        var gameState = new GameState { Hand = new Hand(new DefendCard()) };
+        var gameState = new GameState
+        {
+            Enemy = new JawWorm(),
+            Hand = new Hand(new DefendCard())
+        };
         Assert.AreEqual(new DefendAction(gameState), new DefendAction(gameState));
     }
 
@@ -27,13 +39,37 @@ public class DefendActionTests
         var gameState1 = new GameState
         {
             Hand = new Hand(new DefendCard()),
+            Enemy = new JawWorm(),
             Turn = new Turn(1)
         };
         var gameState2 = new GameState
         {
             Hand = new Hand(new DefendCard()),
+            Enemy = new JawWorm(),
             Turn = new Turn(2)
         };
         Assert.AreNotEqual(new DefendAction(gameState1), new DefendAction(gameState2));
+    }
+
+    [Test]
+    [TestCase(0, 5)]
+    [TestCase(2, 7)]
+    public void AddsArmorToPlayerAndRemovesDefendCard(int initialArmorValue, int expectedArmorValue)
+    {
+        var gameState = new GameState()
+        {
+            Player = new Player { Armor = new Armor(initialArmorValue) },
+            Enemy = new JawWorm(),
+            Hand = new Hand(new DefendCard())
+        };
+        var defendAction = new DefendAction(gameState);
+        var resolvedGameState = defendAction.Resolve();
+        var expectedGameState = new GameState()
+        {
+            Player = new Player { Armor = new Armor(expectedArmorValue) },
+            Enemy = new JawWorm(),
+            Hand = new Hand()
+        };
+        Assert.AreEqual(expectedGameState, resolvedGameState);
     }
 }
--- SlayTheSpireSolver/Cards/Defend/DefendAction.cs
+++ SlayTheSpireSolver/Cards/Defend/DefendAction.cs
@@ -6,12 +6,17 @@ public record DefendAction : IAction
 
     public DefendAction(GameState gameState)
     {
+        if (gameState.Enemy == null) throw new ArgumentException("Cannot play Defend when no enemies remain");
         if (!gameState.Hand.Cards.Contains(new DefendCard())) throw new ArgumentException("No Defend card in hand");
         GameState = gameState;
     }
 
     public GameState Resolve()
     {
-        throw new NotImplementedException();
+        var oldPlayerArmor = GameState.Player.Armor;
+        var newPlayerArmor = new Armor(oldPlayerArmor.Value + 5);
+        var newPlayer = GameState.Player with { Armor = newPlayerArmor };
+        var newHand = GameState.Hand.Remove(new DefendCard());
+        return GameState with { Player = newPlayer, Hand = newHand };
     }
 }
