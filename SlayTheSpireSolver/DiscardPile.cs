f9557ade463163406cc0298e024f4e99d8e886b7
cvennevik <cvennevik@gmail.com>
2022-05-15 18:27:23 +0200

    Add DiscardPile

---
+++ SlayTheSpireSolver/DiscardPile.cs
@@ -0,0 +1,32 @@
+﻿using SlayTheSpireSolver.Cards;
+
+namespace SlayTheSpireSolver;
+
+public class DiscardPile
+{
+    public IReadOnlyCollection<ICard> Cards { get; }
+
+    public DiscardPile(params ICard[] cards)
+    {
+        Cards = cards;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not DiscardPile otherDiscardPile) return false;
+        if (otherDiscardPile.Cards.Count != Cards.Count) return false;
+        var orderedCards = Cards.OrderBy(x => x.ToString());
+        var orderedOtherCards = otherDiscardPile.Cards.OrderBy(x => x.ToString());
+        return orderedCards.SequenceEqual(orderedOtherCards);
+    }
+
+    public override int GetHashCode()
+    {
+        return 0;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(DiscardPile)}: [{string.Join(",", Cards)}]";
+    }
+}
