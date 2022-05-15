c1a5fdac8ca2118cc90d5837c9e13cbfd44ce547
cvennevik <cvennevik@gmail.com>
2022-05-15 16:05:38 +0200

    Add Enemy.Damage method

---
+++ SlayTheSpireSolver.Tests/Enemies/JawWorms/JawWormTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using SlayTheSpireSolver.Enemies.JawWorms;
+using System;
+
+namespace SlayTheSpireSolver.Tests.Enemies.JawWorms;
+
+[TestFixture]
+public class JawWormTests
+{
+    [Test]
+    [TestCase(10, 6, 4)]
+    [TestCase(8, 1, 7)]
+    [TestCase(7, 10, -3)]
+    [TestCase(1, 1, 0)]
+    [TestCase(10, 0, 10)]
+    public void DamageReducesHealth(int initialHealthValue, int damageValue, int expectedHealthValue)
+    {
+        var jawWorm = new JawWorm { Health = new Health(initialHealthValue) };
+        var damagedJawWorm = jawWorm.Damage(damageValue);
+        Assert.AreEqual(new JawWorm { Health = new Health(expectedHealthValue) }, damagedJawWorm);
+    }
+
+    [Test]
+    [TestCase(-1)]
+    [TestCase(-2)]
+    [TestCase(-999)]
+    public void DamageCannotBeNegative(int damageValue)
+    {
+        var jawWorm = new JawWorm { Health = new Health(10) };
+        Assert.Throws<ArgumentOutOfRangeException>(() => jawWorm.Damage(damageValue));
+    }
+}
--- SlayTheSpireSolver/Enemies/Enemy.cs
+++ SlayTheSpireSolver/Enemies/Enemy.cs
@@ -4,4 +4,10 @@ public abstract record Enemy
 {
     public Health Health { get; init; } = new Health(1);
     public abstract IEnemyMove GetIntendedMove();
+
+    public Enemy Damage(int damageValue)
+    {
+        if (damageValue < 0) throw new ArgumentOutOfRangeException(nameof(damageValue));
+        return this with { Health = new Health(Health.Value - damageValue) };
+    }
 }
