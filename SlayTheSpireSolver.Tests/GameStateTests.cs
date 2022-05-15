410d750c071a84667e71fe8a5b699a65674955dd
cvennevik <cvennevik@gmail.com>
2022-05-15 14:19:47 +0200

    Refactor GameStateTests duplication

--- SlayTheSpireSolver.Tests/GameStateTests.cs
+++ SlayTheSpireSolver.Tests/GameStateTests.cs
@@ -9,133 +9,76 @@ namespace SlayTheSpireSolver.Tests;
 [TestFixture]
 public class GameStateTests
 {
+    private static GameState CreateBasicGameState()
+    {
+        return new()
+        {
+            Player = new Player { Health = new Health(70) },
+            EnemyParty = new EnemyParty(new JawWorm { Health = new Health(40), IntendedMove = new Chomp() }),
+            Hand = new Hand(new StrikeCard()),
+            Turn = new Turn(1)
+        };
+    }
+
     [Test]
     public void TestEquality()
     {
-        var gameState1 = new GameState();
-        var gameState2 = new GameState();
+        var gameState1 = CreateBasicGameState();
+        var gameState2 = CreateBasicGameState();
         Assert.AreEqual(gameState1, gameState2);
     }
 
     [Test]
-    public void TestGetLegalActionsWithEmptyHand()
+    public void TestBasicGameState()
     {
-        var gameState = new GameState
-        {
-            EnemyParty = new EnemyParty(new JawWorm { IntendedMove = new Chomp() }),
-            Player = new Player { Health = new Health(50) }
-        };
-        var legalActions = gameState.GetLegalActions();
-        Assert.AreEqual(1, legalActions.Count);
-        Assert.AreEqual(new EndTurnAction(gameState), legalActions.First());
-    }
-
-    [Test]
-    public void TestGetLegalActionsWithStrikeInHand()
-    {
-        var gameState = new GameState
-        {
-            EnemyParty = new EnemyParty(new JawWorm { IntendedMove = new Chomp() }),
-            Player = new Player { Health = new Health(50) },
-            Hand = new Hand(new StrikeCard())
-        };
+        var gameState = CreateBasicGameState();
         var legalActions = gameState.GetLegalActions().ToList();
         Assert.AreEqual(2, legalActions.Count);
         Assert.Contains(new EndTurnAction(gameState), legalActions);
         Assert.Contains(new StrikeAction(gameState), legalActions);
+        Assert.False(gameState.IsVictory());
+        Assert.False(gameState.IsDefeat());
     }
 
     [Test]
-    public void TestGetLegalActionsWhenDefeated()
+    public void TestEmptyHand()
     {
-        var gameState = new GameState
-        {
-            EnemyParty = new EnemyParty(new JawWorm { IntendedMove = new Chomp() }),
-            Player = new Player { Health = new Health(0) }
-        };
-        var legalActions = gameState.GetLegalActions().ToList();
-        Assert.IsEmpty(legalActions);
-    }
-
-    [Test]
-    public void TestLegalActionsWithNoEnemiesAndEmptyHand()
-    {
-        var gameState = new GameState
-        {
-            Player = new Player { Health = new Health(50) }
-        };
-        var legalActions = gameState.GetLegalActions().ToList();
-        Assert.IsEmpty(legalActions);
-    }
-
-    [Test]
-    public void TestLegalActionsWithNoEnemiesAndStrikeInHand()
-    {
-        var gameState = new GameState
-        {
-            Player = new Player { Health = new Health(50) },
-            Hand = new Hand(new StrikeCard())
-        };
-        var legalActions = gameState.GetLegalActions().ToList();
-        Assert.IsEmpty(legalActions);
+        var gameState =  CreateBasicGameState() with { Hand = new Hand() };
+        var legalActions = gameState.GetLegalActions();
+        Assert.AreEqual(1, legalActions.Count);
+        Assert.AreEqual(new EndTurnAction(gameState), legalActions.First());
+        Assert.False(gameState.IsVictory());
+        Assert.False(gameState.IsDefeat());
     }
 
     [Test]
     [TestCase(0)]
     [TestCase(-1)]
     [TestCase(-999)]
-    public void IsDefeatWhenHealthBelowOne(int healthValue)
+    public void TestHealthBelowOne(int healthValue)
     {
-        var gameState = new GameState
-        {
-            Player = new Player { Health = new Health(healthValue) }
-        };
+        var gameState = CreateBasicGameState() with { Player = new Player { Health = new Health(healthValue) } };
+        Assert.IsEmpty(gameState.GetLegalActions());
         Assert.True(gameState.IsDefeat());
     }
 
     [Test]
-    [TestCase(1)]
-    [TestCase(2)]
-    [TestCase(999)]
-    public void IsNotDefeatWhenHealthAboveZero(int healthValue)
+    public void TestNoEnemiesLeft()
     {
-        var gameState = new GameState
-        {
-            Player = new Player { Health = new Health(healthValue) }
-        };
-        Assert.False(gameState.IsDefeat());
-    }
-
-    [Test]
-    public void IsVictoryWhenNoEnemy()
-    {
-        var gameState = new GameState
-        {
-            Player = new Player { Health = new Health(10) },
-            EnemyParty = new EnemyParty()
-        };
+        var gameState = CreateBasicGameState() with { EnemyParty = new EnemyParty() };
         Assert.True(gameState.IsVictory());
+        Assert.IsEmpty(gameState.GetLegalActions());
     }
 
     [Test]
-    public void IsNotVictoryWhenOneEnemy()
+    public void TestHealthBelowOneWithNoEnemies()
     {
-        var gameState = new GameState
-        {
-            Player = new Player { Health = new Health(10) },
-            EnemyParty = new EnemyParty(new JawWorm())
-        };
-        Assert.False(gameState.IsVictory());
-    }
-
-    [Test]
-    public void IsNotVictoryWhenNoEnemyAndHealthBelowOne()
-    {
-        var gameState = new GameState
+        var gameState = CreateBasicGameState() with
         {
             Player = new Player { Health = new Health(0) },
             EnemyParty = new EnemyParty()
         };
         Assert.False(gameState.IsVictory());
+        Assert.True(gameState.IsDefeat());
     }
 }
