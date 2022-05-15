69fdcc9445399a4e432b22cf2334639225d77bff
cvennevik <cvennevik@gmail.com>
2022-05-15 09:02:33 +0200

    Set default Health and Armor values

--- SlayTheSpireSolver/Enemies/Enemy.cs
+++ SlayTheSpireSolver/Enemies/Enemy.cs
@@ -2,6 +2,6 @@
 
 public abstract record Enemy
 {
-    public Health Health { get; init; }
+    public Health Health { get; init; } = new Health(1);
     public abstract IEnemyMove GetIntendedMove();
 }
--- SlayTheSpireSolver/Player.cs
+++ SlayTheSpireSolver/Player.cs
@@ -2,6 +2,6 @@
 
 public record Player
 {
-    public Health Health { get; init; }
-    public Armor Armor { get; init; }
+    public Health Health { get; init; } = new Health(1);
+    public Armor Armor { get; init; } = new Armor(0);
 }
