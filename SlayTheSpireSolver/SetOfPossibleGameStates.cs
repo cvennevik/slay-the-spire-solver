b08ee9d49866b39dfc68c494f5455f3427884168
cvennevik <cvennevik@gmail.com>
2022-05-17 20:02:49 +0200

    feat: add SetOfPossibleGameStates class

---
+++ SlayTheSpireSolver/SetOfPossibleGameStates.cs
@@ -0,0 +1,11 @@
+﻿namespace SlayTheSpireSolver;
+
+public record SetOfPossibleGameStates
+{
+    public IReadOnlyCollection<PossibleGameState> PossibleGameStates { get; }
+
+    public SetOfPossibleGameStates(params PossibleGameState[] possibleGameStates)
+    {
+        PossibleGameStates = possibleGameStates;
+    }
+}
