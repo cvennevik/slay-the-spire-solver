8df4ec03d9ad7bc9240150730479376235702b5e
cvennevik <cvennevik@gmail.com>
2022-05-21 16:53:00 +0200

    refactor(HandTests): divide tests into subclasses and rename tests

--- SlayTheSpireSolver.Tests/RulesEngine/HandTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/HandTests.cs
@@ -9,151 +9,163 @@ namespace SlayTheSpireSolver.Tests.RulesEngine;
 [TestFixture]
 public class HandTests
 {
-    [Test]
-    public void TestEquality1()
+    [TestFixture]
+    public class EqualityTests : HandTests
     {
-        Assert.AreEqual(new Hand(), new Hand());
+        [Test]
+            public void Test1()
+            {
+                Assert.AreEqual(new Hand(), new Hand());
+            }
+        
+            [Test]
+            public void Test2()
+            {
+                var hand1 = new Hand();
+                var hand2 = new Hand(new StrikeCard());
+                Assert.AreNotEqual(hand1, hand2);
+            }
+        
+            [Test]
+            public void Test3()
+            {
+                var hand1 = new Hand(new StrikeCard());
+                var hand2 = new Hand(new StrikeCard());
+                Assert.AreEqual(hand1, hand2);
+            }
+        
+            [Test]
+            public void Test4()
+            {
+                var hand1 = new Hand(new StrikeCard());
+                var hand2 = new Hand(new StrikeCard(), new StrikeCard());
+                Assert.AreNotEqual(hand1, hand2);
+            }
+        
+            [Test]
+            public void Test5()
+            {
+                var hand1 = new Hand(new StrikeCard(), new StrikeCard());
+                var hand2 = new Hand(new StrikeCard(), new StrikeCard());
+                Assert.AreEqual(hand1, hand2);
+            }
+        
+            [Test]
+            public void Test6()
+            {
+                var hand1 = new Hand(new StrikeCard(), new StrikeCard());
+                var hand2 = new Hand(new StrikeCard(), new DefendCard());
+                Assert.AreNotEqual(hand1, hand2);
+            }
+        
+            [Test]
+            public void Test7()
+            {
+                var hand1 = new Hand(new StrikeCard(), new StrikeCard());
+                var hand2 = new Hand(new StrikeCard(), new StrikeCard(), new DefendCard());
+                Assert.AreNotEqual(hand1, hand2);
+            }
+        
+            [Test]
+            public void Test8()
+            {
+                var hand1 = new Hand(new StrikeCard(), new DefendCard());
+                var hand2 = new Hand(new StrikeCard(), new DefendCard());
+                Assert.AreEqual(hand1, hand2);
+            }
+        
+            [Test]
+            public void EqualityIgnoresOrder()
+            {
+                var hand1 = new Hand(new DefendCard(), new StrikeCard());
+                var hand2 = new Hand(new StrikeCard(), new DefendCard());
+                Assert.AreEqual(hand1, hand2);
+            }
     }
 
-    [Test]
-    public void TestEquality2()
+    [TestFixture]
+    public class RemoveTests : HandTests
     {
-        var hand1 = new Hand();
-        var hand2 = new Hand(new StrikeCard());
-        Assert.AreNotEqual(hand1, hand2);
+        [Test]
+        public void ThrowsExceptionWhenHandEmpty()
+        {
+            var hand = new Hand();
+            Assert.Throws<ArgumentException>(() => hand.Remove(new StrikeCard()));
+        }
+
+        [Test]
+        public void RemoveStrikeCard()
+        {
+            var hand = new Hand(new StrikeCard());
+            var newHand = hand.Remove(new StrikeCard());
+            Assert.AreEqual(new Hand(), newHand);
+        }
+
+        [Test]
+        public void RemovesDefendCard()
+        {
+            var hand = new Hand(new DefendCard());
+            var newHand = hand.Remove(new DefendCard());
+            Assert.AreEqual(new Hand(), newHand);
+        }
+
+        [Test]
+        public void RemovesOnlyOneCopy()
+        {
+            var hand = new Hand(new StrikeCard(), new StrikeCard());
+            var newHand = hand.Remove(new StrikeCard());
+            Assert.AreEqual(new Hand(new StrikeCard()), newHand);
+        }
+
+        [Test]
+        public void RemovesCorrectCardType()
+        {
+            var hand = new Hand(new StrikeCard(), new DefendCard());
+            var newHand = hand.Remove(new StrikeCard());
+            Assert.AreEqual(new Hand(new DefendCard()), newHand);
+        }
+
+        [Test]
+        public void ThrowsExceptionWhenCardTypeNotInHand()
+        {
+            var hand = new Hand(new DefendCard());
+            Assert.Throws<ArgumentException>(() => hand.Remove(new StrikeCard()));
+        }
     }
 
-    [Test]
-    public void TestEquality3()
+    [TestFixture]
+    public class ContainsTests : HandTests
     {
-        var hand1 = new Hand(new StrikeCard());
-        var hand2 = new Hand(new StrikeCard());
-        Assert.AreEqual(hand1, hand2);
-    }
+        [Test]
+        public void TestSingleDefend()
+        {
+            var hand = new Hand(new DefendCard());
+            Assert.True(hand.Contains(new DefendCard()));
+            Assert.False(hand.Contains(new StrikeCard()));
+        }
 
-    [Test]
-    public void TestEquality4()
-    {
-        var hand1 = new Hand(new StrikeCard());
-        var hand2 = new Hand(new StrikeCard(), new StrikeCard());
-        Assert.AreNotEqual(hand1, hand2);
-    }
+        [Test]
+        public void TestStrikeAndDefend()
+        {
+            var hand = new Hand(new DefendCard(), new StrikeCard());
+            Assert.True(hand.Contains(new DefendCard()));
+            Assert.True(hand.Contains(new StrikeCard()));
+        }
 
-    [Test]
-    public void TestEquality5()
-    {
-        var hand1 = new Hand(new StrikeCard(), new StrikeCard());
-        var hand2 = new Hand(new StrikeCard(), new StrikeCard());
-        Assert.AreEqual(hand1, hand2);
-    }
+        [Test]
+        public void TestEmptyHand()
+        {
+            var hand = new Hand();
+            Assert.False(hand.Contains(new DefendCard()));
+            Assert.False(hand.Contains(new StrikeCard()));
+        }
 
-    [Test]
-    public void TestEquality6()
-    {
-        var hand1 = new Hand(new StrikeCard(), new StrikeCard());
-        var hand2 = new Hand(new StrikeCard(), new DefendCard());
-        Assert.AreNotEqual(hand1, hand2);
-    }
-
-    [Test]
-    public void TestEquality7()
-    {
-        var hand1 = new Hand(new StrikeCard(), new StrikeCard());
-        var hand2 = new Hand(new StrikeCard(), new StrikeCard(), new DefendCard());
-        Assert.AreNotEqual(hand1, hand2);
-    }
-
-    [Test]
-    public void TestEquality8()
-    {
-        var hand1 = new Hand(new StrikeCard(), new DefendCard());
-        var hand2 = new Hand(new StrikeCard(), new DefendCard());
-        Assert.AreEqual(hand1, hand2);
-    }
-
-    [Test]
-    public void TestEquality9()
-    {
-        var hand1 = new Hand(new DefendCard(), new StrikeCard());
-        var hand2 = new Hand(new StrikeCard(), new DefendCard());
-        Assert.AreEqual(hand1, hand2);
-    }
-
-    [Test]
-    public void TestRemove1()
-    {
-        var hand = new Hand();
-        Assert.Throws<ArgumentException>(() => hand.Remove(new StrikeCard()));
-    }
-
-    [Test]
-    public void TestRemove2()
-    {
-        var hand = new Hand(new StrikeCard());
-        var newHand = hand.Remove(new StrikeCard());
-        Assert.AreEqual(new Hand(), newHand);
-    }
-
-    [Test]
-    public void TestRemove3()
-    {
-        var hand = new Hand(new StrikeCard(), new StrikeCard());
-        var newHand = hand.Remove(new StrikeCard());
-        Assert.AreEqual(new Hand(new StrikeCard()), newHand);
-    }
-
-    [Test]
-    public void TestRemove4()
-    {
-        var hand = new Hand(new StrikeCard(), new DefendCard());
-        var newHand = hand.Remove(new StrikeCard());
-        Assert.AreEqual(new Hand(new DefendCard()), newHand);
-    }
-
-    [Test]
-    public void TestRemove5()
-    {
-        var hand = new Hand(new DefendCard());
-        Assert.Throws<ArgumentException>(() => hand.Remove(new StrikeCard()));
-    }
-
-    [Test]
-    public void TestRemove6()
-    {
-        var hand = new Hand(new DefendCard());
-        var newHand = hand.Remove(new DefendCard());
-        Assert.AreEqual(new Hand(), newHand);
-    }
-
-    [Test]
-    public void TestContains1()
-    {
-        var hand = new Hand(new DefendCard());
-        Assert.True(hand.Contains(new DefendCard()));
-        Assert.False(hand.Contains(new StrikeCard()));
-    }
-
-    [Test]
-    public void TestContains2()
-    {
-        var hand = new Hand(new DefendCard(), new StrikeCard());
-        Assert.True(hand.Contains(new DefendCard()));
-        Assert.True(hand.Contains(new StrikeCard()));
-    }
-
-    [Test]
-    public void TestContains3()
-    {
-        var hand = new Hand();
-        Assert.False(hand.Contains(new DefendCard()));
-        Assert.False(hand.Contains(new StrikeCard()));
-    }
-
-    [Test]
-    public void TestContains4()
-    {
-        var hand = new Hand(new DefendCard(), new DefendCard());
-        Assert.True(hand.Contains(new DefendCard()));
-        Assert.False(hand.Contains(new StrikeCard()));
+        [Test]
+        public void TestTwoDefends()
+        {
+            var hand = new Hand(new DefendCard(), new DefendCard());
+            Assert.True(hand.Contains(new DefendCard()));
+            Assert.False(hand.Contains(new StrikeCard()));
+        }
     }
 }
