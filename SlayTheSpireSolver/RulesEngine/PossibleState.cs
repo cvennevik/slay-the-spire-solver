5da1110735062399791c8a25ef23abeb5c6063fa
cvennevik <cvennevik@gmail.com>
2022-05-21 13:34:02 +0200

    refactor: convert PossibleState record to primary constructor

--- SlayTheSpireSolver/RulesEngine/PossibleState.cs
+++ SlayTheSpireSolver/RulesEngine/PossibleState.cs
@@ -2,14 +2,4 @@
 
 namespace SlayTheSpireSolver.RulesEngine;
 
-public record PossibleState
-{
-    public GameState GameState { get; }
-    public Probability Probability { get; }
-
-    public PossibleState(GameState gameState, Probability probability)
-    {
-        GameState = gameState;
-        Probability = probability;
-    }
-}
+public record PossibleState(GameState GameState, Probability Probability);
