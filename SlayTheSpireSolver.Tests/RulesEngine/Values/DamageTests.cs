7b6f993c777681a2b36205a3ec773216e521619e
cvennevik <cvennevik@gmail.com>
2022-05-21 19:03:28 +0200

    feat(Damage): add Armor comparison and subtraction to Damage

--- SlayTheSpireSolver.Tests/RulesEngine/Values/DamageTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Values/DamageTests.cs
@@ -7,21 +7,105 @@ namespace SlayTheSpireSolver.Tests.RulesEngine.Values;
 [TestFixture]
 public class DamageTests
 {
-    [Test]
-    [TestCase(-1)]
-    [TestCase(-2)]
-    [TestCase(-999)]
-    public void AmmountCannotBeNegative(int amount)
+    [TestFixture]
+    public class BasicTests : DamageTests
     {
-        Assert.Throws<ArgumentOutOfRangeException>(() => new Damage(amount));
+        [Test]
+        [TestCase(-1)]
+        [TestCase(-2)]
+        [TestCase(-999)]
+        public void AmmountCannotBeNegative(int amount)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Damage(amount));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void DamageWithEqualAmountsAreEqual(int amount)
+        {
+            Assert.AreEqual(new Damage(amount), new Damage(amount));
+        }
     }
 
-    [Test]
-    [TestCase(0)]
-    [TestCase(1)]
-    [TestCase(10)]
-    public void DamageWithEqualAmountsAreEqual(int amount)
+    [TestFixture]
+    public class ArmorOperatorTests : DamageTests
     {
-        Assert.AreEqual(new Damage(amount), new Damage(amount));
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, 0)]
+        [TestCase(0, 2, 0)]
+        [TestCase(1, 0, 1)]
+        [TestCase(1, 1, 0)]
+        [TestCase(1, 2, 0)]
+        [TestCase(2, 0, 2)]
+        [TestCase(2, 1, 1)]
+        [TestCase(2, 2, 0)]
+        public void TestSubtraction(int amountOfDamage, int amountOfArmor, int remainingAmountOfDamage)
+        {
+            Assert.AreEqual(new Damage(remainingAmountOfDamage),
+                new Damage(amountOfDamage) - new Armor(amountOfArmor));
+        }
+
+        [Test]
+        [TestCase(0, 0, false)]
+        [TestCase(0, 1, false)]
+        [TestCase(0, 2, false)]
+        [TestCase(1, 0, true)]
+        [TestCase(1, 1, false)]
+        [TestCase(1, 2, false)]
+        [TestCase(2, 0, true)]
+        [TestCase(2, 1, true)]
+        [TestCase(2, 2, false)]
+        public void TestGreaterThan(int amountOfDamage, int amountOfArmor, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new Damage(amountOfDamage) > new Armor(amountOfArmor));
+        }
+    
+        [Test]
+        [TestCase(0, 0, true)]
+        [TestCase(0, 1, false)]
+        [TestCase(0, 2, false)]
+        [TestCase(1, 0, true)]
+        [TestCase(1, 1, true)]
+        [TestCase(1, 2, false)]
+        [TestCase(2, 0, true)]
+        [TestCase(2, 1, true)]
+        [TestCase(2, 2, true)]
+        public void TestGreaterThanOrEqualTo(int amountOfDamage, int amountOfArmor, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new Damage(amountOfDamage) >= new Armor(amountOfArmor));
+        }
+    
+        [Test]
+        [TestCase(0, 0, false)]
+        [TestCase(0, 1, true)]
+        [TestCase(0, 2, true)]
+        [TestCase(1, 0, false)]
+        [TestCase(1, 1, false)]
+        [TestCase(1, 2, true)]
+        [TestCase(2, 0, false)]
+        [TestCase(2, 1, false)]
+        [TestCase(2, 2, false)]
+        public void TestLessThan(int amountOfDamage, int amountOfArmor, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new Damage(amountOfDamage) < new Armor(amountOfArmor));
+        }
+    
+        [Test]
+        [TestCase(0, 0, true)]
+        [TestCase(0, 1, true)]
+        [TestCase(0, 2, true)]
+        [TestCase(1, 0, false)]
+        [TestCase(1, 1, true)]
+        [TestCase(1, 2, true)]
+        [TestCase(2, 0, false)]
+        [TestCase(2, 1, false)]
+        [TestCase(2, 2, true)]
+        public void TestLessThanOrEqualTo(int amountOfDamage, int amountOfArmor, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, new Damage(amountOfDamage) <= new Armor(amountOfArmor));
+        }
     }
 }
--- SlayTheSpireSolver/RulesEngine/Values/Damage.cs
+++ SlayTheSpireSolver/RulesEngine/Values/Damage.cs
@@ -9,4 +9,12 @@ public record Damage
         if (amount < 0) throw new ArgumentOutOfRangeException(nameof(amount));
         Amount = amount;
     }
+
+    public static bool operator >(Damage damage, Armor armor) => damage.Amount > armor.Amount;
+    public static bool operator >=(Damage damage, Armor armor) => damage.Amount >= armor.Amount;
+    public static bool operator <(Damage damage, Armor armor) => damage.Amount < armor.Amount;
+    public static bool operator <=(Damage damage, Armor armor) => damage.Amount <= armor.Amount;
+
+    public static Damage operator -(Damage damage, Armor armor) =>
+        new(damage < armor ? 0 : damage.Amount - armor.Amount);
 }
