bed5ade0865f72fb5605b6c4cba2fca0c8c96b3d
cvennevik <cvennevik@gmail.com>
2022-05-21 19:24:03 +0200

    refactor(Health): convert to primary constructor

--- SlayTheSpireSolver/RulesEngine/Values/Health.cs
+++ SlayTheSpireSolver/RulesEngine/Values/Health.cs
@@ -1,13 +1,6 @@
 ﻿namespace SlayTheSpireSolver.RulesEngine.Values;
 
-public record Health
+public record Health(int Amount)
 {
-    public int Amount { get; }
-
-    public Health(int amount)
-    {
-        Amount = amount;
-    }
-
     public static Health operator -(Health health, Damage damage) => new(health.Amount - damage.Amount);
 }
