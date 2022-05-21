92e46a3dc6b974b0fb42aa7d52819b536a83d061
cvennevik <cvennevik@gmail.com>
2022-05-21 17:01:18 +0200

    style(DrawPileTests): fix variable case

--- SlayTheSpireSolver.Tests/RulesEngine/DrawPileTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/DrawPileTests.cs
@@ -11,8 +11,8 @@ public class DrawPileTests
     [Test]
     public void Test()
     {
-        var DrawPile = new DrawPile();
-        Assert.IsEmpty(DrawPile.Cards);
+        var drawPile = new DrawPile();
+        Assert.IsEmpty(drawPile.Cards);
     }
 
     [Test]
@@ -24,64 +24,64 @@ public class DrawPileTests
     [Test]
     public void TestEquality2()
     {
-        var DrawPile1 = new DrawPile();
-        var DrawPile2 = new DrawPile(new StrikeCard());
-        Assert.AreNotEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile();
+        var drawPile2 = new DrawPile(new StrikeCard());
+        Assert.AreNotEqual(drawPile1, drawPile2);
     }
 
     [Test]
     public void TestEquality3()
     {
-        var DrawPile1 = new DrawPile(new StrikeCard());
-        var DrawPile2 = new DrawPile(new StrikeCard());
-        Assert.AreEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile(new StrikeCard());
+        var drawPile2 = new DrawPile(new StrikeCard());
+        Assert.AreEqual(drawPile1, drawPile2);
     }
 
     [Test]
     public void TestEquality4()
     {
-        var DrawPile1 = new DrawPile(new StrikeCard());
-        var DrawPile2 = new DrawPile(new StrikeCard(), new StrikeCard());
-        Assert.AreNotEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile(new StrikeCard());
+        var drawPile2 = new DrawPile(new StrikeCard(), new StrikeCard());
+        Assert.AreNotEqual(drawPile1, drawPile2);
     }
 
     [Test]
     public void TestEquality5()
     {
-        var DrawPile1 = new DrawPile(new StrikeCard(), new StrikeCard());
-        var DrawPile2 = new DrawPile(new StrikeCard(), new StrikeCard());
-        Assert.AreEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile(new StrikeCard(), new StrikeCard());
+        var drawPile2 = new DrawPile(new StrikeCard(), new StrikeCard());
+        Assert.AreEqual(drawPile1, drawPile2);
     }
 
     [Test]
     public void TestEquality6()
     {
-        var DrawPile1 = new DrawPile(new StrikeCard(), new StrikeCard());
-        var DrawPile2 = new DrawPile(new StrikeCard(), new DefendCard());
-        Assert.AreNotEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile(new StrikeCard(), new StrikeCard());
+        var drawPile2 = new DrawPile(new StrikeCard(), new DefendCard());
+        Assert.AreNotEqual(drawPile1, drawPile2);
     }
 
     [Test]
     public void TestEquality7()
     {
-        var DrawPile1 = new DrawPile(new StrikeCard(), new StrikeCard());
-        var DrawPile2 = new DrawPile(new StrikeCard(), new StrikeCard(), new DefendCard());
-        Assert.AreNotEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile(new StrikeCard(), new StrikeCard());
+        var drawPile2 = new DrawPile(new StrikeCard(), new StrikeCard(), new DefendCard());
+        Assert.AreNotEqual(drawPile1, drawPile2);
     }
 
     [Test]
     public void TestEquality8()
     {
-        var DrawPile1 = new DrawPile(new StrikeCard(), new DefendCard());
-        var DrawPile2 = new DrawPile(new StrikeCard(), new DefendCard());
-        Assert.AreEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile(new StrikeCard(), new DefendCard());
+        var drawPile2 = new DrawPile(new StrikeCard(), new DefendCard());
+        Assert.AreEqual(drawPile1, drawPile2);
     }
 
     [Test]
     public void EqualityIgnoresOrder()
     {
-        var DrawPile1 = new DrawPile(new DefendCard(), new StrikeCard());
-        var DrawPile2 = new DrawPile(new StrikeCard(), new DefendCard());
-        Assert.AreEqual(DrawPile1, DrawPile2);
+        var drawPile1 = new DrawPile(new DefendCard(), new StrikeCard());
+        var drawPile2 = new DrawPile(new StrikeCard(), new DefendCard());
+        Assert.AreEqual(drawPile1, drawPile2);
     }
 }
