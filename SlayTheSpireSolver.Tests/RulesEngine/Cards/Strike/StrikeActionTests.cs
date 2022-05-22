239ed8a3bd246d0c0fc1a493086629118aa7c946
cvennevik <cvennevik@gmail.com>
2022-05-22 09:27:35 +0200

    refactor(StrikeActionTests): divide into subclasses

--- SlayTheSpireSolver.Tests/RulesEngine/Cards/Strike/StrikeActionTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Cards/Strike/StrikeActionTests.cs
@@ -14,7 +14,7 @@ public class StrikeActionTests
 {
     private static GameState CreateBasicGameState()
     {
-        return new()
+        return new GameState
         {
             PlayerHealth = new Health(70),
             Energy = new Energy(3),
@@ -24,79 +24,87 @@ public class StrikeActionTests
         };
     }
 
-    [Test]
-    public void EnemiesMustExist()
+    [TestFixture]
+    public class LegalityTests : StrikeActionTests
     {
-        var gameState = CreateBasicGameState() with { EnemyParty = new EnemyParty() };
-        Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
-    }
-
-    [Test]
-    public void HandMustContainStrike()
-    {
-        var gameState = CreateBasicGameState() with { Hand = new Hand() };
-        Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
-    }
-
-    [Test]
-    public void PlayerMustBeAlive()
-    {
-        var gameState = CreateBasicGameState() with { PlayerHealth = new Health(0) };
-        Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
-    }
-
-    [Test]
-    public void EnergyMustBeAtLeastOne()
-    {
-        var gameState = CreateBasicGameState() with { Energy = new Energy(0) };
-        Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
-    }
-
-    [Test]
-    public void TestStrike()
-    {
-        var initialGameState = CreateBasicGameState() with
+        [Test]
+        public void EnemiesMustExist()
         {
-            Hand = new Hand(new StrikeCard()),
-            DiscardPile = new DiscardPile(),
-            Energy = new Energy(3),
-            EnemyParty = new EnemyParty(new JawWorm { Health = new Health(10) }),
-        };
+            var gameState = CreateBasicGameState() with { EnemyParty = new EnemyParty() };
+            Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
+        }
 
-        var strikeAction = new StrikeAction(initialGameState);
-        var resolvedStates = strikeAction.ResolveToPossibleStates();
-
-        var expectedState = CreateBasicGameState() with
+        [Test]
+        public void HandMustContainStrike()
         {
-            Hand = new Hand(),
-            DiscardPile = new DiscardPile(new StrikeCard()),
-            Energy = new Energy(2),
-            EnemyParty = new EnemyParty(new JawWorm { Health = new Health(4) }),
-        };
-        Assert.AreEqual(expectedState, resolvedStates.Single());
+            var gameState = CreateBasicGameState() with { Hand = new Hand() };
+            Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
+        }
+
+        [Test]
+        public void PlayerMustBeAlive()
+        {
+            var gameState = CreateBasicGameState() with { PlayerHealth = new Health(0) };
+            Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
+        }
+
+        [Test]
+        public void EnergyMustBeAtLeastOne()
+        {
+            var gameState = CreateBasicGameState() with { Energy = new Energy(0) };
+            Assert.Throws<ArgumentException>(() => new StrikeAction(gameState));
+        }
     }
 
-    [Test]
-    public void TestLethalStrike()
+    [TestFixture]
+    public class ResolveTests : StrikeActionTests
     {
-        var initialGameState = CreateBasicGameState() with
+        [Test]
+        public void TestStrike()
         {
-            Hand = new Hand(new StrikeCard()),
-            DiscardPile = new DiscardPile(),
-            Energy = new Energy(3),
-            EnemyParty = new EnemyParty(new JawWorm { Health = new Health(6) }),
-        };
+            var initialGameState = CreateBasicGameState() with
+            {
+                Hand = new Hand(new StrikeCard()),
+                DiscardPile = new DiscardPile(),
+                Energy = new Energy(3),
+                EnemyParty = new EnemyParty(new JawWorm { Health = new Health(10) }),
+            };
 
-        var strikeAction = new StrikeAction(initialGameState);
-        var resolvedStates = strikeAction.ResolveToPossibleStates();
+            var strikeAction = new StrikeAction(initialGameState);
+            var resolvedStates = strikeAction.ResolveToPossibleStates();
 
-        var expectedState = CreateBasicGameState() with
+            var expectedState = CreateBasicGameState() with
+            {
+                Hand = new Hand(),
+                DiscardPile = new DiscardPile(new StrikeCard()),
+                Energy = new Energy(2),
+                EnemyParty = new EnemyParty(new JawWorm { Health = new Health(4) }),
+            };
+            Assert.AreEqual(expectedState, resolvedStates.Single());
+        }
+
+        [Test]
+        public void TestLethalStrike()
         {
-            Hand = new Hand(),
-            DiscardPile = new DiscardPile(new StrikeCard()),
-            Energy = new Energy(2),
-            EnemyParty = new EnemyParty()
-        };
-        Assert.AreEqual(expectedState, resolvedStates.Single());
+            var initialGameState = CreateBasicGameState() with
+            {
+                Hand = new Hand(new StrikeCard()),
+                DiscardPile = new DiscardPile(),
+                Energy = new Energy(3),
+                EnemyParty = new EnemyParty(new JawWorm { Health = new Health(6) }),
+            };
+
+            var strikeAction = new StrikeAction(initialGameState);
+            var resolvedStates = strikeAction.ResolveToPossibleStates();
+
+            var expectedState = CreateBasicGameState() with
+            {
+                Hand = new Hand(),
+                DiscardPile = new DiscardPile(new StrikeCard()),
+                Energy = new Energy(2),
+                EnemyParty = new EnemyParty()
+            };
+            Assert.AreEqual(expectedState, resolvedStates.Single());
+        }
     }
 }
