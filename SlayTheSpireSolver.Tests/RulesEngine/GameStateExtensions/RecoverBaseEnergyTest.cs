d93187dd3ee05e4fe5b78a98660012b759b66019
cvennevik <cvennevik@gmail.com>
2022-05-28 18:15:05 +0200

    feat(GameState):add RecoverBaseEnergy extension

---
+++ SlayTheSpireSolver.Tests/RulesEngine/GameStateExtensions/RecoverBaseEnergyTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine;
+using SlayTheSpireSolver.RulesEngine.GameStateExtensions;
+using SlayTheSpireSolver.RulesEngine.Values;
+
+namespace SlayTheSpireSolver.Tests.RulesEngine.GameStateExtensions;
+
+[TestFixture]
+public class RecoverBaseEnergyTest
+{
+    [Test]
+    [TestCase(3, 0)]
+    [TestCase(3, 1)]
+    [TestCase(3, 2)]
+    [TestCase(3, 3)]
+    [TestCase(3, 4)]
+    [TestCase(3, 10)]
+    [TestCase(4, 3)]
+    [TestCase(4, 4)]
+    public void ResetsEnergyToBaseEnergy(int amountOfBaseEnergy, int initialAmountOfEnergy)
+    {
+        var gameState = new GameState
+        {
+            BaseEnergy = new Energy(amountOfBaseEnergy),
+            Energy = new Energy(initialAmountOfEnergy)
+        };
+        var nextGameState = gameState.RecoverBaseEnergy();
+        var expectedGameState = gameState with { Energy = new Energy(amountOfBaseEnergy) };
+        Assert.AreEqual(expectedGameState, nextGameState);
+    }
+}
---
+++ SlayTheSpireSolver/RulesEngine/GameStateExtensions/RecoverBaseEnergyExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace SlayTheSpireSolver.RulesEngine.GameStateExtensions;
+
+public static class RecoverBaseEnergyExtension
+{
+    public static GameState RecoverBaseEnergy(this GameState gameState)
+    {
+        return gameState with { Energy = gameState.BaseEnergy };
+    }
+}
