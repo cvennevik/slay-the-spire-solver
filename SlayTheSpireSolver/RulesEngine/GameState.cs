541d7561dae48f1f1c21e9e85af638df48f991cc
cvennevik <cvennevik@gmail.com>
2022-05-28 18:07:32 +0200

    feat(GameState): add base energy

--- SlayTheSpireSolver/RulesEngine/GameState.cs
+++ SlayTheSpireSolver/RulesEngine/GameState.cs
@@ -7,6 +7,7 @@ public record GameState
 {
     public Health PlayerHealth { get; init; } = new(1);
     public Armor PlayerArmor { get; init; } = new(0);
+    public Energy BaseEnergy { get; init; } = new(3);
     public Energy Energy { get; init; } = new(0);
     public EnemyParty EnemyParty { get; init; } = new();
     public Turn Turn { get; init; } = new(1);
