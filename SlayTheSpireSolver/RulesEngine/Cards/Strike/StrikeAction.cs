4f20e112623f998821b0488bff40026de429e5e2
cvennevik <cvennevik@gmail.com>
2022-06-11 17:16:07 +0200

    refactor(StrikeAction): resolve using effects

--- SlayTheSpireSolver/RulesEngine/Cards/Strike/StrikeAction.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Strike/StrikeAction.cs
@@ -1,5 +1,4 @@
-﻿using SlayTheSpireSolver.RulesEngine.Enemies;
-using SlayTheSpireSolver.RulesEngine.GameStateExtensions;
+﻿using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Cards.Strike;
@@ -7,6 +6,7 @@ namespace SlayTheSpireSolver.RulesEngine.Cards.Strike;
 public readonly record struct StrikeAction : IAction
 {
     private readonly GameState _gameState;
+    private readonly IEffect _effect;
 
     private static readonly Energy EnergyCost = new(1);
     private static readonly Damage Damage = new(6);
@@ -22,20 +22,16 @@ public readonly record struct StrikeAction : IAction
     {
         if (!IsLegal(gameState)) throw new ArgumentException("Illegal Strike action");
         _gameState = gameState;
+        var targetEnemy = _gameState.EnemyParty.First();
+        _effect = new CombinedEffect(
+            new RemoveEnergyEffect(1),
+            new RemoveCardFromHandEffect(new StrikeCard()),
+            new DamageEnemyEffect(targetEnemy, Damage),
+            new AddCardToDiscardPileEffect(new StrikeCard()));
     }
 
     public IReadOnlyList<GameState> ResolveToPossibleStates()
     {
-        var damagedEnemy = _gameState.EnemyParty.First().DealDamage(Damage);
-        var newEnemyParty = damagedEnemy.Health.Amount > 0
-            ? new EnemyParty(damagedEnemy)
-            : new EnemyParty();
-        var resolvedState = _gameState
-                .Remove(EnergyCost)
-                .MoveCardFromHandToDiscardPile(new StrikeCard()) with
-            {
-                EnemyParty = newEnemyParty
-            };
-        return new[] { resolvedState };
+        return _effect.ApplyTo(_gameState);
     }
 }
