9d1ee8a1a0fc3e95a83666e4794b7f491b85edc2
cvennevik <cvennevik@gmail.com>
2022-06-11 19:06:19 +0200

    refactor: replace card action tests with asserting card effect

--- SlayTheSpireSolver.Tests/RulesEngine/Cards/CommonCardTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Cards/CommonCardTests.cs
@@ -1,5 +1,8 @@
+using System.Linq;
 using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Cards;
+using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Cards;
 
@@ -7,8 +10,23 @@ namespace SlayTheSpireSolver.Tests.RulesEngine.Cards;
 public class CommonCardTests
 {
     [TestFixture]
-    public class CommonStrikeTests : CommonCardTestsBase<Strike> { }
+    public class CommonStrikeTests : CommonCardTestsBase<Strike>
+    {
+        [Test]
+        public void TestEffect()
+        {
+            Assert.AreEqual(new DamageEnemyEffect(BasicGameState.EnemyParty.First(), new Damage(6)),
+                Card.GetEffect(BasicGameState));
+        }
+    }
 
     [TestFixture]
-    public class CommonDefendTests : CommonCardTestsBase<Defend> { }
+    public class CommonDefendTests : CommonCardTestsBase<Defend>
+    {
+        [Test]
+        public void TestEffect()
+        {
+            Assert.AreEqual(new GainPlayerArmorEffect(5), Card.GetEffect(BasicGameState));
+        }
+    }
 }
--- SlayTheSpireSolver.Tests/RulesEngine/Cards/CommonCardTestsBase.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Cards/CommonCardTestsBase.cs
@@ -11,18 +11,18 @@ namespace SlayTheSpireSolver.Tests.RulesEngine.Cards;
 
 public class CommonCardTestsBase<TCard> where TCard : ICard, new()
 {
-    private readonly TCard _card;
-    private readonly GameState _basicGameState;
+    protected readonly TCard Card;
+    protected readonly GameState BasicGameState;
 
     protected CommonCardTestsBase()
     {
-        _card = new TCard();
-        _basicGameState = new GameState
+        Card = new TCard();
+        BasicGameState = new GameState
         {
             PlayerHealth = new Health(70),
             Energy = new Energy(3),
             EnemyParty = new EnemyParty(new JawWorm { Health = new Health(40), IntendedMove = new Chomp() }),
-            Hand = new Hand(_card),
+            Hand = new Hand(Card),
             DiscardPile = new DiscardPile(),
         };
     }
@@ -36,50 +36,50 @@ public class CommonCardTestsBase<TCard> where TCard : ICard, new()
     [Test]
     public void PlayCardActionsWithSameGameStateAreEqual()
     {
-        Assert.AreEqual(new PlayCardAction(_basicGameState, _card), new PlayCardAction(_basicGameState, _card));
+        Assert.AreEqual(new PlayCardAction(BasicGameState, Card), new PlayCardAction(BasicGameState, Card));
     }
 
     [Test]
     public void PlayCardActionsWithDifferentGameStatesAreNotEqual()
     {
-        Assert.AreNotEqual(new PlayCardAction(_basicGameState, _card),
-            new PlayCardAction(_basicGameState with {Turn = new Turn(2)}, _card));
+        Assert.AreNotEqual(new PlayCardAction(BasicGameState, Card),
+            new PlayCardAction(BasicGameState with {Turn = new Turn(2)}, Card));
     }
 
     [Test]
     public void OneLegalActionForBasicGameState()
     {
-        var legalActions = PlayCardAction.GetLegalActions(_basicGameState, _card);
-        Assert.AreEqual(new PlayCardAction(_basicGameState, _card), legalActions.Single());
+        var legalActions = PlayCardAction.GetLegalActions(BasicGameState, Card);
+        Assert.AreEqual(new PlayCardAction(BasicGameState, Card), legalActions.Single());
     }
 
     [Test]
     public void NoLegalActionsWhenNoEnemies()
     {
-        AssertNoLegalActions(_basicGameState with { EnemyParty = new EnemyParty() });
+        AssertNoLegalActions(BasicGameState with { EnemyParty = new EnemyParty() });
     }
 
     [Test]
     public void NoLegalActionsWhenCardNotInHand()
     {
-        AssertNoLegalActions(_basicGameState with { Hand = new Hand() });
+        AssertNoLegalActions(BasicGameState with { Hand = new Hand() });
     }
 
     [Test]
     public void NoLegalActionsWhenPlayerDefeated()
     {
-        AssertNoLegalActions(_basicGameState with { PlayerHealth = new Health(0) });
+        AssertNoLegalActions(BasicGameState with { PlayerHealth = new Health(0) });
     }
 
     [Test]
     public void NoLegalActionsWhenNoEnergy()
     {
-        AssertNoLegalActions(_basicGameState with { Energy = new Energy(0) });
+        AssertNoLegalActions(BasicGameState with { Energy = new Energy(0) });
     }
 
     private void AssertNoLegalActions(GameState gameState)
     {
-        Assert.IsEmpty(PlayCardAction.GetLegalActions(gameState, _card));
-        Assert.Throws<ArgumentException>(() => new PlayCardAction(gameState, _card));
+        Assert.IsEmpty(PlayCardAction.GetLegalActions(gameState, Card));
+        Assert.Throws<ArgumentException>(() => new PlayCardAction(gameState, Card));
     }
 }
