f1353e03d2e7497abe99af5606ed8c5d628e770f
cvennevik <cvennevik@gmail.com>
2022-06-12 17:43:54 +0200

    feat(Effect): remove ApplyTo interface method

--- SlayTheSpireSolver/RulesEngine/Effects/IEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/IEffect.cs
@@ -2,6 +2,5 @@ namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public interface IEffect
 {
-    IReadOnlyList<GameState> ApplyTo(GameState gameState);
     IReadOnlyCollection<GameStateWithEffectStack> Resolve(GameState gameState);
 }
