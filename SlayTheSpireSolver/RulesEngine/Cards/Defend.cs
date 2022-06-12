7f022822ec63bbad94b4b783b94d007db66b2252
cvennevik <cvennevik@gmail.com>
2022-06-12 16:13:33 +0200

    refactor(Card): move Cost property up to Card

--- SlayTheSpireSolver/RulesEngine/Cards/Defend.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Defend.cs
@@ -5,10 +5,13 @@ namespace SlayTheSpireSolver.RulesEngine.Cards;
 
 public record Defend : Card
 {
-    private static readonly Energy Cost = new(1);
+    public Defend()
+    {
+        Cost = new Energy(1);
+    }
+
     private static readonly IEffect Effect = new GainPlayerArmorEffect(new Armor(5));
 
-    public override Energy GetCost() => Cost;
     public override IEffect GetEffect(GameState gameState) => Effect;
 
     public override IReadOnlyCollection<IAction> GetLegalActions(GameState gameState) =>
--- SlayTheSpireSolver/RulesEngine/Cards/Strike.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Strike.cs
@@ -5,9 +5,11 @@ namespace SlayTheSpireSolver.RulesEngine.Cards;
 
 public record Strike : Card
 {
-    private static readonly Energy Cost = new(1);
+    public Strike()
+    {
+        Cost = new Energy(1);
+    }
 
-    public override Energy GetCost() => Cost;
     public override IEffect GetEffect(GameState gameState) => new DamageEnemyEffect(gameState.EnemyParty.First(), new Damage(6));
 
     public override IReadOnlyCollection<IAction> GetLegalActions(GameState gameState) =>
