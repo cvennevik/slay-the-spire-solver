f7b3b9960d9fdaff27b9371b90e351df02c52b05
cvennevik <cvennevik@gmail.com>
2022-06-12 17:22:50 +0200

    refactor(PlayCardAction): use EffectStack instead of CombinedEffect

--- SlayTheSpireSolver/RulesEngine/Cards/PlayCardAction.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/PlayCardAction.cs
@@ -5,21 +5,29 @@ namespace SlayTheSpireSolver.RulesEngine.Cards;
 public readonly record struct PlayCardAction : IAction
 {
     private readonly GameState _gameState;
-    private readonly IEffect _effect;
+    private readonly EffectStack _effectStack;
 
     public PlayCardAction(GameState gameState, Card card)
     {
         if (!card.CanBePlayed(gameState)) throw new ArgumentException("Illegal PlayCard action");
         _gameState = gameState;
-        _effect = new CombinedEffect(
-            new RemoveEnergyEffect(card.GetCost()),
-            new RemoveCardFromHandEffect(card),
+        _effectStack = new EffectStack(
+            new AddCardToDiscardPileEffect(card),
             card.GetEffect(gameState),
-            new AddCardToDiscardPileEffect(card));
+            new RemoveCardFromHandEffect(card),
+            new RemoveEnergyEffect(card.GetCost()));
     }
 
     public IReadOnlyList<GameState> ResolveToPossibleStates()
     {
-        return _effect.Resolve(_gameState).Select(x => x.GameState).ToList();
+        var remainingEffectStack = _effectStack;
+        var workingGameState = _gameState;
+        while (remainingEffectStack != new EffectStack())
+        {
+            (var effect, remainingEffectStack) = remainingEffectStack.Pop();
+            workingGameState = effect.Resolve(workingGameState).Select(x => x.GameState).Single();
+        }
+
+        return new[] { workingGameState };
     }
 }
