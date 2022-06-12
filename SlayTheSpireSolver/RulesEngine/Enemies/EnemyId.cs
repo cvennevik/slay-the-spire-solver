7b3ea3c40c8483b510f99b998dcd978d1d40d1b3
cvennevik <cvennevik@gmail.com>
2022-06-12 20:54:18 +0200

    feat(EnemyId): assign random string and print as ToString()

--- SlayTheSpireSolver/RulesEngine/Enemies/EnemyId.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/EnemyId.cs
@@ -4,7 +4,25 @@ public class EnemyId
 {
     public static readonly EnemyId Default = new();
 
-    private EnemyId() { }
-
     public static EnemyId New() => new();
+
+    // Arbitrary string ID makes enemies' ToString() more readable
+    private readonly string _printedId;
+
+    private EnemyId()
+    {
+        _printedId = GeneratePrintedId();
+    }
+
+    private static string GeneratePrintedId()
+    {
+        var random = new Random();
+        const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        return new string(Enumerable.Repeat(chars, 4).Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    public override string ToString()
+    {
+        return $"{_printedId}";
+    }
 }
