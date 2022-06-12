79efe98f55af325e347e7768dfd1c918ed1edd8f
cvennevik <cvennevik@gmail.com>
2022-06-12 15:40:41 +0200

    feat: add ActionWithEffectStack

---
+++ SlayTheSpireSolver.Tests/RulesEngine/ActionWithEffectStackTests.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine;
+using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Values;
+
+namespace SlayTheSpireSolver.Tests.RulesEngine;
+
+[TestFixture]
+public class ActionWithEffectStackTests
+{
+    [Test]
+    public void ResolvesZeroEffects()
+    {
+        var actionWithEffectStack = new ActionWithEffectStack(new GameState(), new EffectStack());
+        var resolvedState = actionWithEffectStack.ResolveToPossibleStates().Single();
+        Assert.AreEqual(new GameState(), resolvedState);
+    }
+
+    [Test]
+    public void ResolvesOneEffect()
+    {
+        var gameState = new GameState { PlayerArmor = new Armor(0) };
+        var effectStack = new EffectStack(new GainPlayerArmorEffect(5));
+        var actionWithEffectStack = new ActionWithEffectStack(gameState, effectStack);
+        var resolvedState = actionWithEffectStack.ResolveToPossibleStates().Single();
+        Assert.AreEqual(new GameState { PlayerArmor = new Armor(5) }, resolvedState);
+    }
+
+    [Test]
+    public void ResolvesTwoEffects()
+    {
+        var gameState = new GameState { Energy = new Energy(2), PlayerArmor = new Armor(0) };
+        var effectStack = new EffectStack(new GainPlayerArmorEffect(5), new RemoveEnergyEffect(new Energy(1)));
+        var actionWithEffectStack = new ActionWithEffectStack(gameState, effectStack);
+        var resolvedState = actionWithEffectStack.ResolveToPossibleStates().Single();
+        Assert.AreEqual(new GameState { Energy = new Energy(1), PlayerArmor = new Armor(5) }, resolvedState);
+    }
+
+    [Test]
+    public void EqualityTests()
+    {
+        var gameState1 = new GameState { Turn = new Turn(1) };
+        var gameState2 = new GameState { Turn = new Turn(2) };
+        var effectStack1 = new EffectStack(new RemoveEnergyEffect(new Energy(1)));
+        var effectStack2 = new EffectStack(new RemoveEnergyEffect(new Energy(2)));
+        Assert.AreEqual(new ActionWithEffectStack(gameState1, effectStack1),
+            new ActionWithEffectStack(gameState1, effectStack1));
+        Assert.AreEqual(new ActionWithEffectStack(gameState2, effectStack2),
+            new ActionWithEffectStack(gameState2, effectStack2));
+        Assert.AreNotEqual(new ActionWithEffectStack(gameState1, effectStack1),
+            new ActionWithEffectStack(gameState1, effectStack2));
+        Assert.AreNotEqual(new ActionWithEffectStack(gameState1, effectStack1),
+            new ActionWithEffectStack(gameState2, effectStack1));
+    }
+}
---
+++ SlayTheSpireSolver/RulesEngine/ActionWithEffectStack.cs
@@ -0,0 +1,19 @@
+using SlayTheSpireSolver.RulesEngine.Effects;
+
+namespace SlayTheSpireSolver.RulesEngine;
+
+public record ActionWithEffectStack(GameState GameState, EffectStack EffectStack) : IAction
+{
+    public IReadOnlyList<GameState> ResolveToPossibleStates()
+    {
+        var remainingEffectStack = EffectStack;
+        var workingGameState = GameState;
+        while (remainingEffectStack != new EffectStack())
+        {
+            (var effect, remainingEffectStack) = remainingEffectStack.Pop();
+            workingGameState = effect.Resolve(workingGameState).Select(x => x.GameState).Single();
+        }
+
+        return new[] { workingGameState };
+    }
+};
