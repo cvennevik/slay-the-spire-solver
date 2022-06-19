d7f91cc1de0df9c869af2ea26848a63905a719dc
cvennevik <cvennevik@gmail.com>
2022-06-19 19:05:44 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/GainEnemyArmorEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/GainEnemyArmorEffectTests.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
@@ -12,5 +14,6 @@ public class GainEnemyArmorEffectTests
     public void Test()
     {
         var effect = new GainEnemyArmorEffect(EnemyId.Default, new Armor(5));
+        var gameState = new GameState { EnemyParty = new EnemyParty(new JawWorm()) };
     }
 }
