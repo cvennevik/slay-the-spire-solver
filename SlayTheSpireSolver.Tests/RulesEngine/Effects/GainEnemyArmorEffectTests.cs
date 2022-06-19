5006c184bd814f5fe51392f7b3b0c220911b8e40
cvennevik <cvennevik@gmail.com>
2022-06-19 19:05:20 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/GainEnemyArmorEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/GainEnemyArmorEffectTests.cs
@@ -1,4 +1,7 @@
 using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 
@@ -8,6 +11,6 @@ public class GainEnemyArmorEffectTests
     [Test]
     public void Test()
     {
-        
+        var effect = new GainEnemyArmorEffect(EnemyId.Default, new Armor(5));
     }
 }
