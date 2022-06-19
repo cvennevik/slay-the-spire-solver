a69f4bc042ced0cee4cd74fdbe221ebc30538d62
cvennevik <cvennevik@gmail.com>
2022-06-19 19:08:21 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/GainEnemyArmorEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/GainEnemyArmorEffect.cs
@@ -7,6 +7,7 @@ public record GainEnemyArmorEffect(EnemyId EnemyId, Armor Armor) : IEffect
 {
     public IReadOnlyCollection<GameStateWithEffectStack> Resolve(GameState gameState)
     {
+        var enemy = gameState.EnemyParty.Get(EnemyId);
         return new[] { gameState.WithEffectStack() };
     }
 }
