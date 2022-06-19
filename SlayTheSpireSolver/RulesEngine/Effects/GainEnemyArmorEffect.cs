afef39b3ee1acb73b0a36f00081fa525fac3f30a
cvennevik <cvennevik@gmail.com>
2022-06-19 19:04:07 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/GainEnemyArmorEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/GainEnemyArmorEffect.cs
@@ -1,5 +1,6 @@
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record GainEnemyArmorEffect(EnemyId EnemyId);
+public record GainEnemyArmorEffect(EnemyId EnemyId, Armor Armor);
