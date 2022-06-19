e0f3365155ecc55d373db6f66843a967ce1c6ca8
cvennevik <cvennevik@gmail.com>
2022-06-19 18:52:43 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/DamagePlayerEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/DamagePlayerEffectTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Effects;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 
@@ -8,6 +9,6 @@ public class DamagePlayerEffectTests
     [Test]
     public void Test()
     {
-        
+        var damagePlayerEffect = new DamagePlayerEffect();
     }
 }
