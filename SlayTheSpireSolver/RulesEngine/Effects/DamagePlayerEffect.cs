84d6dd0e0a4196da860fb0e686f117e473942ecd
cvennevik <cvennevik@gmail.com>
2022-06-19 18:54:08 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
@@ -2,12 +2,17 @@ using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record DamagePlayerEffect
+public record DamagePlayerEffect : IEffect
 {
     public Damage Damage { get; }
 
     public DamagePlayerEffect(Damage damage)
     {
         Damage = damage;
+    }
+
+    public IReadOnlyCollection<GameStateWithEffectStack> Resolve(GameState gameState)
+    {
+        throw new NotImplementedException();
     }
 }
