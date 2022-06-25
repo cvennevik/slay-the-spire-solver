eceed9e58ea7ada8fa2487b927718272129cd515
cvennevik <cvennevik@gmail.com>
2022-06-25 21:18:36 +0200

    working

--- SlayTheSpireSolver/RulesEngine/ResolvablePossibility.cs
+++ SlayTheSpireSolver/RulesEngine/ResolvablePossibility.cs
@@ -20,6 +20,12 @@ public record ResolvablePossibility(ResolvableGameState ResolvableGameState, Pro
         return new GameStatePossibility[] { ResolvableGameState.GameState };
     }
 
+    private ResolvablePossibilitySet ResolveTopEffect()
+    {
+        var (effect, remainingEffectStack) = ResolvableGameState.EffectStack.Pop();
+        return effect.ResolveWithBaseEffectStack(ResolvableGameState.GameState, remainingEffectStack);
+    }
+
     public ResolvablePossibility WithBaseEffectStack(EffectStack baseEffectStack)
     {
         return this with
