107cacdaef12814e8b3f3095aea316debda69faa
cvennevik <cvennevik@gmail.com>
2022-06-25 12:50:35 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Action.cs
+++ SlayTheSpireSolver/RulesEngine/Action.cs
@@ -11,28 +11,4 @@ public record Action : GameStateWithEffectStack
     {
         return ResolveEffects(new GameStateWithEffectStack(GameState, EffectStack));
     }
-
-    private IReadOnlyList<GameStateWithEffectStack> ResolveTopEffect(GameState gameState, EffectStack effectStack)
-    {
-        var (effect, remainingEffectStack) = effectStack.Pop();
-        var outcomes = effect.Resolve(gameState);
-        return outcomes.Select(gameStateWithAddedEffects =>
-            gameStateWithAddedEffects.GameState.WithEffectStack(
-                remainingEffectStack.Push(gameStateWithAddedEffects.EffectStack))).ToList();
-    }
-
-    private IReadOnlyList<GameStateWithEffectStack> ResolveTopEffect(GameStateWithEffectStack gameStateWithEffectStack)
-    {
-        return ResolveTopEffect(gameStateWithEffectStack.GameState, gameStateWithEffectStack.EffectStack);
-    }
-
-    private IReadOnlyList<GameState> ResolveEffects(GameStateWithEffectStack gameStateWithEffectStack)
-    {
-        if (gameStateWithEffectStack.EffectStack.IsEmpty())
-        {
-            return new[] { gameStateWithEffectStack.GameState };
-        }
-
-        return ResolveTopEffect(gameStateWithEffectStack).SelectMany(ResolveEffects).Distinct().ToList();
-    }
 }
--- SlayTheSpireSolver/RulesEngine/Effects/GameStateWithEffectStack.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/GameStateWithEffectStack.cs
@@ -20,4 +20,28 @@ public record GameStateWithEffectStack
 
     public GameStateWithEffectStack(GameState gameState, params IEffect[] effects)
         : this(gameState, new EffectStack(effects)) { }
+
+    protected IReadOnlyList<GameState> ResolveEffects(GameStateWithEffectStack gameStateWithEffectStack)
+    {
+        if (gameStateWithEffectStack.EffectStack.IsEmpty())
+        {
+            return new[] { gameStateWithEffectStack.GameState };
+        }
+
+        return ResolveTopEffect(gameStateWithEffectStack).SelectMany(ResolveEffects).Distinct().ToList();
+    }
+
+    private IReadOnlyList<GameStateWithEffectStack> ResolveTopEffect(GameState gameState, EffectStack effectStack)
+    {
+        var (effect, remainingEffectStack) = effectStack.Pop();
+        var outcomes = effect.Resolve(gameState);
+        return outcomes.Select(gameStateWithAddedEffects =>
+            gameStateWithAddedEffects.GameState.WithEffectStack(
+                remainingEffectStack.Push(gameStateWithAddedEffects.EffectStack))).ToList();
+    }
+
+    private IReadOnlyList<GameStateWithEffectStack> ResolveTopEffect(GameStateWithEffectStack gameStateWithEffectStack)
+    {
+        return ResolveTopEffect(gameStateWithEffectStack.GameState, gameStateWithEffectStack.EffectStack);
+    }
 }
