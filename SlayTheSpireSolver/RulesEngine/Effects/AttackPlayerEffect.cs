17324093a2b37d0d58d085f9e989cc21fa3b3516
cvennevik <cvennevik@gmail.com>
2022-06-25 15:20:24 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/AttackPlayerEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/AttackPlayerEffect.cs
@@ -8,6 +8,7 @@ public record AttackPlayerEffect(EnemyId EnemyId, Damage BaseDamage) : IEffect
     public ResolvableGameStateSet Resolve(GameState gameState)
     {
         if (!gameState.EnemyParty.Has(EnemyId)) return gameState;
+        var enemyStrength = gameState.EnemyParty.Get(EnemyId).Strength;
 
         return gameState.WithEffects(new EffectStack(
             new DamagePlayerEffect(BaseDamage.Amount + gameState.EnemyParty.Get(EnemyId).Strength.Amount)));
