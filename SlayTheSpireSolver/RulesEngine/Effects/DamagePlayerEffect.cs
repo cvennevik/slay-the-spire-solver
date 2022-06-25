3943d5782843bfadbe71151e06bd7afa5636de34
cvennevik <cvennevik@gmail.com>
2022-06-25 13:55:05 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
@@ -6,7 +6,6 @@ public record DamagePlayerEffect(Damage Damage) : IEffect
 {
     public ResolvableGameStateSet Resolve(GameState gameState)
     {
-        var result = gameState with { PlayerHealth = gameState.PlayerHealth - Damage };
-        return new[] { result.AsResolvable() };
+        return gameState with { PlayerHealth = gameState.PlayerHealth - Damage };
     }
 }
