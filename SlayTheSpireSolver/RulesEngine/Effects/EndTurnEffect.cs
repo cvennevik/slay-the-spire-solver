8085dabf80769d4227c47e3ab64fd17eb9ce238a
cvennevik <cvennevik@gmail.com>
2022-06-25 13:56:27 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/EndTurnEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/EndTurnEffect.cs
@@ -4,19 +4,16 @@ public record EndTurnEffect : IEffect
 {
     public ResolvableGameStateSet Resolve(GameState gameState)
     {
-        return new[]
-        {
-            gameState.AsResolvable(new EffectStack(
-                new DrawCardEffect(),
-                new DrawCardEffect(),
-                new DrawCardEffect(),
-                new DrawCardEffect(),
-                new DrawCardEffect(),
-                new RecoverBaseEnergyEffect(),
-                new IncrementTurnEffect(),
-                new ResolveAllEnemyMovesEffect(),
-                new ClearAllEnemyArmorEffect(),
-                new MoveHandToDiscardPileEffect()))
-        };
+        return gameState.AsResolvable(new EffectStack(
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new RecoverBaseEnergyEffect(),
+            new IncrementTurnEffect(),
+            new ResolveAllEnemyMovesEffect(),
+            new ClearAllEnemyArmorEffect(),
+            new MoveHandToDiscardPileEffect()));
     }
 }
