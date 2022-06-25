20792af7086d582700206e0b1ef2df203e84ab32
cvennevik <cvennevik@gmail.com>
2022-06-25 13:30:09 +0200

    working

--- SlayTheSpireSolver/RulesEngine/ResolvableGameStateSet.cs
+++ SlayTheSpireSolver/RulesEngine/ResolvableGameStateSet.cs
@@ -2,4 +2,10 @@ namespace SlayTheSpireSolver.RulesEngine;
 
 public class ResolvableGameStateSet
 {
+    private readonly ResolvableGameState[] _resolvableGameStates;
+
+    public ResolvableGameStateSet(params ResolvableGameState[] resolvableGameStates)
+    {
+        _resolvableGameStates = resolvableGameStates;
+    }
 }
