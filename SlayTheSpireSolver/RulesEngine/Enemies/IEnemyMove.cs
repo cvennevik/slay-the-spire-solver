cc5f423136c72059e944b4c940b0e438f060856c
cvennevik <cvennevik@gmail.com>
2022-06-25 15:33:00 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/IEnemyMove.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/IEnemyMove.cs
@@ -4,6 +4,5 @@ namespace SlayTheSpireSolver.RulesEngine.Enemies;
 
 public interface IEnemyMove
 {
-    GameState Resolve(GameState gameState);
     EffectStack GetEffects(Enemy enemy);
 }
--- SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Thrash.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Thrash.cs
@@ -8,28 +8,6 @@ public record Thrash : IJawWormMove
     private static readonly Damage Damage = 7;
     private static readonly Armor ArmorGain = 5;
 
-    public GameState Resolve(GameState gameState)
-    {
-        var newPlayerArmor = gameState.PlayerArmor - Damage;
-        var newPlayerNealth = gameState.PlayerHealth - (Damage - gameState.PlayerArmor);
-
-        var enemy = gameState.EnemyParty.FirstOrDefault();
-        var newEnemyParty = gameState.EnemyParty;
-        if (enemy != null)
-        {
-            enemy = enemy with { Armor = enemy.Armor + ArmorGain };
-            var otherEnemies = gameState.EnemyParty.Skip(1).ToArray();
-            newEnemyParty = new EnemyParty(new[] { enemy }.Concat(otherEnemies).ToArray());
-        }
-
-        return gameState with
-        {
-            PlayerHealth = newPlayerNealth,
-            PlayerArmor = newPlayerArmor,
-            EnemyParty = newEnemyParty
-        };
-    }
-
     public EffectStack GetEffects(Enemy enemy)
     {
         return new EffectStack(new GainEnemyArmorEffect(enemy.Id, ArmorGain), new AttackPlayerEffect(enemy.Id, Damage));
