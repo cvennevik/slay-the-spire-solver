30c53ea6134d366d776e6f01e6652f74ec7a0519
cvennevik <cvennevik@gmail.com>
2022-06-25 09:44:38 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/ClearAllEnemyArmorEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/ClearAllEnemyArmorEffectTests.cs
@@ -5,5 +5,9 @@ namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 [TestFixture]
 public class ClearAllEnemyArmorEffectTests
 {
-    
+    [Test]
+    public void Test()
+    {
+        
+    }
 }
