3c3e1f4558fdbb72da9dcf7e96b6a733c3232786
cvennevik <cvennevik@gmail.com>
2022-06-25 14:43:42 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DrawCardEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DrawCardEffect.cs
@@ -6,6 +6,28 @@ public record DrawCardEffect : IEffect
 {
     public ResolvableGameStateSet Resolve(GameState gameState)
     {
-        return gameState.DrawCard().Select(x => x.WithEffects()).ToArray();
+        return DrawCard(gameState).Select(x => x.WithEffects()).ToArray();
+    }
+
+    public static IReadOnlyList<GameState> DrawCard(GameState gameState)
+    {
+        if (gameState.DrawPile.Cards.Count == 0)
+        {
+            return gameState.DiscardPile.Cards.Any()
+                ? gameState.ShuffleDiscardPileIntoDrawPile().DrawCard()
+                : new[] { gameState };
+        }
+
+        var possibleStates = new List<GameState>();
+        foreach (var card in gameState.DrawPile.Cards)
+        {
+            possibleStates.Add(gameState with
+            {
+                Hand = gameState.Hand.Add(card),
+                DrawPile = gameState.DrawPile.Remove(card)
+            });
+        }
+
+        return possibleStates.Distinct().ToArray();
     }
 }
