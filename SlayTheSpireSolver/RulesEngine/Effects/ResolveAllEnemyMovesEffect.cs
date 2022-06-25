42db2b6f2f5f1eabd9b4c917e6094bad93fd4a76
cvennevik <cvennevik@gmail.com>
2022-06-25 13:58:54 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ResolveAllEnemyMovesEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ResolveAllEnemyMovesEffect.cs
@@ -6,6 +6,6 @@ public record ResolveAllEnemyMovesEffect : IEffect
     {
         var resolveEnemyMoveEffects =
             gameState.EnemyParty.Select(enemy => (IEffect)new ResolveEnemyMoveEffect(enemy.Id)).Reverse();
-        return new[] { gameState.AsResolvable(new EffectStack(resolveEnemyMoveEffects.ToArray())) };
+        return gameState.AsResolvable(new EffectStack(resolveEnemyMoveEffects.ToArray()));
     }
 }
