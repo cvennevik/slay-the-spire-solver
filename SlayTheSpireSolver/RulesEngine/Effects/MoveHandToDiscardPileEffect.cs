a08e3054cca9d7aa071bdfb770279886ab3c2378
cvennevik <cvennevik@gmail.com>
2022-06-25 10:13:37 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/MoveHandToDiscardPileEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/MoveHandToDiscardPileEffect.cs
@@ -5,6 +5,7 @@ public record MoveHandToDiscardPileEffect : IEffect
     public IReadOnlyCollection<GameStateWithEffectStack> Resolve(GameState gameState)
     {
         var cardsInHand = gameState.Hand.Cards;
+        var newDiscardPile = gameState.DiscardPile;
         return new[] { gameState.WithEffectStack() };
     }
 }
