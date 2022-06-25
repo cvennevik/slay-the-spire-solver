db2b32bfa15a2e252427599b2d4f956f5fa966af
cvennevik <cvennevik@gmail.com>
2022-06-25 15:33:19 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Chomp.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Chomp.cs
@@ -5,19 +5,7 @@ namespace SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 public record Chomp : IJawWormMove
 {
-    private static readonly Damage Damage = new(12);
-
-    public GameState Resolve(GameState gameState)
-    {
-        if (Damage > gameState.PlayerArmor)
-        {
-            var remainingDamage = Damage - gameState.PlayerArmor;
-            var newPlayerHealth = gameState.PlayerHealth - remainingDamage;
-            return gameState with { PlayerHealth = newPlayerHealth, PlayerArmor = 0 };
-        }
-
-        return gameState with { PlayerArmor = gameState.PlayerArmor - Damage };
-    }
+    private static readonly Damage BaseDamage = new(12);
 
     public EffectStack GetEffects(Enemy enemy)
     {
