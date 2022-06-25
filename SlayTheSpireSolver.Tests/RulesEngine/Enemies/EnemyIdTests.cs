82d302b28262460b8f18e95ece27699ef7f11bde
cvennevik <cvennevik@gmail.com>
2022-06-25 16:05:48 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Enemies/EnemyIdTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Enemies/EnemyIdTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Enemies;
 
@@ -14,5 +15,24 @@ public class EnemyIdTests
         Assert.AreEqual(enemyId1, enemyId1);
         Assert.AreEqual(enemyId2, enemyId2);
         Assert.AreNotEqual(enemyId1, enemyId2);
+    }
+
+    [Test]
+    public void NewEnemiesAreEqual()
+    {
+        Assert.AreEqual(new JawWorm(), new JawWorm());
+    }
+
+    [Test]
+    public void NewEnemiesHaveDefaultId()
+    {
+        Assert.AreEqual(EnemyId.Default, new JawWorm().Id);
+        Assert.AreEqual(new JawWorm().Id, new JawWorm().Id);
+    }
+
+    [Test]
+    public void EnemiesWithDifferentIdsAreNotEqual()
+    {
+        Assert.AreNotEqual(new JawWorm { Id = EnemyId.New() }, new JawWorm { Id = EnemyId.New() });
     }
 }
