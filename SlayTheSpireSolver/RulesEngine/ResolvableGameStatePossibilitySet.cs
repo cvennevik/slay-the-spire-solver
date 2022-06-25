c6508584365abf79071cb98690554cb6aebf11cf
cvennevik <cvennevik@gmail.com>
2022-06-25 18:13:37 +0200

    working

--- SlayTheSpireSolver/RulesEngine/ResolvableGameStatePossibilitySet.cs
+++ SlayTheSpireSolver/RulesEngine/ResolvableGameStatePossibilitySet.cs
@@ -2,8 +2,10 @@ using System.Collections;
 
 namespace SlayTheSpireSolver.RulesEngine;
 
-public class ResolvableGameStatePossibilitySet : IEnumerable<ResolvableGameStatePossibility>
+public class ResolvableGameStatePossibilitySet : IEnumerable<ResolvableGameStatePossibility>, IEquatable<ResolvableGameStatePossibilitySet>
 {
+    private readonly List<ResolvableGameState> _possibilities;
+
     public IEnumerator<ResolvableGameStatePossibility> GetEnumerator()
     {
         throw new NotImplementedException();
@@ -12,5 +14,25 @@ public class ResolvableGameStatePossibilitySet : IEnumerable<ResolvableGameState
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
+    }
+
+    public bool Equals(ResolvableGameStatePossibilitySet? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _possibilities.Equals(other._possibilities);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ResolvableGameStatePossibilitySet)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return _possibilities.GetHashCode();
     }
 }
