707d0570e4b2bd578478780f425efbc875835fd1
cvennevik <cvennevik@gmail.com>
2022-06-25 10:42:42 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/EffectStackTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/EffectStackTests.cs
@@ -21,12 +21,17 @@ public class EffectStackTests
     }
 
     [Test]
-    public void TestPush()
+    public void TestPushEffect()
     {
         var effect1 = new RemoveEnergyEffect(1);
         var effect2 = new RemoveEnergyEffect(2);
         Assert.AreEqual(new EffectStack(effect1), new EffectStack().Push(effect1));
         Assert.AreEqual(new EffectStack(effect1, effect2), new EffectStack(effect1).Push(effect2));
+    }
+
+    [Test]
+    public void TestPushEffectStack()
+    {
     }
 
     [Test]
