8e4cbb00dcabfc766a1631ea91b991fc0e12040e
cvennevik <cvennevik@gmail.com>
2022-06-25 18:18:00 +0200

    working

--- SlayTheSpireSolver/RulesEngine/ResolvableGameStatePossibility.cs
+++ SlayTheSpireSolver/RulesEngine/ResolvableGameStatePossibility.cs
@@ -4,5 +4,6 @@ namespace SlayTheSpireSolver.RulesEngine;
 
 public record ResolvableGameStatePossibility(ResolvableGameState ResolvableGameState, Probability Probability)
 {
-    
+    public static implicit operator ResolvableGameStatePossibility(ResolvableGameState resolvableGameState) =>
+        new(resolvableGameState, new Probability(1));
 }
