be0bb2689cfb0225e8b4317e519409ff5a441e9e
cvennevik <cvennevik@gmail.com>
2022-06-25 15:28:25 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/DamagePlayerEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/DamagePlayerEffectTests.cs
@@ -13,6 +13,7 @@ public class DamagePlayerEffectTests
     [TestCase(20, 0, 0, 20, 0)]
     [TestCase(5, 0, 10, -5, 0)]
     [TestCase(10, 10, 5, 10, 5)]
+    [TestCase(10, 10, 10, 10, 0)]
     public void Test(int initialHealth, int initialArmor, int damage, int expectedHealth, int expectedArmor)
     {
         var damagePlayerEffect = new DamagePlayerEffect(damage);
