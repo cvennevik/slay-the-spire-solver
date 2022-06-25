de40fb90288cd2c6da1860fae75e56285a9502ec
cvennevik <cvennevik@gmail.com>
2022-06-25 15:41:50 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/AddEnemyStrengthEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/AddEnemyStrengthEffectTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Enemies;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 
@@ -8,6 +10,7 @@ public class AddEnemyStrengthEffectTests
     [Test]
     public void TestEquality()
     {
-        
+        Assert.AreEqual(new AddEnemyStrengthEffect(EnemyId.Default, 1),
+            new AddEnemyStrengthEffect(EnemyId.Default, 1));
     }
 }
