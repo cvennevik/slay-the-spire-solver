51eb06189a8683144ccd6b6824590966d33cbeff
cvennevik <cvennevik@gmail.com>
2022-06-25 20:54:37 +0200

    working

--- SlayTheSpireSolver/RulesEngine/GameStatePossibility.cs
+++ SlayTheSpireSolver/RulesEngine/GameStatePossibility.cs
@@ -2,4 +2,7 @@ using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine;
 
-public record GameStatePossibility(GameState GameState, Probability Probability);
+public record GameStatePossibility(GameState GameState, Probability Probability)
+{
+    public static implicit operator GameStatePossibility(GameState gameState) => new(gameState, new Probability(1));
+}
