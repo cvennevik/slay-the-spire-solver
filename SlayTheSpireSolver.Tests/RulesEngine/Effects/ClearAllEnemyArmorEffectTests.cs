5909fe3a7f21d449415b87a01c3516a246988ae6
cvennevik <cvennevik@gmail.com>
2022-06-25 09:46:05 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/ClearAllEnemyArmorEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/ClearAllEnemyArmorEffectTests.cs
@@ -2,7 +2,6 @@ using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
-using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 
