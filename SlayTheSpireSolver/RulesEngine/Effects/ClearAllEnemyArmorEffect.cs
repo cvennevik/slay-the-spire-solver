ac4f2213b8611422afe6919e159cca9f5a9e8246
cvennevik <cvennevik@gmail.com>
2022-06-25 13:54:51 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ClearAllEnemyArmorEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ClearAllEnemyArmorEffect.cs
@@ -4,9 +4,8 @@ public record ClearAllEnemyArmorEffect : IEffect
 {
     public ResolvableGameStateSet Resolve(GameState gameState)
     {
-        var enemies = gameState.EnemyParty.ToList();
         var newEnemyParty = gameState.EnemyParty;
-        foreach (var enemy in enemies)
+        foreach (var enemy in gameState.EnemyParty)
         {
             newEnemyParty = newEnemyParty.ModifyEnemy(enemy.Id, enemy => enemy with { Armor = 0 });
         }
