55849ee94b7e552106cf93658da6d7111548c0e4
cvennevik <cvennevik@gmail.com>
2022-06-25 17:36:21 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Values/ProbabilityTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Values/ProbabilityTests.cs
@@ -13,5 +13,6 @@ public class ProbabilityTests
         Assert.AreEqual(new Probability(1, 1), new Probability(1, 1));
         Assert.AreEqual(new Probability(0, 1), new Probability(0, 1));
         Assert.AreEqual(new Probability(3, 7), new Probability(3, 7));
+        Assert.AreNotEqual(new Probability(3, 7), new Probability(3, 8));
     }
 }
