e4bb47ba1417cbba7e432594df6f0c371859b99a
cvennevik <cvennevik@gmail.com>
2022-06-25 16:00:50 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Bellow.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Bellow.cs
@@ -1,11 +1,14 @@
 using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 public record Bellow : IJawWormMove
 {
+    private static readonly Armor ArmorGain = 9; 
+
     public EffectStack GetEffects(Enemy enemy)
     {
-        throw new NotImplementedException();
+        return new EffectStack();
     }
 }
