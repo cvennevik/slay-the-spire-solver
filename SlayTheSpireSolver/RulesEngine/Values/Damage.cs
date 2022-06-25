b372942652d273c09c68b0530db4c7de9889f33d
cvennevik <cvennevik@gmail.com>
2022-06-25 15:23:02 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Values/Damage.cs
+++ SlayTheSpireSolver/RulesEngine/Values/Damage.cs
@@ -1,3 +1,5 @@
+using SlayTheSpireSolver.RulesEngine.Buffs;
+
 namespace SlayTheSpireSolver.RulesEngine.Values;
 
 public record Damage
@@ -17,6 +19,9 @@ public record Damage
 
     public static Damage operator -(Damage damage, Armor armor) =>
         new(damage < armor ? 0 : damage.Amount - armor.Amount);
+
+    public static Damage operator +(Damage damage, Strength strength) =>
+        new(damage.Amount + strength.Amount);
 
     public static implicit operator Damage(int amount) => amount > 0 ? new Damage(amount) : new Damage(0);
 
