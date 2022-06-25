7dd6f0866290b55a2017628a22591881f763c631
cvennevik <cvennevik@gmail.com>
2022-06-25 18:13:00 +0200

    working

--- SlayTheSpireSolver/RulesEngine/ResolvableGameStatePossibilitySet.cs
+++ SlayTheSpireSolver/RulesEngine/ResolvableGameStatePossibilitySet.cs
@@ -1,6 +1,16 @@
+using System.Collections;
+
 namespace SlayTheSpireSolver.RulesEngine;
 
-public class ResolvableGameStatePossibilitySet
+public class ResolvableGameStatePossibilitySet : IEnumerable<ResolvableGameStatePossibility>
 {
-    
+    public IEnumerator<ResolvableGameStatePossibility> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 }
