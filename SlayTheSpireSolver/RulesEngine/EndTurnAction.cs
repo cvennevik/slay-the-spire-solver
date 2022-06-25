a6fd0ccaba16ee3636bbed866f87b032c265c8dc
cvennevik <cvennevik@gmail.com>
2022-06-25 11:21:32 +0200

    working

--- SlayTheSpireSolver/RulesEngine/EndTurnAction.cs
+++ SlayTheSpireSolver/RulesEngine/EndTurnAction.cs
@@ -4,11 +4,6 @@ namespace SlayTheSpireSolver.RulesEngine;
 
 public record EndTurnAction : ActionWithEffectStack
 {
-    private static bool IsLegal(GameState gameState)
-    {
-        return !gameState.IsCombatOver();
-    }
-
     public EndTurnAction(GameState gameState) : base(gameState, new EffectStack(new EndTurnEffect()))
     {
     }
