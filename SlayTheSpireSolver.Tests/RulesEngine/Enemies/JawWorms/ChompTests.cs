a8693740ffe7b3d08cb6430ca33ecf96aaa2fab0
cvennevik <cvennevik@gmail.com>
2022-06-25 15:31:29 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWorms/ChompTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWorms/ChompTests.cs
@@ -1,9 +1,7 @@
 ﻿using NUnit.Framework;
-using SlayTheSpireSolver.RulesEngine;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
 using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
-using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Enemies.JawWorms;
 
@@ -11,36 +9,6 @@ namespace SlayTheSpireSolver.Tests.RulesEngine.Enemies.JawWorms;
 public class ChompTests
 {
     private Chomp Chomp { get; } = new();
-
-    [Test]
-    [TestCase(70, 58)]
-    [TestCase(12, 0)]
-    [TestCase(5, -7)]
-    public void TestWithoutArmor(int initialPlayerHealth, int expectedPlayerHealth)
-    {
-        var gameState = new GameState { PlayerHealth = initialPlayerHealth, PlayerArmor = 0 };
-        var resolvedGameState = Chomp.Resolve(gameState);
-        var expectedGameState = new GameState { PlayerHealth = expectedPlayerHealth, PlayerArmor = 0 };
-        Assert.AreEqual(expectedGameState, resolvedGameState);
-    }
-
-    [Test]
-    public void TestWithArmor1()
-    {
-        var gameState = new GameState { PlayerHealth = 50, PlayerArmor = 10 };
-        var resolvedGameState = Chomp.Resolve(gameState);
-        var expectedGameState = new GameState { PlayerHealth = 48, PlayerArmor = 0 };
-        Assert.AreEqual(expectedGameState, resolvedGameState);
-    }
-
-    [Test]
-    public void TestWithArmor2()
-    {
-        var gameState = new GameState { PlayerHealth = 50, PlayerArmor = 20 };
-        var resolvedGameState = Chomp.Resolve(gameState);
-        var expectedGameState = new GameState { PlayerHealth = 50, PlayerArmor = 8 };
-        Assert.AreEqual(expectedGameState, resolvedGameState);
-    }
 
     [Test]
     public void TestEffects()
