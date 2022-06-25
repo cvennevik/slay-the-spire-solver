1a4b4ead76b1738339a3f01d72f5c66265399b11
cvennevik <cvennevik@gmail.com>
2022-06-25 15:50:40 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/AddEnemyStrengthEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/AddEnemyStrengthEffectTests.cs
@@ -54,21 +54,16 @@ public class AddEnemyStrengthEffectTests
     [Test]
     public void AddsStrengthToEnemyWithExistingStrength()
     {
-        var targetId = EnemyId.New();
-        var otherEnemyId = EnemyId.New();
         var gameState = new GameState
         {
-            EnemyParty = new EnemyParty(new JawWorm { Id = targetId },
-                new JawWorm { Id = otherEnemyId })
+            EnemyParty = new EnemyParty(new JawWorm {Strength = 4})
         };
-        var effect = new AddEnemyStrengthEffect(targetId, 5);
+        var effect = new AddEnemyStrengthEffect(EnemyId.Default, 5);
         var result = effect.Resolve(gameState).SingleResolvedGameState();
         var expectedResult = new GameState
         {
-            EnemyParty = new EnemyParty(new JawWorm { Id = targetId, Strength = 5 },
-                new JawWorm { Id = otherEnemyId })
+            EnemyParty = new EnemyParty(new JawWorm {Strength = 9})
         };
-        Assert.AreEqual(expectedResult, result);
     }
 
     [Test]
