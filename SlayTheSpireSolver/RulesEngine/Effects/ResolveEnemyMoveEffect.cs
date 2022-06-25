88ec6abb91a8187084854654a74119b5be6b5735
cvennevik <cvennevik@gmail.com>
2022-06-25 08:56:14 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ResolveEnemyMoveEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ResolveEnemyMoveEffect.cs
@@ -1,6 +1,13 @@
+using SlayTheSpireSolver.RulesEngine.Enemies;
+
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public record ResolveEnemyMoveEffect
 {
-    
+    private EnemyId Id { get; }
+
+    public ResolveEnemyMoveEffect(EnemyId enemyId)
+    {
+        Id = enemyId;
+    }
 }
