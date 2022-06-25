0cf17b043ae026fd68a19f2d146ca8efd905549e
cvennevik <cvennevik@gmail.com>
2022-06-25 13:03:46 +0200

    working

--- SlayTheSpireSolver/RulesEngine/ResolvableGameStateSet.cs
+++ SlayTheSpireSolver/RulesEngine/ResolvableGameStateSet.cs
@@ -1,1 +1,8 @@
+using System.Collections.ObjectModel;
+
 namespace SlayTheSpireSolver.RulesEngine;
+
+public class ResolvableGameStateSet : ReadOnlyCollection<ResolvableGameState>
+{
+    public ResolvableGameStateSet(IList<ResolvableGameState> list) : base(list) { }
+}
