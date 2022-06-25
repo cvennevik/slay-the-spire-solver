d318972dc549483177ce31535c98d0a3e2c2e869
cvennevik <cvennevik@gmail.com>
2022-06-25 15:37:21 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DamageEnemyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DamageEnemyEffect.cs
@@ -36,12 +36,9 @@ public record DamageEnemyEffect : IEffect
 
     private static Enemy DamageEnemy(Enemy enemy, Damage damage)
     {
-        if (damage > enemy.Armor)
-        {
-            var remainingDamage = damage - enemy.Armor;
-            return enemy with { Armor = 0, Health = enemy.Health - remainingDamage };
-        }
+        if (damage <= enemy.Armor) return enemy with { Armor = enemy.Armor - damage };
+        var remainingDamage = damage - enemy.Armor;
+        return enemy with { Armor = 0, Health = enemy.Health - remainingDamage };
 
-        return enemy with { Armor = enemy.Armor - damage };
     }
 }
