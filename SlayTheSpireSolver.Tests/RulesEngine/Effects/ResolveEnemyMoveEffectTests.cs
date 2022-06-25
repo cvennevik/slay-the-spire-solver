7ce42987615a8a6221a61b87a9008ca8d140efc9
cvennevik <cvennevik@gmail.com>
2022-06-25 09:00:07 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/ResolveEnemyMoveEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/ResolveEnemyMoveEffectTests.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 
@@ -11,7 +12,7 @@ public class ResolveEnemyMoveEffectTests
     [Test]
     public void Test()
     {
-        var gameState = new GameState { EnemyParty = new EnemyParty() };
+        var gameState = new GameState { EnemyParty = new EnemyParty(new JawWorm()) };
     }
 
     [Test]
