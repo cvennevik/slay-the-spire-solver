b02bb86b6d6bb5824f070696b443b7dbac008179
cvennevik <cvennevik@gmail.com>
2022-06-25 09:43:07 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ClearEnemyArmorEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ClearEnemyArmorEffect.cs
@@ -1,3 +1,9 @@
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record ClearEnemyArmorEffect();
+public record ClearEnemyArmorEffect : IEffect
+{
+    public IReadOnlyCollection<GameStateWithEffectStack> Resolve(GameState gameState)
+    {
+        throw new NotImplementedException();
+    }
+}
