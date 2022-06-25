f26b4771be560ea87bc32e0089cd0485b473c872
cvennevik <cvennevik@gmail.com>
2022-06-25 15:32:37 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWorms/ThrashTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWorms/ThrashTests.cs
@@ -10,67 +10,6 @@ namespace SlayTheSpireSolver.Tests.RulesEngine.Enemies.JawWorms;
 public class ThrashTests
 {
     [Test]
-    [TestCase(70, 63, 0, 5)]
-    [TestCase(7, 0, 2, 7)]
-    [TestCase(5, -2, 5, 10)]
-    public void TestPlayerWithoutArmor(int initialPlayerHealth, int expectedPlayerHealth, int initialEnemyArmor,
-        int expectedEnemyArmor)
-    {
-        var gameState = new GameState
-        {
-            PlayerHealth = initialPlayerHealth,
-            PlayerArmor = 0,
-            EnemyParty = new EnemyParty(new JawWorm {Armor = initialEnemyArmor})
-        };
-        var resolvedGameState = new Thrash().Resolve(gameState);
-        var expectedGameState = new GameState
-        {
-            PlayerHealth = expectedPlayerHealth,
-            PlayerArmor = 0,
-            EnemyParty = new EnemyParty(new JawWorm {Armor = expectedEnemyArmor})
-        };
-        Assert.AreEqual(expectedGameState, resolvedGameState);
-    }
-
-    [Test]
-    public void TestPlayerWithMoreArmorThanDamage()
-    {
-        var gameState = new GameState
-        {
-            PlayerHealth = 10,
-            PlayerArmor = 10,
-            EnemyParty = new EnemyParty(new JawWorm {Armor = 0})
-        };
-        var resolvedGameState = new Thrash().Resolve(gameState);
-        var expectedGameState = new GameState
-        {
-            PlayerHealth = 10,
-            PlayerArmor = 3,
-            EnemyParty = new EnemyParty(new JawWorm {Armor = 5})
-        };
-        Assert.AreEqual(expectedGameState, resolvedGameState);
-    }
-
-    [Test]
-    public void TestPlayerWithLessArmorThanDamage()
-    {
-        var gameState = new GameState
-        {
-            PlayerHealth = 10,
-            PlayerArmor = 5,
-            EnemyParty = new EnemyParty(new JawWorm {Armor = 0})
-        };
-        var resolvedGameState = new Thrash().Resolve(gameState);
-        var expectedGameState = new GameState
-        {
-            PlayerHealth = 8,
-            PlayerArmor = 0,
-            EnemyParty = new EnemyParty(new JawWorm {Armor = 5})
-        };
-        Assert.AreEqual(expectedGameState, resolvedGameState);
-    }
-
-    [Test]
     public void TestEffects()
     {
         var enemyId = EnemyId.New();
