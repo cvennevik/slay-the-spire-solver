67ad14e0bba3113881856c163a8d373e45482e75
cvennevik <cvennevik@gmail.com>
2022-07-02 17:26:01 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/Enemy.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/Enemy.cs
@@ -11,7 +11,7 @@ public abstract record Enemy
     public Health Health { get; init; } = 1;
     public Armor Armor { get; init; } = 0;
     public Strength Strength { get; init; } = 0;
-    public Vulnerable Vulnerable { get; init; } = new(0);
+    public Vulnerable Vulnerable { get; init; } = 0;
     public EnemyMoveHistory PreviousMoves = new();
     public abstract IEnemyMove IntendedMove { get; init; }
     public abstract IReadOnlyCollection<(IEnemyMove, Probability)> GetNextPossibleMoves();
