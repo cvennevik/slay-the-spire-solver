d95d956aa417c3628f613bbe7e528d763cbffbf0
cvennevik <cvennevik@gmail.com>
2022-07-02 19:22:50 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ResolveForAllEnemiesEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ResolveForAllEnemiesEffect.cs
@@ -1,3 +1,8 @@
+using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
+using SlayTheSpireSolver.TestHelpers;
+
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public record ResolveForAllEnemiesEffect<T> : Effect where T : TargetEnemyEffect, new()
@@ -7,5 +12,55 @@ public record ResolveForAllEnemiesEffect<T> : Effect where T : TargetEnemyEffect
         var resolveEnemyMoveEffects =
             gameState.EnemyParty.Select(enemy => new T {Target = enemy.Id}).Reverse();
         return gameState.WithEffects(new EffectStack(resolveEnemyMoveEffects));
+    }
+}
+
+
+[TestFixture]
+public class ResolveForAllEnemiesEffectTests
+{
+    [TestFixture]
+    public class ResolveAllEnemyMovesEffectTest : ResolveForAllEnemiesEffectTestBase<ResolveEnemyMoveEffect> { }
+
+    [TestFixture]
+    public class ChooseAllNewEnemyMovesEffectTest : ResolveForAllEnemiesEffectTestBase<ChooseNewEnemyMoveEffect> { }
+
+    [TestFixture]
+    public class DecreaseAllEnemiesVulnerableEffectTest :
+        ResolveForAllEnemiesEffectTestBase<DecreaseEnemyVulnerableEffect> { }
+}
+
+public abstract class ResolveForAllEnemiesEffectTestBase<T> where T : TargetEnemyEffect, new()
+{
+    [Test]
+    public void DoesNothingWhenNoEnemies()
+    {
+        var gameState = new GameState();
+        var effect = new ResolveForAllEnemiesEffect<T>();
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
+    }
+
+    [Test]
+    public void ResolvesSingleEnemyEffect()
+    {
+        var enemy = new JawWorm { Id = EnemyId.New() };
+        var gameState = new GameState { EnemyParty = new EnemyParty(enemy) };
+        var effect = new ResolveForAllEnemiesEffect<T>();
+        var result = effect.Resolve(gameState).SingleUnresolvedState();
+        Assert.AreEqual(gameState.WithEffects(new EffectStack(new T { Target = enemy.Id })), result);
+    }
+
+    [Test]
+    public void ResolvesMultipleEnemyEffectsInOrder()
+    {
+        var enemy1 = new JawWorm { Id = EnemyId.New() };
+        var enemy2 = new JawWorm { Id = EnemyId.New() };
+        var gameState = new GameState { EnemyParty = new EnemyParty(enemy1, enemy2) };
+        var effect = new ResolveForAllEnemiesEffect<T>();
+        var result = effect.Resolve(gameState).SingleUnresolvedState();
+        Assert.AreEqual(
+            gameState.WithEffects(new EffectStack(new T { Target = enemy2.Id }, new T { Target = enemy1.Id })),
+            result);
     }
 }
