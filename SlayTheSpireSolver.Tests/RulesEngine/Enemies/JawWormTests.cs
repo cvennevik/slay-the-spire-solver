85edd4729956d4cc52b15eefa4537c97e6593b5e
cvennevik <cvennevik@gmail.com>
2022-07-02 12:46:51 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWormTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWormTests.cs
@@ -1,6 +1,13 @@
+using NUnit.Framework;
+
 namespace SlayTheSpireSolver.Tests.RulesEngine.Enemies;
 
+[TestFixture]
 public class JawWormTests
 {
-    
+    [Test]
+    public void Test()
+    {
+        
+    }
 }
