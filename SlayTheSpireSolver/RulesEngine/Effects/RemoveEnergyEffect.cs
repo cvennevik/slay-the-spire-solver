28f47be69ad671fb01f7d73eca0b90e2de0d7561
cvennevik <cvennevik@gmail.com>
2022-07-02 18:15:30 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/RemoveEnergyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/RemoveEnergyEffect.cs
@@ -2,10 +2,10 @@ using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record RemoveEnergyEffect(Energy _energyToRemove) : Effect
+public record RemoveEnergyEffect(Energy EnergyToRemove) : Effect
 {
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
-        return gameState with { Energy = gameState.Energy - _energyToRemove };
+        return gameState with { Energy = gameState.Energy - EnergyToRemove };
     }
 }
