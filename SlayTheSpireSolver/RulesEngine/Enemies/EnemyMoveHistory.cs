952879e5e7608bbb3591207a5db45dccdc33a390
cvennevik <cvennevik@gmail.com>
2022-07-02 12:35:51 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/EnemyMoveHistory.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/EnemyMoveHistory.cs
@@ -8,4 +8,17 @@ public class EnemyMoveHistory
     {
         _moves = moves;
     }
+
+    public static bool operator ==(EnemyMoveHistory a, EnemyMoveHistory b) => a.Equals(b);
+    public static bool operator !=(EnemyMoveHistory a, EnemyMoveHistory b) => !a.Equals(b);
+    public override bool Equals(object? obj)
+    {
+        if (obj is not EnemyMoveHistory otherHistory) return false;
+        return _moves.SequenceEqual(otherHistory._moves);
+    }
+
+    public override int GetHashCode()
+    {
+        return 0;
+    }
 }
