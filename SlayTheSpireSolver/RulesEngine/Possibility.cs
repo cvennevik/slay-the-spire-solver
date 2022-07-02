2d64535af3e08e958072b8cfed9abf94d42b2e57
cvennevik <cvennevik@gmail.com>
2022-07-02 11:37:09 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Possibility.cs
+++ SlayTheSpireSolver/RulesEngine/Possibility.cs
@@ -5,4 +5,9 @@ namespace SlayTheSpireSolver.RulesEngine;
 public record Possibility(GameState GameState, Probability Probability)
 {
     public static implicit operator Possibility(GameState gameState) => new(gameState, new Probability(1));
+
+    public bool IsEqualTo(Possibility other, double tolerance = double.Epsilon)
+    {
+        return GameState == other.GameState && Probability.IsEqualTo(other.Probability, tolerance);
+    }
 }
