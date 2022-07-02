636c9b97eca87d3a4b70309018d726b899565b88
cvennevik <cvennevik@gmail.com>
2022-07-02 18:13:57 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/MoveHandToDiscardPileEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/MoveHandToDiscardPileEffect.cs
@@ -1,8 +1,8 @@
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record MoveHandToDiscardPileEffect : IEffect
+public record MoveHandToDiscardPileEffect : Effect
 {
-    public ResolvablePossibilitySet Resolve(GameState gameState)
+    public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         var newDiscardPile = gameState.Hand.Cards.Aggregate(
             gameState.DiscardPile, (current, card) => current.Add(card));
