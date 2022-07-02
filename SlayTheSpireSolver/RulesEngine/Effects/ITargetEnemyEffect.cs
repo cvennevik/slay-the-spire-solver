0272231fd4f73311144d58c0f928a2b671f3049d
cvennevik <cvennevik@gmail.com>
2022-07-02 15:28:33 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ITargetEnemyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ITargetEnemyEffect.cs
@@ -4,10 +4,12 @@ namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public abstract record TargetEnemyEffect : IEffect
 {
+    public EnemyId Target { get; init; }
+
     protected TargetEnemyEffect(EnemyId Target)
     {
         this.Target = Target;
     }
+
     public abstract ResolvablePossibilitySet Resolve(GameState gameState);
-    public EnemyId Target { get; init; }
 }
