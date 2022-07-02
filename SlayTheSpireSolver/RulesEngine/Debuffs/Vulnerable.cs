afaa3e9cae7b6e448154d01e0888ebf758f1ed04
cvennevik <cvennevik@gmail.com>
2022-07-02 17:31:37 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Debuffs/Vulnerable.cs
+++ SlayTheSpireSolver/RulesEngine/Debuffs/Vulnerable.cs
@@ -12,4 +12,9 @@ public record Vulnerable
 
     public static implicit operator Vulnerable(int duration) =>
         duration > 0 ? new Vulnerable(duration) : new Vulnerable(0);
+
+    public Vulnerable Decrease()
+    {
+        return new Vulnerable(Duration - 1);
+    }
 }
