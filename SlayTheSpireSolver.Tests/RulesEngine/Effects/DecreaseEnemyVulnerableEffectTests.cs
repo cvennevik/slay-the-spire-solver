3a3c01b4fbf1c08cd166c401792f98c4331c7669
cvennevik <cvennevik@gmail.com>
2022-07-02 17:23:59 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/DecreaseEnemyVulnerableEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/DecreaseEnemyVulnerableEffectTests.cs
@@ -14,5 +14,6 @@ public class DecreaseEnemyVulnerableEffectTests
         var gameState = new GameState { Turn = 3 };
         var effect = new DecreaseEnemyVulnerableEffect(EnemyId.Default);
         var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
     }
 }
