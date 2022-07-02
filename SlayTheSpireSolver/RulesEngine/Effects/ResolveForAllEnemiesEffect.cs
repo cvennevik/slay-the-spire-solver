2ef505eeddfc1c9a303c37b74a8c2f47bff0e0c6
cvennevik <cvennevik@gmail.com>
2022-07-02 18:15:42 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ResolveForAllEnemiesEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ResolveForAllEnemiesEffect.cs
@@ -1,8 +1,8 @@
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record ResolveForAllEnemiesEffect<T> : IEffect where T : TargetEnemyEffect, new()
+public record ResolveForAllEnemiesEffect<T> : Effect where T : TargetEnemyEffect, new()
 {
-    public ResolvablePossibilitySet Resolve(GameState gameState)
+    public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         var resolveEnemyMoveEffects =
             gameState.EnemyParty.Select(enemy => new T {Target = enemy.Id}).Reverse();
