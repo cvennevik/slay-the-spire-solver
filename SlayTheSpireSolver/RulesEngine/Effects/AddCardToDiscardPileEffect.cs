fdc532572fdbb632d3a53c9b3e14ee6a11b362fb
cvennevik <cvennevik@gmail.com>
2022-07-02 18:09:46 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/AddCardToDiscardPileEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/AddCardToDiscardPileEffect.cs
@@ -2,11 +2,11 @@ using SlayTheSpireSolver.RulesEngine.Cards;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record AddCardToDiscardPileEffect(Card _cardToAdd) : Effect
+public record AddCardToDiscardPileEffect(Card CardToAdd) : Effect
 {
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
-        var newCardsInDiscardPile = gameState.DiscardPile.Cards.Append(_cardToAdd).ToArray();
+        var newCardsInDiscardPile = gameState.DiscardPile.Cards.Append(CardToAdd).ToArray();
         return gameState with { DiscardPile = new DiscardPile(newCardsInDiscardPile) };
     }
 }
