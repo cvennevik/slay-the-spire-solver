94346639cc512c8d28d15926a301b1e1f10eb180
cvennevik <cvennevik@gmail.com>
2022-07-02 18:14:49 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/RemoveCardFromHandEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/RemoveCardFromHandEffect.cs
@@ -2,12 +2,12 @@ using SlayTheSpireSolver.RulesEngine.Cards;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record RemoveCardFromHandEffect(Card _cardToRemove) : Effect
+public record RemoveCardFromHandEffect(Card CardToRemove) : Effect
 {
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         var cardsInHand = gameState.Hand.Cards.ToList();
-        cardsInHand.Remove(_cardToRemove);
+        cardsInHand.Remove(CardToRemove);
         return gameState with { Hand = new Hand(cardsInHand.ToArray()) };
     }
 }
