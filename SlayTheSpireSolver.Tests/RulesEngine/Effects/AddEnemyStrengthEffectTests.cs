87b3aa19f09256dce51568820ba0bb3c222c25c3
cvennevik <cvennevik@gmail.com>
2022-07-02 18:20:21 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/AddEnemyStrengthEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/AddEnemyStrengthEffectTests.cs
@@ -64,15 +64,4 @@ public class AddEnemyStrengthEffectTests
         };
         Assert.AreEqual(expectedResult, result);
     }
-
-    [Test]
-    public void TestEquality()
-    {
-        Assert.AreEqual(new AddEnemyStrengthEffect(EnemyId.Default, new Strength(1)),
-            new AddEnemyStrengthEffect(EnemyId.Default, new Strength(1)));
-        Assert.AreNotEqual(new AddEnemyStrengthEffect(EnemyId.Default, new Strength(1)),
-            new AddEnemyStrengthEffect(EnemyId.Default, new Strength(2)));
-        Assert.AreNotEqual(new AddEnemyStrengthEffect(EnemyId.Default, new Strength(1)),
-            new AddEnemyStrengthEffect(EnemyId.New(), new Strength(1)));
-    }
 }
