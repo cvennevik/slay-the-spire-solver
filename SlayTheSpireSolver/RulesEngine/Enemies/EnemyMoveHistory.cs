13ae62c1500bee6d370dfda6514c094bc68ad645
cvennevik <cvennevik@gmail.com>
2022-07-02 12:32:17 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/EnemyMoveHistory.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/EnemyMoveHistory.cs
@@ -2,8 +2,10 @@ namespace SlayTheSpireSolver.RulesEngine.Enemies;
 
 public class EnemyMoveHistory
 {
+    private readonly IEnemyMove[] _moves;
+
     public EnemyMoveHistory(params IEnemyMove[] moves)
     {
-        
+        _moves = moves;
     }
 }
