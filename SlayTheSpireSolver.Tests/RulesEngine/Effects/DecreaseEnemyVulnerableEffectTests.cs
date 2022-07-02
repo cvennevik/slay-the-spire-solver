d68f235c1f497497ee5fab88287bdc5a5ac22ccc
cvennevik <cvennevik@gmail.com>
2022-07-02 17:24:51 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/DecreaseEnemyVulnerableEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/DecreaseEnemyVulnerableEffectTests.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 
@@ -11,7 +12,7 @@ public class DecreaseEnemyVulnerableEffectTests
     [Test]
     public void DoesNothingWhenNoEnemyHasTargetId()
     {
-        var gameState = new GameState { Turn = 3 };
+        var gameState = new GameState { EnemyParty = new[] { new JawWorm {Id = EnemyId.New()} } };
         var effect = new DecreaseEnemyVulnerableEffect(EnemyId.Default);
         var result = effect.Resolve(gameState).SingleResolvedState();
         Assert.AreEqual(gameState, result);
