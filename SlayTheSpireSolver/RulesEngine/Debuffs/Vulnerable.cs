48a5c18bbf5537eb3a357c4b407b654b295c7942
cvennevik <cvennevik@gmail.com>
2022-07-02 16:58:50 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Debuffs/Vulnerable.cs
+++ SlayTheSpireSolver/RulesEngine/Debuffs/Vulnerable.cs
@@ -1,3 +1,12 @@
 namespace SlayTheSpireSolver.RulesEngine.Debuffs;
 
-public record Vulnerable();
+public record Vulnerable
+{
+    public int Duration { get; }
+
+    public Vulnerable(int duration)
+    {
+        if (duration < 0) throw new ArgumentOutOfRangeException(nameof(duration));
+        Duration = duration;
+    }
+}
