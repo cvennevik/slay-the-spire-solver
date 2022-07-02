6ce3abb58cbf87e2f8463d31b6f25bd1ea85b303
cvennevik <cvennevik@gmail.com>
2022-07-02 18:13:38 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/KillEnemyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/KillEnemyEffect.cs
@@ -2,15 +2,8 @@ using SlayTheSpireSolver.RulesEngine.Enemies;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
-public record KillEnemyEffect : IEffect
+public record KillEnemyEffect(EnemyId _targetId) : IEffect
 {
-    private readonly EnemyId _targetId;
-
-    public KillEnemyEffect(EnemyId targetId)
-    {
-        _targetId = targetId;
-    }
-
     public ResolvablePossibilitySet Resolve(GameState gameState)
     {
         return gameState with { EnemyParty = gameState.EnemyParty.Remove(_targetId) };
