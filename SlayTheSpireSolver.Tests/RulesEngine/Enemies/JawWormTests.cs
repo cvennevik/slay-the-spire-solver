04f76b5cf04514cebdf1e0237b920252f60eb608
cvennevik <cvennevik@gmail.com>
2022-07-02 12:51:31 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWormTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Enemies/JawWormTests.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
+using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.Tests.RulesEngine.Enemies;
 
@@ -11,5 +13,6 @@ public class JawWormTests
     {
         var jawWorm = new JawWorm();
         var nextPossibleMoves = jawWorm.GetNextPossibleMoves();
+        Assert.AreEqual((new Chomp(), new Probability(1)), nextPossibleMoves.Single());
     }
 }
