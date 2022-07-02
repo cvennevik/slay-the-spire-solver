0c1b58de48208b8b367919cccda3d5fea8f0292e
cvennevik <cvennevik@gmail.com>
2022-07-02 14:46:26 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ChooseNewEnemyMoveEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ChooseNewEnemyMoveEffect.cs
@@ -7,6 +7,9 @@ public record ChooseNewEnemyMoveEffect(EnemyId Target) : IEffect
     public ResolvablePossibilitySet Resolve(GameState gameState)
     {
         if (!gameState.EnemyParty.Has(Target)) return gameState;
+
+        var possibleMoves = gameState.EnemyParty.Get(Target);
+
         return gameState;
     }
 }
