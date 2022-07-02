45b1d90e96c67feaadbc5022b9fc959f9b2b765c
cvennevik <cvennevik@gmail.com>
2022-07-02 13:23:39 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/EnemyMoveHistory.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/EnemyMoveHistory.cs
@@ -1,10 +1,10 @@
 namespace SlayTheSpireSolver.RulesEngine.Enemies;
 
-public class EnemyMoveHistory : List<IEnemyMove>
+public class EnemyMoveHistory
 {
     private readonly List<IEnemyMove> _moves;
 
-    public EnemyMoveHistory(params IEnemyMove[] moves) : base(moves)
+    public EnemyMoveHistory(params IEnemyMove[] moves)
     {
         _moves = moves.ToList();
     }
@@ -15,7 +15,7 @@ public class EnemyMoveHistory : List<IEnemyMove>
     public static bool operator !=(EnemyMoveHistory a, EnemyMoveHistory b) => !a.Equals(b);
     public override bool Equals(object? obj)
     {
-        return obj is EnemyMoveHistory otherHistory && this.SequenceEqual(otherHistory);
+        return obj is EnemyMoveHistory otherHistory && this._moves.SequenceEqual(otherHistory._moves);
     }
 
     public override int GetHashCode()
