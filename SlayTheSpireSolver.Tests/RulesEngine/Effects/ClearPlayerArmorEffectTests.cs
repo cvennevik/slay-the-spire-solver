32666aa829ae4798091d75a332cf0b795bd32a1f
cvennevik <cvennevik@gmail.com>
2022-07-02 11:57:02 +0200

    working

--- SlayTheSpireSolver.Tests/RulesEngine/Effects/ClearPlayerArmorEffectTests.cs
+++ SlayTheSpireSolver.Tests/RulesEngine/Effects/ClearPlayerArmorEffectTests.cs
@@ -1,6 +1,13 @@
+using NUnit.Framework;
+
 namespace SlayTheSpireSolver.Tests.RulesEngine.Effects;
 
+[TestFixture]
 public class ClearPlayerArmorEffectTests
 {
-    
+    [Test]
+    public void Test()
+    {
+        
+    }
 }
