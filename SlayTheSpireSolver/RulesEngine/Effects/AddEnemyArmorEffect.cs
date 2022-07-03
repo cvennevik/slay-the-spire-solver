c0da71ba979fc088263054d0b0b2e3c4c224eb42
cvennevik <cvennevik@gmail.com>
2022-07-03 09:51:42 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/AddEnemyArmorEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/AddEnemyArmorEffect.cs
@@ -1,5 +1,8 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 using SlayTheSpireSolver.RulesEngine.Values;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -9,5 +12,67 @@ public record AddEnemyArmorEffect(EnemyId EnemyId, Armor ArmorGain) : Effect
     {
         return gameState.ModifyEnemy(EnemyId,
             enemy => enemy with { Armor = enemy.Armor + ArmorGain });
+    }
+}
+
+[TestFixture]
+public class GainEnemyArmorEffectTests
+{
+    [Test]
+    public void AddsEnemyArmor()
+    {
+        var effect = new AddEnemyArmorEffect(EnemyId.Default, new Armor(5));
+        var gameState = new GameState { EnemyParty = new EnemyParty(new JawWorm()) };
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        var expectedGameState = new GameState { EnemyParty = new EnemyParty(new JawWorm { Armor = 5 }) };
+        Assert.AreEqual(expectedGameState, result);
+    }
+
+    [Test]
+    public void AddsToExistingEnemyArmor()
+    {
+        var effect = new AddEnemyArmorEffect(EnemyId.Default, new Armor(5));
+        var gameState = new GameState { EnemyParty = new EnemyParty(new JawWorm {Armor = 5}) };
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        var expectedGameState = new GameState { EnemyParty = new EnemyParty(new JawWorm { Armor = 10 }) };
+        Assert.AreEqual(expectedGameState, result);
+    }
+
+    [Test]
+    public void OnlyAddsArmorToTargetEnemy()
+    {
+        var targetEnemyId = EnemyId.New();
+        var otherEnemyId = EnemyId.New();
+        var effect = new AddEnemyArmorEffect(targetEnemyId, new Armor(5));
+        var gameState = new GameState
+        {
+            EnemyParty = new EnemyParty(new JawWorm { Id = targetEnemyId },
+                new JawWorm { Id = otherEnemyId })
+        };
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        var expectedGameState = new GameState
+        {
+            EnemyParty = new EnemyParty(new JawWorm { Id = targetEnemyId, Armor = new Armor(5) },
+                new JawWorm { Id = otherEnemyId })
+        };
+        Assert.AreEqual(expectedGameState, result);
+    }
+
+    [Test]
+    public void DoesNothingWhenNoEnemies()
+    {
+        var effect = new AddEnemyArmorEffect(EnemyId.Default, 1);
+        var gameState = new GameState { Turn = 2 };
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
+    }
+
+    [Test]
+    public void DoesNothingWhenNoEnemyWithTargetId()
+    {
+        var effect = new AddEnemyArmorEffect(EnemyId.Default, 5);
+        var gameState = new GameState { EnemyParty = new EnemyParty(new JawWorm { Id = EnemyId.New() }) };
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
     }
 }
