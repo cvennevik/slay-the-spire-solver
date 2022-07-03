9f6beefc2c85014a6a1e8b5b5360d118da28fa29
cvennevik <cvennevik@gmail.com>
2022-07-03 15:25:55 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -1,15 +1,15 @@
 ﻿using SlayTheSpireSolver.RulesEngine;
 using SlayTheSpireSolver.RulesEngine.Effects;
-using Action = SlayTheSpireSolver.RulesEngine.Action;
+using PlayerAction = SlayTheSpireSolver.RulesEngine.Action;
 
 namespace SlayTheSpireSolver.AI;
 
 public static class Solver
 {
-    public static Action GetBestAction(GameState gameState)
+    public static PlayerAction GetBestAction(GameState gameState)
     {
         var legalActions = gameState.GetLegalActions();
-        var endTurnAction = new Action(gameState, new EndTurnEffect());
+        var endTurnAction = new PlayerAction(gameState, new EndTurnEffect());
         var actionsExceptEndTurn = legalActions.Except(new[] { endTurnAction }).ToList();
         if (actionsExceptEndTurn.Any())
         {
