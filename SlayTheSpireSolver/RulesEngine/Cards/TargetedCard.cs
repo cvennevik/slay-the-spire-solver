aaa0c5b885087ccab0ab92c224f56bbf1db5214f
cvennevik <cvennevik@gmail.com>
2022-07-03 16:21:36 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/TargetedCard.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/TargetedCard.cs
@@ -49,8 +49,8 @@ internal abstract class TargetedCardTests<TCard> : CardTests<TCard> where TCard 
         var gameState = BasicGameState with { EnemyParty = new[] { enemy1, enemy2, enemy3 } };
 
         var expectedAction1 = new PlayTargetedCardAction(gameState, Card, enemy1.Id);
-        var expectedAction2 = new PlayTargetedCardAction(gameState, GetExpectedEffectStack(enemy2.Id));
-        var expectedAction3 = new PlayTargetedCardAction(gameState, GetExpectedEffectStack(enemy3.Id));
+        var expectedAction2 = new PlayTargetedCardAction(gameState, Card, enemy2.Id);
+        var expectedAction3 = new PlayTargetedCardAction(gameState, Card, enemy3.Id);
         var expectedActions = new[] { expectedAction1, expectedAction2, expectedAction3 };
         Assert.That(Card.GetLegalActions(gameState), Is.EquivalentTo(expectedActions));
     }
