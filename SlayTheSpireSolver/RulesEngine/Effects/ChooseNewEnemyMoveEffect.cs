35ea5943fb4360729cc01ae37d28127573b6750d
cvennevik <cvennevik@gmail.com>
2022-07-03 12:52:39 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ChooseNewEnemyMoveEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ChooseNewEnemyMoveEffect.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -21,5 +24,34 @@ public record ChooseNewEnemyMoveEffect : TargetEnemyEffect
         ).ToArray();
 
         return result;
+    }
+}
+
+[TestFixture]
+public class ChooseNewEnemyMoveEffectTests
+{
+    [Test]
+    public void DoesNothingWhenNoEnemyHasTargetId()
+    {
+        var gameState = new GameState { EnemyParty = new[] { new JawWorm { IntendedMove = new Chomp() } } };
+        var effect = new ChooseNewEnemyMoveEffect(EnemyId.New());
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
+    }
+
+    [Test]
+    public void AddsIntendedMoveToPreviousMovesAndSetsNewIntendedMove()
+    {
+        var gameState = new GameState { EnemyParty = new[] { new JawWorm { IntendedMove = new Chomp() } } };
+        var effect = new ChooseNewEnemyMoveEffect(EnemyId.Default);
+        var result = effect.Resolve(gameState);
+        var bellowEnemy = new JawWorm { IntendedMove = new Bellow(), PreviousMoves = new[] { new Chomp() } };
+        var thrashEnemy = new JawWorm { IntendedMove = new Thrash(), PreviousMoves = new[] { new Chomp() } };
+        var expectedPossibilities = new ResolvablePossibility[]
+        {
+            (gameState with { EnemyParty = new[] { bellowEnemy } }).WithProbability(0.45 / 0.75),
+            (gameState with { EnemyParty = new[] { thrashEnemy } }).WithProbability(0.3 / 0.75)
+        };
+        Assert.That(result, Is.EquivalentTo(expectedPossibilities));
     }
 }
