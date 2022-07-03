12f4ee2bba5918227f765df61536dda64e09ddac
cvennevik <cvennevik@gmail.com>
2022-07-03 16:32:59 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -8,12 +8,11 @@ public static class Solver
     public static PlayerAction GetBestAction(GameState gameState)
     {
         var legalActions = gameState.GetLegalActions();
-        var endTurnAction = new EndTurnAction(gameState);
-        var actionsExceptEndTurn = legalActions.Except(new[] { endTurnAction }).ToList();
+        var actionsExceptEndTurn = legalActions.Where(x => x is not EndTurnAction).ToList();
         if (actionsExceptEndTurn.Any())
         {
             return actionsExceptEndTurn.First();
         }
-        return endTurnAction;
+        return legalActions.First();
     }
 }
