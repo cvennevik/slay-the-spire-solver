a4699360f478529bfb9ee2332f377515e9254ee3
cvennevik <cvennevik@gmail.com>
2022-07-03 12:58:26 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/RemoveEnergyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/RemoveEnergyEffect.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Values;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -7,5 +9,23 @@ public record RemoveEnergyEffect(Energy EnergyToRemove) : Effect
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         return gameState with { Energy = gameState.Energy - EnergyToRemove };
+    }
+}
+
+[TestFixture]
+public class RemoveEnergyEffectTests
+{
+    [Test]
+    [TestCase(0, 0, 0)]
+    [TestCase(1, 0, 1)]
+    [TestCase(0, 1, 0)]
+    [TestCase(1, 1, 0)]
+    [TestCase(2, 1, 1)]
+    public void TestEffect(int initialEnergy, int effectAmount, int expectedEnergy)
+    {
+        var gameState = new GameState { Energy = initialEnergy };
+        var effect = new RemoveEnergyEffect(effectAmount);
+        var expectedGameState = new GameState { Energy = expectedEnergy };
+        Assert.AreEqual(expectedGameState, effect.Resolve(gameState).SingleResolvedState());
     }
 }
