959bb9a2e22af926c846f440cb1f04da1ed8d1b7
cvennevik <cvennevik@gmail.com>
2022-07-03 13:02:51 +0200

    working

--- SlayTheSpireSolver/RulesEngine/DiscardPile.cs
+++ SlayTheSpireSolver/RulesEngine/DiscardPile.cs
@@ -1,4 +1,5 @@
-﻿using SlayTheSpireSolver.RulesEngine.Cards;
+﻿using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Cards;
 
 namespace SlayTheSpireSolver.RulesEngine;
 
@@ -11,5 +12,79 @@ public class DiscardPile : CardCollection<DiscardPile>
     protected override DiscardPile CreateNew(params Card[] cards)
     {
         return new DiscardPile(cards);
+    }
+}
+
+[TestFixture]
+public class DiscardPileTests
+{
+    [Test]
+    public void TestEquality1()
+    {
+        Assert.AreEqual(new DiscardPile(), new DiscardPile());
+    }
+
+    [Test]
+    public void TestEquality2()
+    {
+        var discardPile1 = new DiscardPile();
+        var discardPile2 = new DiscardPile(new Strike());
+        Assert.AreNotEqual(discardPile1, discardPile2);
+    }
+
+    [Test]
+    public void TestEquality3()
+    {
+        var discardPile1 = new DiscardPile(new Strike());
+        var discardPile2 = new DiscardPile(new Strike());
+        Assert.AreEqual(discardPile1, discardPile2);
+    }
+
+    [Test]
+    public void TestEquality4()
+    {
+        var discardPile1 = new DiscardPile(new Strike());
+        var discardPile2 = new DiscardPile(new Strike(), new Strike());
+        Assert.AreNotEqual(discardPile1, discardPile2);
+    }
+
+    [Test]
+    public void TestEquality5()
+    {
+        var discardPile1 = new DiscardPile(new Strike(), new Strike());
+        var discardPile2 = new DiscardPile(new Strike(), new Strike());
+        Assert.AreEqual(discardPile1, discardPile2);
+    }
+
+    [Test]
+    public void TestEquality6()
+    {
+        var discardPile1 = new DiscardPile(new Strike(), new Strike());
+        var discardPile2 = new DiscardPile(new Strike(), new Defend());
+        Assert.AreNotEqual(discardPile1, discardPile2);
+    }
+
+    [Test]
+    public void TestEquality7()
+    {
+        var discardPile1 = new DiscardPile(new Strike(), new Strike());
+        var discardPile2 = new DiscardPile(new Strike(), new Strike(), new Defend());
+        Assert.AreNotEqual(discardPile1, discardPile2);
+    }
+
+    [Test]
+    public void TestEquality8()
+    {
+        var discardPile1 = new DiscardPile(new Strike(), new Defend());
+        var discardPile2 = new DiscardPile(new Strike(), new Defend());
+        Assert.AreEqual(discardPile1, discardPile2);
+    }
+
+    [Test]
+    public void TestEquality9()
+    {
+        var discardPile1 = new DiscardPile(new Defend(), new Strike());
+        var discardPile2 = new DiscardPile(new Strike(), new Defend());
+        Assert.AreEqual(discardPile1, discardPile2);
     }
 }
