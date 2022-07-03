44176b2f36082a240814476279f0b70b49417f58
cvennevik <cvennevik@gmail.com>
2022-07-03 12:59:19 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/EnemyId.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/EnemyId.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
+
 namespace SlayTheSpireSolver.RulesEngine.Enemies;
 
 public class EnemyId
@@ -24,5 +27,38 @@ public class EnemyId
     public override string ToString()
     {
         return $"{_printedId}";
+    }
+}
+
+[TestFixture]
+public class EnemyIdTests
+{
+    [Test]
+    public void TestEquality()
+    {
+        var enemyId1 = EnemyId.New();
+        var enemyId2 = EnemyId.New();
+        Assert.AreEqual(enemyId1, enemyId1);
+        Assert.AreEqual(enemyId2, enemyId2);
+        Assert.AreNotEqual(enemyId1, enemyId2);
+    }
+
+    [Test]
+    public void NewEnemiesAreEqual()
+    {
+        Assert.AreEqual(new JawWorm(), new JawWorm());
+    }
+
+    [Test]
+    public void NewEnemiesHaveDefaultId()
+    {
+        Assert.AreEqual(EnemyId.Default, new JawWorm().Id);
+        Assert.AreEqual(new JawWorm().Id, new JawWorm().Id);
+    }
+
+    [Test]
+    public void EnemiesWithDifferentIdsAreNotEqual()
+    {
+        Assert.AreNotEqual(new JawWorm { Id = EnemyId.New() }, new JawWorm { Id = EnemyId.New() });
     }
 }
