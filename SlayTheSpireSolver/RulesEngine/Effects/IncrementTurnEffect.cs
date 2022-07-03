f713430cde68ae81d5ce048d49c517a8f344a0c2
cvennevik <cvennevik@gmail.com>
2022-07-03 12:56:21 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/IncrementTurnEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/IncrementTurnEffect.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using SlayTheSpireSolver.TestHelpers;
+
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public record IncrementTurnEffect : Effect
@@ -5,5 +8,21 @@ public record IncrementTurnEffect : Effect
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         return gameState with { Turn = gameState.Turn.Number + 1 };
+    }
+}
+
+[TestFixture]
+public class IncrementTurnEffectTests
+{
+    [Test]
+    [TestCase(1, 2)]
+    [TestCase(2, 3)]
+    public void Test(int initialTurn, int expectedTurn)
+    {
+        var gameState = new GameState { PlayerHealth = 21, Turn = initialTurn };
+        var effect = new IncrementTurnEffect();
+        var newGameState = effect.Resolve(gameState).SingleResolvedState();
+        var expectedGameState = gameState with { Turn = expectedTurn };
+        Assert.AreEqual(expectedGameState, newGameState);
     }
 }
