f5aad4b0744e0ad139eb541efc6658894aabe2e2
cvennevik <cvennevik@gmail.com>
2022-07-03 16:23:19 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Actions/PlayTargetedCardAction.cs
+++ SlayTheSpireSolver/RulesEngine/Actions/PlayTargetedCardAction.cs
@@ -1,14 +1,10 @@
 using SlayTheSpireSolver.RulesEngine.Cards;
-using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
 
 namespace SlayTheSpireSolver.RulesEngine.Actions;
 
 public record PlayTargetedCardAction : PlayCardAction
 {
-    public PlayTargetedCardAction(GameState gameState, EffectStack effectStack) : base(
-        new ResolvableGameState(gameState, effectStack)) { }
-
     public PlayTargetedCardAction(GameState gameState, TargetedCard card, EnemyId target)
         : base(gameState, card, card.GetTargetedEffects(target)) { }
 }
