9517b03d22466c2d83782b33f16e69f91c5131c3
cvennevik <cvennevik@gmail.com>
2022-07-03 13:00:50 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Values/Turn.cs
+++ SlayTheSpireSolver/RulesEngine/Values/Turn.cs
@@ -1,4 +1,6 @@
-﻿namespace SlayTheSpireSolver.RulesEngine.Values;
+﻿using NUnit.Framework;
+
+namespace SlayTheSpireSolver.RulesEngine.Values;
 
 public record Turn
 {
@@ -16,5 +18,57 @@ public record Turn
     public override string ToString()
     {
         return $"{Number}";
+    }
+}
+
+[TestFixture]
+public class TurnTests
+{
+    [Test]
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(-99999)]
+    public void DoesNotPermitNonPositiveTurnNumbers(int turnNumber)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Turn(turnNumber));
+    }
+
+    [Test]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(99999)]
+    public void PermitsPositiveTurnNumbers(int turnNumber)
+    {
+        var turn = new Turn(turnNumber);
+        Assert.AreEqual(turnNumber, turn.Number);
+    }
+
+    [Test]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(10)]
+    public void TestEquality(int turnNumber)
+    {
+        Assert.AreEqual(new Turn(turnNumber), new Turn(turnNumber));
+    }
+
+    
+    [Test]
+    [TestCase(1)]
+    [TestCase(5)]
+    public void ImplicitlyConvertedTurnEqualsExplicitTurn(int turnNumber)
+    {
+        Turn turn = turnNumber;
+        Assert.AreEqual(new Turn(turnNumber), turn);
+        Assert.True(new Turn(turnNumber) == turnNumber);
+        Assert.True(turnNumber == new Turn(turnNumber));
+    }
+
+    [Test]
+    public void ImplicitlyConvertsNonPositiveTurnNumbersToTurnOne()
+    {
+        Assert.True(new Turn(1) == 0);
+        Assert.True(new Turn(1) == -1);
+        Assert.True(new Turn(1) == -5);
     }
 }
