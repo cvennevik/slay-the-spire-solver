889ed3be8f5dbfd03eee67cf1de173cfd9fc107f
cvennevik <cvennevik@gmail.com>
2022-07-03 10:26:48 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/Strike.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Strike.cs
@@ -1,4 +1,5 @@
-﻿using SlayTheSpireSolver.RulesEngine.Effects;
+﻿using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Cards;
@@ -11,4 +12,16 @@ public record Strike : Card
     public override Effect GetEffect(GameState gameState) => new DamageEnemyEffect(gameState.EnemyParty.First().Id, 6);
 
     public override string ToString() => "Strike";
+}
+
+
+[TestFixture]
+public class StrikeTests : CommonCardTests<Strike>
+{
+    [Test]
+    public void TestEffect()
+    {
+        Assert.AreEqual(new DamageEnemyEffect(BasicGameState.EnemyParty.First().Id, 6),
+            Card.GetEffect(BasicGameState));
+    }
 }
