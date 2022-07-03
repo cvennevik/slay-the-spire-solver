13f23f4a805ae99b696f3b4e04277693cd734e77
cvennevik <cvennevik@gmail.com>
2022-07-03 13:29:27 +0200

    working

---
+++ SlayTheSpireSolver/RulesEngine/Cards/Bash.cs
@@ -0,0 +1,18 @@
+using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Values;
+
+namespace SlayTheSpireSolver.RulesEngine.Cards;
+
+public record Bash : TargetedCard
+{
+    public override Energy GetCost()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Effect GetTargetedEffect(EnemyId target)
+    {
+        throw new NotImplementedException();
+    }
+}
