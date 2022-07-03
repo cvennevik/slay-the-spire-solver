5db3aea5dcdd11b2382c6196aec4aea1c20b8a28
cvennevik <cvennevik@gmail.com>
2022-07-03 12:57:31 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/RemoveCardFromHandEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/RemoveCardFromHandEffect.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Cards;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -9,5 +11,33 @@ public record RemoveCardFromHandEffect(Card CardToRemove) : Effect
         var cardsInHand = gameState.Hand.Cards.ToList();
         cardsInHand.Remove(CardToRemove);
         return gameState with { Hand = new Hand(cardsInHand.ToArray()) };
+    }
+}
+
+[TestFixture]
+public class RemoveCardFromHandEffectTests
+{
+    [Test]
+    public void DoesNothingWhenCardNotInHand()
+    {
+        var gameState = new GameState { Hand = new Hand(new Strike()) };
+        var effect = new RemoveCardFromHandEffect(new Defend());
+        Assert.AreEqual(gameState, effect.Resolve(gameState).SingleResolvedState());
+    }
+
+    [Test]
+    public void RemovesSingleCardFromHand()
+    {
+        var gameState = new GameState { Hand = new Hand(new Strike()) };
+        var effect = new RemoveCardFromHandEffect(new Strike());
+        Assert.AreEqual(new GameState { Hand = new Hand() }, effect.Resolve(gameState).SingleResolvedState());
+    }
+
+    [Test]
+    public void RemovesOnlyOneCardWhenMultipleInHand()
+    {
+        var gameState = new GameState { Hand = new Hand(new Strike(), new Strike()) };
+        var effect = new RemoveCardFromHandEffect(new Strike());
+        Assert.AreEqual(new GameState { Hand = new Hand(new Strike()) }, effect.Resolve(gameState).SingleResolvedState());
     }
 }
