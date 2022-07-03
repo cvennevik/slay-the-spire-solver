9049984f5cc54ef7054df2ec5c8fee9afb99b5be
cvennevik <cvennevik@gmail.com>
2022-07-03 12:53:29 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ClearPlayerArmorEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ClearPlayerArmorEffect.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using SlayTheSpireSolver.TestHelpers;
+
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public record ClearPlayerArmorEffect : Effect
@@ -5,5 +8,27 @@ public record ClearPlayerArmorEffect : Effect
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         return gameState with {PlayerArmor = 0};
+    }
+}
+
+[TestFixture]
+public class ClearPlayerArmorEffectTests
+{
+    [Test]
+    public void DoesNothingWhenNoPlayerArmor()
+    {
+        var gameState = new GameState {PlayerArmor = 0, Turn = 3};
+        var effect = new ClearPlayerArmorEffect();
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
+    }
+
+    [Test]
+    public void ClearsPlayerArmor()
+    {
+        var gameState = new GameState {PlayerArmor = 5, Turn = 3};
+        var effect = new ClearPlayerArmorEffect();
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState with {PlayerArmor = 0}, result);
     }
 }
