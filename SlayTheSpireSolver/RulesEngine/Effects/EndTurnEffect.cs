5e09b30ca7aa6560c13d79e6ba25c6ba082e57ca
cvennevik <cvennevik@gmail.com>
2022-07-03 12:55:10 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/EndTurnEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/EndTurnEffect.cs
@@ -1,3 +1,9 @@
+using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Cards;
+using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
+using SlayTheSpireSolver.TestHelpers;
+
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public record EndTurnEffect : Effect
@@ -16,5 +22,57 @@ public record EndTurnEffect : Effect
             new ResolveForAllEnemiesEffect<ResolveEnemyMoveEffect>(),
             new ClearAllEnemyArmorEffect(),
             new MoveHandToDiscardPileEffect()));
+    }
+}
+
+[TestFixture]
+public class EndTurnEffectTests
+{
+    [Test]
+    public void Test()
+    {
+        var gameState = new GameState { Turn = 2 };
+        var effect = new EndTurnEffect();
+        var newGameStateWithEffectStack = effect.Resolve(gameState).SingleUnresolvedState();
+        var expectedEffectStack = new EffectStack(
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new DrawCardEffect(),
+            new RecoverBaseEnergyEffect(),
+            new IncrementTurnEffect(),
+            new ResolveForAllEnemiesEffect<ChooseNewEnemyMoveEffect>(),
+            new ResolveForAllEnemiesEffect<ResolveEnemyMoveEffect>(),
+            new ClearAllEnemyArmorEffect(),
+            new MoveHandToDiscardPileEffect());
+        var expectedResult = new ResolvableGameState(gameState, expectedEffectStack);
+        Assert.AreEqual(expectedResult, newGameStateWithEffectStack);
+    }
+
+    [Test]
+    public void TestResolvingFully()
+    {
+        var gameState = new GameState
+        {
+            BaseEnergy = 3,
+            Energy = 0,
+            PlayerHealth = 50,
+            PlayerArmor = 15,
+            EnemyParty = new[]
+            {
+                new JawWorm { Id = EnemyId.New(), IntendedMove = new Chomp() },
+                new JawWorm { Id = EnemyId.New(), IntendedMove = new Thrash() }
+            },
+            Hand = new Hand(new Strike(), new Defend()),
+            DiscardPile = new DiscardPile(new Strike(), new Strike(), new Strike()),
+            DrawPile = new DrawPile(new Defend(), new Defend(), new Defend(), new Strike())
+        };
+        var result = gameState.WithEffects(new EndTurnEffect()).Resolve();
+        Assert.AreEqual(12, result.Count);
+        Assert.AreEqual(1, result.Select(x => x.Probability.Value).Sum(), double.Epsilon);
+        Assert.AreEqual(12, result.Count(x => x.GameState.EnemyParty.All(enemy => enemy.PreviousMoves.Count == 1)));
+        Assert.AreEqual(12, result.Count(x => x.GameState.Turn == 2));
+        Assert.AreEqual(12, result.Count(x => x.GameState.PlayerHealth == 46));
     }
 }
