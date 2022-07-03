123c8f8afce61333b5cdea37006300f7d8fab7a1
cvennevik <cvennevik@gmail.com>
2022-07-03 12:42:51 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/Strike.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Strike.cs
@@ -8,7 +8,7 @@ namespace SlayTheSpireSolver.RulesEngine.Cards;
 public record Strike : TargetedCard
 {
     public override Energy GetCost() => 1;
-    public override Effect GetEffect(GameState gameState, EnemyId target) => new AttackEnemyEffect(gameState.EnemyParty.First().Id, 6);
+    public override Effect GetEffect(GameState gameState, EnemyId target) => new AttackEnemyEffect(target, 6);
     public override string ToString() => "Strike";
 }
 
