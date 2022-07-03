b354705728245600821fd3730656ee47eab47a00
cvennevik <cvennevik@gmail.com>
2022-07-03 12:55:25 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/GainPlayerArmorEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/GainPlayerArmorEffect.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Values;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -14,5 +16,22 @@ public record GainPlayerArmorEffect : Effect
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         return gameState with { PlayerArmor = gameState.PlayerArmor + _armorGain };
+    }
+}
+
+[TestFixture]
+public class GainPlayerArmorEffectTests
+{
+    [Test]
+    [TestCase(0, 0, 0)]
+    [TestCase(0, 5, 5)]
+    [TestCase(5, 0, 5)]
+    [TestCase(5, 5, 10)]
+    public void TestEffect(int initialPlayerArmor, int effectAmount, int expectedPlayerArmor)
+    {
+        var gameState = new GameState { PlayerArmor = initialPlayerArmor };
+        var effect = new GainPlayerArmorEffect(effectAmount);
+        Assert.AreEqual(new GameState { PlayerArmor = expectedPlayerArmor },
+            effect.Resolve(gameState).SingleResolvedState());
     }
 }
