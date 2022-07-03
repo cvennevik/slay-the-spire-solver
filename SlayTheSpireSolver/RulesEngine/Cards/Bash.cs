c7393483215d315e2aea75386d00075c30ea3189
cvennevik <cvennevik@gmail.com>
2022-07-03 13:43:12 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/Bash.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Bash.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
 using SlayTheSpireSolver.RulesEngine.Values;
@@ -8,10 +9,10 @@ public record Bash : TargetedCard
 {
     public override Energy GetCost() => 2;
 
-    public override EffectStack GetTargetedEffect(EnemyId target)
-    {
-        throw new NotImplementedException();
-    }
-
+    public override EffectStack GetTargetedEffect(EnemyId target) => new(new ApplyVulnerableToEnemyEffect(target, 2),
+        new AttackEnemyEffect(target, 8));
     protected override string GetName() => "Bash";
 }
+
+[TestFixture]
+internal class BashTests : TargetedCardTests<Bash> { }
