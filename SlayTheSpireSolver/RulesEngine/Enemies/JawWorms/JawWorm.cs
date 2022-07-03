a13ce796a99440aed0e4827ccc393d0f6c1ca88a
cvennevik <cvennevik@gmail.com>
2022-07-03 13:00:10 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/JawWorm.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/JawWorm.cs
@@ -1,4 +1,5 @@
-﻿using SlayTheSpireSolver.RulesEngine.Values;
+﻿using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
@@ -53,5 +54,58 @@ public record JawWorm : Enemy
             (new Thrash(), ThrashProbability),
             (new Chomp(), ChompProbability)
         };
+    }
+}
+
+[TestFixture]
+public class JawWormTests
+{
+    [Test]
+    public void AlwaysPicksChompWhenNoPreviousMoves()
+    {
+        var jawWorm = new JawWorm();
+        var nextPossibleMoves = jawWorm.GetNextPossibleMoves();
+        Assert.AreEqual((new Chomp(), new Probability(1)), nextPossibleMoves.Single());
+    }
+
+    [Test]
+    public void PicksAnyMoveAfterSingleThrash()
+    {
+        var jawWorm = new JawWorm { PreviousMoves = new[] { new Thrash() } };
+        var nextPossibleMoves = jawWorm.GetNextPossibleMoves();
+        Assert.AreEqual(3, nextPossibleMoves.Count);
+        Assert.Contains((new Chomp(), new Probability(0.25)), nextPossibleMoves.ToList());
+        Assert.Contains((new Thrash(), new Probability(0.3)), nextPossibleMoves.ToList());
+        Assert.Contains((new Bellow(), new Probability(0.45)), nextPossibleMoves.ToList());
+    }
+
+    [Test]
+    public void DoesNotPickBellowTwiceInARow()
+    {
+        var jawWorm = new JawWorm { PreviousMoves = new[] { new Bellow() } };
+        var nextPossibleMoves = jawWorm.GetNextPossibleMoves();
+        Assert.AreEqual(2, nextPossibleMoves.Count);
+        Assert.Contains((new Chomp(), new Probability(0.25 / 0.55)), nextPossibleMoves.ToList());
+        Assert.Contains((new Thrash(), new Probability(0.3 / 0.55)), nextPossibleMoves.ToList());
+    }
+
+    [Test]
+    public void DoesNotPickChompTwiceInARow()
+    {
+        var jawWorm = new JawWorm { PreviousMoves = new[] { new Chomp() } };
+        var nextPossibleMoves = jawWorm.GetNextPossibleMoves();
+        Assert.AreEqual(2, nextPossibleMoves.Count);
+        Assert.Contains((new Thrash(), new Probability(0.3 / 0.75)), nextPossibleMoves.ToList());
+        Assert.Contains((new Bellow(), new Probability(0.45 / 0.75)), nextPossibleMoves.ToList());
+    }
+
+    [Test]
+    public void DoesNotPickThrashThreeTimesInARow()
+    {
+        var jawWorm = new JawWorm { PreviousMoves = new[] { new Thrash(), new Thrash() } };
+        var nextPossibleMoves = jawWorm.GetNextPossibleMoves();
+        Assert.AreEqual(2, nextPossibleMoves.Count);
+        Assert.Contains((new Chomp(), new Probability(0.25 / 0.7)), nextPossibleMoves.ToList());
+        Assert.Contains((new Bellow(), new Probability(0.45 / 0.7)), nextPossibleMoves.ToList());
     }
 }
