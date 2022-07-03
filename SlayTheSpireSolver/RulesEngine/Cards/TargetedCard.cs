9f21076784dcdc156e44683a698310a1cda70a76
cvennevik <cvennevik@gmail.com>
2022-07-03 12:18:09 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/TargetedCard.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/TargetedCard.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.RulesEngine.Cards;
 
@@ -13,5 +16,20 @@ public abstract record TargetedCard : Card
                 new RemoveCardFromHandEffect(this),
                 new RemoveEnergyEffect(GetCost()))) }
             : Array.Empty<Action>();
+    }
+}
+
+[TestFixture]
+public class TargetedCardTests
+{
+    private static GameState CreateBasicGameState()
+    {
+        return new()
+        {
+            PlayerHealth = 70,
+            Energy = 3,
+            EnemyParty = new EnemyParty(new JawWorm { Health = 40, IntendedMove = new Chomp() }),
+            Hand = new Hand(new Strike())
+        };
     }
 }
