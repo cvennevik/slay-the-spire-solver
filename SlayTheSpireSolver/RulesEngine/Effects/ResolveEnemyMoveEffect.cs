b4ab7b55e80bb724cbb07dd3e522cfa51dcf0285
cvennevik <cvennevik@gmail.com>
2022-07-03 12:58:49 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/ResolveEnemyMoveEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/ResolveEnemyMoveEffect.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -12,5 +15,42 @@ public record ResolveEnemyMoveEffect : TargetEnemyEffect
         if (!gameState.EnemyParty.Has(Target)) return gameState;
         var enemyMoveEffects = gameState.EnemyParty.Get(Target).GetMoveEffects();
         return gameState.WithEffects(enemyMoveEffects);
+    }
+}
+
+[TestFixture]
+public class ResolveEnemyMoveEffectTests
+{
+    [Test]
+    public void PutsEnemyMoveEffectsOnEffectStack()
+    {
+        var enemy = new JawWorm { IntendedMove = new Chomp() };
+        var gameState = new GameState { EnemyParty = new EnemyParty(enemy) };
+        var effect = new ResolveEnemyMoveEffect(enemy.Id);
+        var result = effect.Resolve(gameState).SingleUnresolvedState();
+        var expectedEffectStack = enemy.GetMoveEffects();
+        Assert.AreEqual(gameState.WithEffects(expectedEffectStack), result);
+    }
+
+    [Test]
+    public void OnlyPlacesEffectsOfTargetEnemyOnEffectStack()
+    {
+        var targetEnemy = new JawWorm { Id = EnemyId.New(), IntendedMove = new Thrash() };
+        var otherEnemy = new JawWorm { Id = EnemyId.New(), IntendedMove = new Chomp() };
+        var gameState = new GameState { EnemyParty = new EnemyParty(targetEnemy, otherEnemy) };
+        var effect = new ResolveEnemyMoveEffect(targetEnemy.Id);
+        var result = effect.Resolve(gameState).SingleUnresolvedState();
+        var expectedEffectStack = targetEnemy.GetMoveEffects();
+        Assert.AreEqual(gameState.WithEffects(expectedEffectStack), result);
+    }
+
+    [Test]
+    public void DoesNothingWhenNoEnemyWithId()
+    {
+        var enemy = new JawWorm { IntendedMove = new Chomp() };
+        var gameState = new GameState { EnemyParty = new EnemyParty(enemy) };
+        var effect = new ResolveEnemyMoveEffect(EnemyId.New());
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
     }
 }
