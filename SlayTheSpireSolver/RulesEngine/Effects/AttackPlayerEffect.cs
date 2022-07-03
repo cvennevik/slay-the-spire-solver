7164ac0eaf2d1baeed1aaabdf87e4ffcb0fe53b1
cvennevik <cvennevik@gmail.com>
2022-07-03 12:52:23 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/AttackPlayerEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/AttackPlayerEffect.cs
@@ -1,5 +1,8 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 using SlayTheSpireSolver.RulesEngine.Values;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -12,5 +15,38 @@ public record AttackPlayerEffect(EnemyId EnemyId, Damage BaseDamage) : Effect
         var damagePlayerEffect = new DamagePlayerEffect(BaseDamage + enemyStrength);
 
         return gameState.WithEffects(damagePlayerEffect);
+    }
+}
+
+[TestFixture]
+public class AttackPlayerEffectTests
+{
+    [Test]
+    public void DoesNothingWhenEnemyMissing()
+    {
+        var gameState = new GameState { PlayerHealth = 10 };
+        var effect = new AttackPlayerEffect(EnemyId.Default, new Damage(1));
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
+    }
+
+    [Test]
+    public void AddsDamagePlayerEffect()
+    {
+        var gameState = new GameState { PlayerHealth = 10, EnemyParty = new EnemyParty(new JawWorm()) };
+        var effect = new AttackPlayerEffect(EnemyId.Default, new Damage(1));
+        var result = effect.Resolve(gameState).SingleUnresolvedState();
+        var expectedResult = gameState.WithEffects(new EffectStack(new DamagePlayerEffect(1)));
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void AddsEnemyStrengthToDamagePlayerEffect()
+    {
+        var gameState = new GameState { PlayerHealth = 10, EnemyParty = new EnemyParty(new JawWorm { Strength = 5}) };
+        var effect = new AttackPlayerEffect(EnemyId.Default, new Damage(1));
+        var result = effect.Resolve(gameState).SingleUnresolvedState();
+        var expectedResult = gameState.WithEffects(new EffectStack(new DamagePlayerEffect(6)));
+        Assert.AreEqual(expectedResult, result);
     }
 }
