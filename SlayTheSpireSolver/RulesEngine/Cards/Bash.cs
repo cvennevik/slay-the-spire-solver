c9cfb9d65cd36b5a71f82df01220c309c3c24847
cvennevik <cvennevik@gmail.com>
2022-07-03 14:54:37 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/Bash.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Bash.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Debuffs;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.RulesEngine.Cards;
@@ -20,4 +21,17 @@ public record Bash : TargetedCard
 }
 
 [TestFixture]
-internal class BashTests : TargetedCardTests<Bash> { }
+internal class BashTests : TargetedCardTests<Bash>
+{
+    [Test]
+    public void BroadTest()
+    {
+        var gameState = new GameState
+        {
+            Energy = 3,
+            Hand = new Hand(new Bash()),
+            EnemyParty = new[] { new JawWorm { Health = 10 } } 
+        };
+        var action = gameState.Hand.Cards.First().GetLegalActions(gameState).First();
+    }
+}
