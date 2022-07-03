679955a0c4c58ffa1dc86bc30404939a7c555179
cvennevik <cvennevik@gmail.com>
2022-07-03 10:35:44 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/AttackEnemyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/AttackEnemyEffect.cs
@@ -11,9 +11,7 @@ public record AttackEnemyEffect(EnemyId Target, Damage Damage) : Effect
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         if (!gameState.EnemyParty.Has(Target)) return gameState;
-        return gameState.EnemyParty.Has(Target)
-            ? gameState.WithEffects(new DamageEnemyEffect(Target, Damage))
-            : gameState;
+        return gameState.WithEffects(new DamageEnemyEffect(Target, Damage));
     }
 }
 
