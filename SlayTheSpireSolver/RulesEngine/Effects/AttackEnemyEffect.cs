41a008bb8a50490932a59f1d5d4c1c71876740fe
cvennevik <cvennevik@gmail.com>
2022-07-03 10:19:49 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/AttackEnemyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/AttackEnemyEffect.cs
@@ -18,6 +18,7 @@ public class AttackEnemyEffectTests
     [Test]
     public void Test()
     {
-        
+        var gameState = new GameState { Turn = 3 };
+        var effect = new AttackEnemyEffect(EnemyId.Default, new Damage(1));
     }
 }
