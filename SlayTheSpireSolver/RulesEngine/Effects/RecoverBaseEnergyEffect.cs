f55a1de7e29d713bff6f552f0d8d974f95846fb5
cvennevik <cvennevik@gmail.com>
2022-07-03 12:57:08 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/RecoverBaseEnergyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/RecoverBaseEnergyEffect.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using SlayTheSpireSolver.TestHelpers;
+
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public record RecoverBaseEnergyEffect : Effect
@@ -5,5 +8,30 @@ public record RecoverBaseEnergyEffect : Effect
     public override ResolvablePossibilitySet Resolve(GameState gameState)
     {
         return gameState with { Energy = gameState.BaseEnergy };
+    }
+}
+
+[TestFixture]
+public class RecoverBaseEnergyEffectTests
+{
+    [Test]
+    [TestCase(3, 0)]
+    [TestCase(3, 1)]
+    [TestCase(3, 2)]
+    [TestCase(3, 3)]
+    [TestCase(3, 4)]
+    [TestCase(3, 10)]
+    [TestCase(4, 3)]
+    [TestCase(4, 4)]
+    public void ResetsEnergyToBaseEnergy(int baseEnergy, int initialEnergy)
+    {
+        var gameState = new GameState
+        {
+            BaseEnergy = baseEnergy,
+            Energy = initialEnergy
+        };
+        var nextGameState = new RecoverBaseEnergyEffect().Resolve(gameState).SingleResolvedState();
+        var expectedGameState = gameState with { Energy = baseEnergy };
+        Assert.AreEqual(expectedGameState, nextGameState);
     }
 }
