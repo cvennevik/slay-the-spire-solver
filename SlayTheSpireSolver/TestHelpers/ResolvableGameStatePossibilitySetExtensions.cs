f3f2220d6f4da979e3e1a29d1036427c614aedc9
cvennevik <cvennevik@gmail.com>
2022-07-03 13:10:48 +0200

    working

--- SlayTheSpireSolver/TestHelpers/ResolvableGameStatePossibilitySetExtensions.cs
+++ SlayTheSpireSolver/TestHelpers/ResolvableGameStatePossibilitySetExtensions.cs
@@ -2,9 +2,9 @@ using SlayTheSpireSolver.RulesEngine;
 
 namespace SlayTheSpireSolver.TestHelpers;
 
-public static class ResolvableGameStatePossibilitySetExtensions
+internal static class ResolvableGameStatePossibilitySetExtensions
 {
-    public static GameState SingleResolvedState(this ResolvablePossibilitySet resolvablePossibilitySet)
+    internal static GameState SingleResolvedState(this ResolvablePossibilitySet resolvablePossibilitySet)
     {
         var unresolvedGameState = resolvablePossibilitySet.Single().ResolvableGameState;
         if (!unresolvedGameState.EffectStack.IsEmpty())
@@ -15,7 +15,7 @@ public static class ResolvableGameStatePossibilitySetExtensions
         return unresolvedGameState.GameState;
     }
 
-    public static ResolvableGameState SingleUnresolvedState(this ResolvablePossibilitySet resolvablePossibilitySet)
+    internal static ResolvableGameState SingleUnresolvedState(this ResolvablePossibilitySet resolvablePossibilitySet)
     {
         var resolvableGameState = resolvablePossibilitySet.Single().ResolvableGameState;
         if (resolvableGameState.EffectStack.IsEmpty())
