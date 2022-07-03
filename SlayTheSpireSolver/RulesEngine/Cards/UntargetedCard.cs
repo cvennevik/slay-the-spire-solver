4e70da91e2ac8c7180f26331f8a4eb8dd92b7eb7
cvennevik <cvennevik@gmail.com>
2022-07-03 12:22:36 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/UntargetedCard.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/UntargetedCard.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Effects;
+using SlayTheSpireSolver.RulesEngine.Enemies;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.RulesEngine.Cards;
 
@@ -13,5 +16,28 @@ public abstract record UntargetedCard : Card
                 new RemoveCardFromHandEffect(this),
                 new RemoveEnergyEffect(GetCost()))) }
             : Array.Empty<Action>();
+    }
+}
+
+
+[TestFixture]
+public class UntargetedCardTests
+{
+    [Test]
+    public void TestGetLegalActions()
+    {
+        var gameState = new GameState
+        {
+            Energy = 1,
+            EnemyParty = new EnemyParty(new JawWorm()),
+            Hand = new Hand(new Strike())
+        };
+        var strike = new Strike();
+        var playStrikeAction = new Action(gameState, new EffectStack(
+            new AddCardToDiscardPileEffect(strike),
+            strike.GetEffect(gameState),
+            new RemoveCardFromHandEffect(strike),
+            new RemoveEnergyEffect(strike.GetCost())));
+        Assert.AreEqual(playStrikeAction, strike.GetLegalActions(gameState).Single());
     }
 }
