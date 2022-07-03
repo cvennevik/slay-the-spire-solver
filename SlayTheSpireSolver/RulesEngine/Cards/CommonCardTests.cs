db77dfb0bf2feabcb021535ff708ef61fc550b98
cvennevik <cvennevik@gmail.com>
2022-07-03 12:31:06 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/CommonCardTests.cs
+++
@@ -1,7 +0,0 @@
-using NUnit.Framework;
-using SlayTheSpireSolver.RulesEngine.Effects;
-using SlayTheSpireSolver.RulesEngine.Enemies;
-using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
-
-namespace SlayTheSpireSolver.RulesEngine.Cards;
-
