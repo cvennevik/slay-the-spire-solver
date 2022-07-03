853b063407fb1f31a7f8c4b8b649271267cccd5e
cvennevik <cvennevik@gmail.com>
2022-07-03 13:44:25 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/Bash.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Bash.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Debuffs;
 using SlayTheSpireSolver.RulesEngine.Effects;
 using SlayTheSpireSolver.RulesEngine.Enemies;
 using SlayTheSpireSolver.RulesEngine.Values;
@@ -7,10 +8,17 @@ namespace SlayTheSpireSolver.RulesEngine.Cards;
 
 public record Bash : TargetedCard
 {
+    private readonly Damage _damage = 8;
+    private readonly Vulnerable _vulnerable = 2;
+
     public override Energy GetCost() => 2;
 
-    public override EffectStack GetTargetedEffect(EnemyId target) => new(new ApplyVulnerableToEnemyEffect(target, 2),
-        new AttackEnemyEffect(target, 8));
+    public override EffectStack GetTargetedEffect(EnemyId target) => new Effect[]
+    {
+        new ApplyVulnerableToEnemyEffect(target, _vulnerable),
+        new AttackEnemyEffect(target, _damage)
+    };
+
     protected override string GetName() => "Bash";
 }
 
