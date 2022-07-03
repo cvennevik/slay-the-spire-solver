6226fc6348f6cb3eb05c893e3b28f76f8445c8b3
cvennevik <cvennevik@gmail.com>
2022-07-03 16:56:15 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/EndTurnEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/EndTurnEffect.cs
@@ -2,7 +2,6 @@ using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Cards;
 using SlayTheSpireSolver.RulesEngine.Enemies;
 using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
-using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
