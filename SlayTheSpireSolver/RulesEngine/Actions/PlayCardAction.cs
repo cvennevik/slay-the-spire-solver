2e0adaf070930188d9de717fcb79933356982516
cvennevik <cvennevik@gmail.com>
2022-07-03 15:37:53 +0200

    working

---
+++ SlayTheSpireSolver/RulesEngine/Actions/PlayCardAction.cs
@@ -0,0 +1,9 @@
+using SlayTheSpireSolver.RulesEngine.Effects;
+
+namespace SlayTheSpireSolver.RulesEngine.Actions;
+
+public record PlayCardAction : PlayerAction
+{
+    public PlayCardAction(GameState gameState, EffectStack effectStack) : base(gameState, effectStack) { }
+    public PlayCardAction(GameState gameState, params Effect[] effects) : base(gameState, effects) { }
+}
