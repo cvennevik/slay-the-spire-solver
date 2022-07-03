89e97ecde7247f7cb25609b777694bc9a6abfd7b
cvennevik <cvennevik@gmail.com>
2022-07-03 12:56:52 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/MoveHandToDiscardPileEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/MoveHandToDiscardPileEffect.cs
@@ -1,3 +1,7 @@
+using NUnit.Framework;
+using SlayTheSpireSolver.RulesEngine.Cards;
+using SlayTheSpireSolver.TestHelpers;
+
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public record MoveHandToDiscardPileEffect : Effect
@@ -8,5 +12,54 @@ public record MoveHandToDiscardPileEffect : Effect
             gameState.DiscardPile, (current, card) => current.Add(card));
 
         return gameState with { Hand = new Hand(), DiscardPile = newDiscardPile };
+    }
+}
+
+[TestFixture]
+public class MoveHandToDiscardPileEffectTests
+{
+    [Test]
+    public void DoesNothingWhenHandIsEmpty()
+    {
+        var gameState = new GameState { Hand = new Hand(), DiscardPile = new DiscardPile(new Strike()) };
+        var effect = new MoveHandToDiscardPileEffect();
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(gameState, result);
+    }
+
+    [Test]
+    public void PutsCardsInHandInEmptyDiscardPile()
+    {
+        var gameState = new GameState
+        {
+            Hand = new Hand(new Strike(), new Strike(), new Defend()),
+            DiscardPile = new DiscardPile()
+        };
+        var effect = new MoveHandToDiscardPileEffect();
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        var expectedGameState = new GameState
+        {
+            Hand = new Hand(),
+            DiscardPile = new DiscardPile(new Strike(), new Strike(), new Defend())
+        };
+        Assert.AreEqual(expectedGameState, result);
+    }
+
+    [Test]
+    public void AddsCardsInHandToExistingDiscardPile()
+    {
+        var gameState = new GameState
+        {
+            Hand = new Hand(new Strike()),
+            DiscardPile = new DiscardPile(new Strike(), new Defend())
+        };
+        var effect = new MoveHandToDiscardPileEffect();
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        var expectedGameState = new GameState
+        {
+            Hand = new Hand(),
+            DiscardPile = new DiscardPile(new Strike(), new Strike(), new Defend())
+        };
+        Assert.AreEqual(expectedGameState, result);
     }
 }
