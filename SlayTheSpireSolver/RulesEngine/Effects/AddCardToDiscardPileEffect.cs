1537926c0001a39168aac81f976acedfe9b26498
cvennevik <cvennevik@gmail.com>
2022-07-03 12:51:50 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/AddCardToDiscardPileEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/AddCardToDiscardPileEffect.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using SlayTheSpireSolver.RulesEngine.Cards;
+using SlayTheSpireSolver.TestHelpers;
 
 namespace SlayTheSpireSolver.RulesEngine.Effects;
 
@@ -8,5 +10,34 @@ public record AddCardToDiscardPileEffect(Card CardToAdd) : Effect
     {
         var newCardsInDiscardPile = gameState.DiscardPile.Cards.Append(CardToAdd).ToArray();
         return gameState with { DiscardPile = new DiscardPile(newCardsInDiscardPile) };
+    }
+}
+
+[TestFixture]
+public class AddCardToDiscardPileEffectTests
+{
+    [Test]
+    public void AddsToEmptyDiscardPile()
+    {
+        var gameState = new GameState { DiscardPile = new DiscardPile() };
+        var effect = new AddCardToDiscardPileEffect(new Strike());
+        var result = effect.Resolve(gameState).SingleResolvedState();
+        Assert.AreEqual(new GameState { DiscardPile = new DiscardPile(new Strike()) }, result);
+    }
+
+    [Test]
+    public void AddsToDiscardPileWithExistingCards()
+    {
+        var gameState = new GameState
+        {
+            DiscardPile = new DiscardPile(new Strike(), new Defend())
+        };
+        var effect = new AddCardToDiscardPileEffect(new Strike());
+        var resultingGameState = effect.Resolve(gameState).SingleResolvedState();
+        var expectedGameState = new GameState
+        {
+            DiscardPile = new DiscardPile(new Strike(), new Defend(), new Strike())
+        };
+        Assert.AreEqual(expectedGameState, resultingGameState);
     }
 }
