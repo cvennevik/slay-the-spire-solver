3a4284eb2dc3dac71751a374ec1e3176e3a07d56
cvennevik <cvennevik@gmail.com>
2022-07-03 12:15:37 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Cards/Card.cs
+++ SlayTheSpireSolver/RulesEngine/Cards/Card.cs
@@ -7,22 +7,5 @@ public abstract record Card
 {
     public abstract Energy GetCost();
     public abstract Effect GetEffect(GameState gameState);
-
-    public virtual IReadOnlyCollection<Action> GetLegalActions(GameState gameState)
-    {
-        return CanBePlayed(gameState)
-            ? new[] { new Action(gameState, new EffectStack(
-                new AddCardToDiscardPileEffect(this),
-                GetEffect(gameState),
-                new RemoveCardFromHandEffect(this),
-                new RemoveEnergyEffect(GetCost()))) }
-            : Array.Empty<Action>();
-    }
-
-    private bool CanBePlayed(GameState gameState)
-    {
-        return !gameState.IsCombatOver()
-               && gameState.Hand.Contains(this)
-               && gameState.Energy >= GetCost();
-    }
+    public abstract IReadOnlyCollection<Action> GetLegalActions(GameState gameState);
 }
