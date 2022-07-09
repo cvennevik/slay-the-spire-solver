a3b51d2f0dd4beff039b69c64cedeafdfc7e5d7e
cvennevik <cvennevik@gmail.com>
2022-07-09 11:30:37 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Actions/PlayCardAction.cs
+++ SlayTheSpireSolver/RulesEngine/Actions/PlayCardAction.cs
@@ -6,8 +6,11 @@ namespace SlayTheSpireSolver.RulesEngine.Actions;
 public abstract record PlayCardAction : PlayerAction
 {
     protected PlayCardAction(GameState gameState, Card card, EffectStack cardEffects)
-        : base(gameState with {EffectStack = new EffectStack(new AddCardToDiscardPileEffect(card))
-            .Push(cardEffects)
-            .Push(new RemoveCardFromHandEffect(card))
-            .Push(new RemoveEnergyEffect(card.GetCost()))}) { }
+        : base(gameState with
+        {
+            EffectStack = new EffectStack(new AddCardToDiscardPileEffect(card))
+                .Push(cardEffects)
+                .Push(new RemoveCardFromHandEffect(card))
+                .Push(new RemoveEnergyEffect(card.GetCost()))
+        }) { }
 }
