13eea62e8613720012c5ad6f3ce0325522cfe7b6
cvennevik <cvennevik@gmail.com>
2022-07-09 12:58:25 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Possibility.cs
+++ SlayTheSpireSolver/RulesEngine/Possibility.cs
@@ -58,7 +58,6 @@ internal class PossibilityTests
     {
         var gameState = new GameState
         {
-            Hand = new Hand(new Strike()),
             EnemyParty = new EnemyParty(new JawWorm { Health = 2 }),
             EffectStack = new Strike().GetTargetedEffects(EnemyId.Default)
         };
