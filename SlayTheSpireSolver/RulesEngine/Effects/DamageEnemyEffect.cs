9af4f13bfab5b954b2ccbb84bfd921a0c0d6a5f3
cvennevik <cvennevik@gmail.com>
2022-07-09 13:47:22 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DamageEnemyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DamageEnemyEffect.cs
@@ -36,7 +36,7 @@ public record DamageEnemyEffect(EnemyId TargetId, Damage Damage) : Effect
 
         if (newGameState.EnemyParty.Get(TargetId).Health.Amount <= 0)
         {
-            return newGameState.WithAddedEffects(new EffectStack(new KillEnemyEffect(TargetId)));
+            return newGameState.WithAddedEffects(new KillEnemyEffect(TargetId));
         }
 
         return newGameState;
