40791ab7f26f03d39b75ea4d8120b4fc1ab324f1
cvennevik <cvennevik@gmail.com>
2022-07-09 12:44:39 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Possibility.cs
+++ SlayTheSpireSolver/RulesEngine/Possibility.cs
@@ -62,6 +62,7 @@ internal class PossibilityTests
             DrawPile = new DrawPile(new Strike(), new Strike(), new Defend()),
             Hand = new Hand(new Strike(), new Strike())
         };
+        var expectedPossibility1 = new Possibility(expectedGameState1, new Probability(0.3));
         Assert.AreEqual(2, result.Count);
         Assert.AreEqual(0.5, result.Sum(x => x.Probability.Value));
         Assert.AreEqual(1, result.Count(x => x.GameState == expectedGameState1));
