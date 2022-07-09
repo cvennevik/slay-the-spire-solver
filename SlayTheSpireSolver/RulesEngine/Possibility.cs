100e81316af5b925c0d9b53581bf2efca1239c31
cvennevik <cvennevik@gmail.com>
2022-07-09 12:43:48 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Possibility.cs
+++ SlayTheSpireSolver/RulesEngine/Possibility.cs
@@ -64,5 +64,6 @@ internal class PossibilityTests
         };
         Assert.AreEqual(2, result.Count);
         Assert.AreEqual(0.5, result.Sum(x => x.Probability.Value));
+        Assert.AreEqual(1, result.Count(x => x.GameState == expectedGameState1));
     }
 }
