c1d4b414682af06ecc168c93ea41865b5466657d
cvennevik <cvennevik@gmail.com>
2022-07-09 13:01:11 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Possibility.cs
+++ SlayTheSpireSolver/RulesEngine/Possibility.cs
@@ -36,7 +36,7 @@ public record Possibility(GameState GameState, Probability Probability)
         var (effect, remainingEffectStack) = GameState.EffectStack.Pop();
         return effect
             .NewResolve(GameState with {EffectStack = remainingEffectStack})
-            .Select(resolvablePossibility => resolvablePossibility with {Probability = resolvablePossibility.Probability * Probability})
+            .Select(possibility => possibility with {Probability = possibility.Probability * Probability})
             .ToArray();
     }
 }
