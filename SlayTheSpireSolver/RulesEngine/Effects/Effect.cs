c1f8218e61e6faba8575801fa272c4799e537f20
cvennevik <cvennevik@gmail.com>
2022-07-09 12:25:14 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/Effect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/Effect.cs
@@ -4,6 +4,14 @@ public abstract record Effect
 {
     public abstract ResolvablePossibilitySet Resolve(GameState gameState);
 
+    public virtual IReadOnlyCollection<Possibility> NewResolve(GameState gameState)
+    {
+        return Resolve(gameState)
+            .Select(resolvablePossibility =>
+                new Possibility(resolvablePossibility.GameState, resolvablePossibility.Probability))
+            .ToList();
+    }
+
     public ResolvablePossibilitySet ResolveWithBaseEffectStack(GameState gameState, EffectStack effectStack)
     {
         return Resolve(gameState)
