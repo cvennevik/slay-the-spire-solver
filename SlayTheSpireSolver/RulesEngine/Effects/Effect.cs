8f66adef5fa38aaf80174b5e5d7a49ed1d1a5daf
cvennevik <cvennevik@gmail.com>
2022-07-09 14:00:29 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/Effect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/Effect.cs
@@ -2,19 +2,5 @@ namespace SlayTheSpireSolver.RulesEngine.Effects;
 
 public abstract record Effect
 {
-    protected ResolvablePossibilitySet OldResolve(GameState gameState)
-    {
-        return gameState;
-    }
-
-    public virtual PossibilitySet NewResolve(GameState gameState)
-    {
-        var originalEffectStack = gameState.EffectStack;
-        var resolvablePossibilities = OldResolve(gameState with { EffectStack = new EffectStack() }).ToList();
-        var possibilitiesWithoutOriginalEffectStack = resolvablePossibilities.Select(x =>
-            new Possibility(x.GameState with { EffectStack = x.GameState.EffectStack }, x.Probability));
-        var possibilities = possibilitiesWithoutOriginalEffectStack.Select(x =>
-            x with { GameState = x.GameState with { EffectStack = originalEffectStack.Push(x.GameState.EffectStack) } });
-        return possibilities.ToArray();
-    }
+    public abstract PossibilitySet NewResolve(GameState gameState);
 }
