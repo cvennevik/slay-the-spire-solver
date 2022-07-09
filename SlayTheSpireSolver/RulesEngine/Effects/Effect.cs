33247c4b2fc3984cf4c84a46bb3ad7b54ab91323
cvennevik <cvennevik@gmail.com>
2022-07-09 13:14:26 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/Effect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/Effect.cs
@@ -9,7 +9,7 @@ public abstract record Effect
         var originalEffectStack = gameState.EffectStack;
         var resolvablePossibilities = Resolve(gameState with { EffectStack = new EffectStack() }).ToList();
         var possibilitiesWithoutOriginalEffectStack = resolvablePossibilities.Select(x =>
-            new Possibility(x.GameState with { EffectStack = x.ResolvableGameState.EffectStack }, x.Probability));
+            new Possibility(x.GameState with { EffectStack = x.GameState.EffectStack }, x.Probability));
         var possibilities = possibilitiesWithoutOriginalEffectStack.Select(x =>
             x with { GameState = x.GameState with { EffectStack = originalEffectStack.Push(x.GameState.EffectStack) } });
         return possibilities.ToList();
