7d01623d793bdcb7d26de8b86a0e9c49daadbcc3
cvennevik <cvennevik@gmail.com>
2022-07-09 12:52:34 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Actions/PlayerAction.cs
+++ SlayTheSpireSolver/RulesEngine/Actions/PlayerAction.cs
@@ -3,9 +3,11 @@ namespace SlayTheSpireSolver.RulesEngine.Actions;
 public abstract record PlayerAction
 {
     private readonly ResolvableGameState _resolvableGameState;
+    private readonly GameState _unresolvedGameState;
 
     protected PlayerAction(GameState gameState)
     {
+        _unresolvedGameState = gameState;
         _resolvableGameState = new ResolvableGameState(gameState);
     }
 
