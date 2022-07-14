833ea244e3a78ff5fffe5409370655f80f330372
cvennevik <cvennevik@gmail.com>
2022-07-14 20:49:52 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -1,21 +1,10 @@
 ﻿using NUnit.Framework;
-using SlayTheSpireSolver.RulesEngine;
-using SlayTheSpireSolver.RulesEngine.Actions;
 
 namespace SlayTheSpireSolver.AI;
 
 public static class Solver
 {
-    public static PlayerAction GetBestAction(GameState gameState)
-    {
-        var legalActions = gameState.GetLegalActions();
-        var actionsExceptEndTurn = legalActions.Where(x => x is not EndTurnAction).ToList();
-        if (actionsExceptEndTurn.Any())
-        {
-            return actionsExceptEndTurn.First();
-        }
-        return legalActions.First();
-    }
+    // todo
 }
 
 [TestFixture]
