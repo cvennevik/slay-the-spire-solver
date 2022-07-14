a2d46889c0db02cd52f76667979ca01e2b25df74
cvennevik <cvennevik@gmail.com>
2022-07-14 20:54:05 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -5,6 +5,9 @@ namespace SlayTheSpireSolver.AI;
 public static class Solver
 {
     // todo
+    //  1. Evaluate terminal game states
+    //  2. Evaluate incomplete game states
+    //  3. Evaluate actions
 }
 
 [TestFixture]
