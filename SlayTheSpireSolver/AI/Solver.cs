18da4310c7f2b813da1e7d21c4ca9b869042070d
cvennevik <cvennevik@gmail.com>
2022-07-16 14:57:49 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -130,6 +130,7 @@ internal class SolverTests
         };
         var searchResult = Solver.FindBestExpectedOutcome(nonTerminalGameState);
         Assert.AreEqual(expectedResult, searchResult.ExpectedValue);
+        Assert.Less(1, searchResult.EvaluatedGameStates);
     }
 
     [Test]
