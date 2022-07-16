f78f8437c3742f8550ac1705ee927a7a7b8756ba
cvennevik <cvennevik@gmail.com>
2022-07-16 16:30:12 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -20,7 +20,7 @@ public class Solver
     public SearchResult FindBestExpectedOutcome(GameState gameState, int gameStateDepthLimit)
     {
         var isCached = _gameStateCache.TryGetValue(gameState, out var cachedResult);
-        if (isCached) return cachedResult!;
+        if (isCached) return cachedResult! with { CacheHits = 1 };
         if (gameState.IsCombatOver())
         {
             var value = Math.Max(gameState.PlayerHealth.Amount, 0);
