83bbe20b4eb4aeef201c463ac62f233a23e4a203
cvennevik <cvennevik@gmail.com>
2022-07-16 15:55:59 +0200

    working

--- SlayTheSpireSolver/Program.cs
+++ SlayTheSpireSolver/Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using SlayTheSpireSolver.AI;
+using SlayTheSpireSolver.RulesEngine;
+using SlayTheSpireSolver.RulesEngine.Cards;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
+
+var jawWorm = new JawWorm
+{
+    Health = 44,
+    IntendedMove = new Chomp()
+};
+var gameState = new GameState
+{
+    PlayerHealth = 80,
+    BaseEnergy = 3,
+    Energy = 3,
+    EnemyParty = new[] { jawWorm },
+    Hand = new Hand(new Strike(), new Strike(), new Strike(), new Bash(), new Defend()),
+    DrawPile = new DrawPile(new Defend(), new Defend(), new Defend(), new Strike(), new Strike()),
+    Turn = 1
+};
+const int gameStateDepthLimit = 3;
+var stopWatch = Stopwatch.StartNew();
+var searchResult = Solver.FindBestExpectedOutcome(gameState);
+stopWatch.Stop();
+
+Console.WriteLine(searchResult);
