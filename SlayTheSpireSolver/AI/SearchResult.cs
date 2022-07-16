42abf7fd86cb297cb2c1b9d6f4cfd604bb26f735
cvennevik <cvennevik@gmail.com>
2022-07-16 16:20:35 +0200

    working

--- SlayTheSpireSolver/AI/SearchResult.cs
+++ SlayTheSpireSolver/AI/SearchResult.cs
@@ -5,4 +5,5 @@ public record SearchResult
     public double ExpectedValue { get; init; }
     public int EvaluatedGameStates { get; init; }
     public int EvaluatedActions { get; init; }
+    public int CacheHits { get; init; }
 }
