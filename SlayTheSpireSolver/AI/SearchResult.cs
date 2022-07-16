b137b186b97ff40390b32332ae1e9fc96ab7939b
cvennevik <cvennevik@gmail.com>
2022-07-16 17:11:29 +0200

    working

--- SlayTheSpireSolver/AI/SearchResult.cs
+++
@@ -1,8 +0,0 @@
-namespace SlayTheSpireSolver.AI;
-
-public record SearchResult
-{
-    public double ExpectedValue { get; init; }
-    public int EvaluatedGameStates { get; init; }
-    public int EvaluatedActions { get; init; }
-}
