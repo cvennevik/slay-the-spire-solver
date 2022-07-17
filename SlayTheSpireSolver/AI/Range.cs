27b470168bebac04852a81151134724294213c67
cvennevik <cvennevik@gmail.com>
2022-07-17 15:44:14 +0200

    working

--- SlayTheSpireSolver/AI/Range.cs
+++ SlayTheSpireSolver/AI/Range.cs
@@ -1,3 +1,9 @@
 namespace SlayTheSpireSolver.AI;
 
-public record Range(double Minimum, double Maximum);
+public record Range(double Minimum, double Maximum)
+{
+    public static Range operator +(Range a, Range b)
+    {
+        return new Range(Math.Min(a.Minimum, b.Minimum), Math.Max(a.Maximum, b.Maximum));
+    }
+}
