cb30c56aacc3030cbb2027a27a72bec662162806
cvennevik <cvennevik@gmail.com>
2022-07-17 22:35:01 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -253,6 +253,7 @@ internal class SolverTests
         Assert.AreEqual(expectedAction, action6);
         Assert.AreEqual(expectedAction, action7);
         Assert.AreEqual(expectedAction, action8);
+        Assert.AreEqual(expectedAction, action9);
         AssertRangeContains(new Range(0, 50), expectedValue2.Range);
         AssertRangeContains(expectedValue2.Range, expectedValue3.Range);
         AssertRangeContains(expectedValue3.Range, expectedValue4.Range);
