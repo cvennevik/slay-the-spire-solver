3c29025345a6e3f5300852e03de3b002ae1f045e
cvennevik <cvennevik@gmail.com>
2022-07-17 09:21:15 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Chomp.cs
+++ SlayTheSpireSolver/RulesEngine/Enemies/JawWorms/Chomp.cs
@@ -6,8 +6,6 @@ namespace SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 public record Chomp : EnemyMove
 {
     private static readonly Damage BaseDamage = new(11);
-
     public override EffectStack GetEffects(Enemy enemy) => new AttackPlayerEffect(enemy.Id, BaseDamage);
-
     protected override string GetName() => "Chomp";
 }
