6f2b6102805acd97e73eece4579cf567e8f03cfa
cvennevik <cvennevik@gmail.com>
2022-07-17 17:01:44 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -240,6 +240,7 @@ internal class SolverTests
         var (action7, expectedValue7) = new Solver { GameStateSearchDepth = 7 }.FindBestAction(gameState);
         Assert.AreEqual(new PlayTargetedCardAction(gameState, new Strike(), EnemyId.Default), action4);
         Assert.AreEqual(action4, action5);
+        Assert.AreEqual(action4, action6);
     }
 
     private void AssertRangeContains(Range range, double value)
