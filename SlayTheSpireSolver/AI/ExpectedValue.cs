1aaf4f31849b08bbdeb428b44dd49da22ff0bb1c
cvennevik <cvennevik@gmail.com>
2022-07-17 14:42:38 +0200

    working

--- SlayTheSpireSolver/AI/ExpectedValue.cs
+++ SlayTheSpireSolver/AI/ExpectedValue.cs
@@ -2,9 +2,22 @@ using SlayTheSpireSolver.RulesEngine.Values;
 
 namespace SlayTheSpireSolver.AI;
 
-public record ExpectedValue(double Minimum, double Maximum)
+public record ExpectedValue(double Minimum, double Maximum) : IComparable<ExpectedValue>
 {
     public double BestEstimate => Minimum;
+
+    public int CompareTo(ExpectedValue? other)
+    {
+        if (other == null) return 1;
+
+        var difference = BestEstimate - other.BestEstimate;
+        return difference switch
+        {
+            < 0 => -1,
+            0 => 0,
+            _ => 1
+        };
+    }
 
     public static ExpectedValue operator +(ExpectedValue a, ExpectedValue b)
     {
