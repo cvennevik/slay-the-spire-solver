1b049dc6a6cdb369c3ae927c957bc6326b100229
cvennevik <cvennevik@gmail.com>
2022-07-17 14:08:19 +0200

    working

--- SlayTheSpireSolver/AI/ExpectedValueRange.cs
+++ SlayTheSpireSolver/AI/ExpectedValueRange.cs
@@ -7,7 +7,11 @@ public record ExpectedValueRange
         this.Minimum = Minimum;
         this.Maximum = Maximum;
     }
+
     public double ToExpectedValue => Minimum;
     public double Minimum { get; init; }
     public double Maximum { get; init; }
+
+    public static ExpectedValueRange operator +(ExpectedValueRange a, ExpectedValueRange b) =>
+        new(a.Minimum + b.Minimum, a.Minimum + b.Maximum);
 }
