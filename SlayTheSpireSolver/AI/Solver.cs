e5ac6c061168572222da4e22fce79efc5ee719bb
cvennevik <cvennevik@gmail.com>
2022-07-17 11:48:25 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -21,9 +21,6 @@ public class Solver
     //  * Parallelize
     //  * Improve non-terminal game state estimation
     //  * Return best actions + their expected value
-    //  * Check correctness
-    //      * Write tests for game states solvable in few steps
-    //      * Try playing a real game using the solver
     //  * Improve Rules Engine performance
 
     public double FindExpectedValue(GameState gameState) =>
