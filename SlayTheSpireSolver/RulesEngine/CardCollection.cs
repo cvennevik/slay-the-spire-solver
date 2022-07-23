42c6c90d574040962bf6e4f5c8df4e85f6f1da5e
cvennevik <cvennevik@gmail.com>
2022-07-23 15:02:05 +0200

    working

--- SlayTheSpireSolver/RulesEngine/CardCollection.cs
+++ SlayTheSpireSolver/RulesEngine/CardCollection.cs
@@ -8,8 +8,7 @@ public abstract class CardCollection<T> where T : CardCollection<T>
 
     protected CardCollection(params Card[] cards)
     {
-        Array.Sort(cards,
-            (card1, card2) => string.Compare(card1.ToString(), card2.ToString(), StringComparison.Ordinal));
+        Array.Sort(cards);
         Cards = cards;
     }
 
