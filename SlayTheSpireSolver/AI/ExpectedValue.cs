54bb299f45e8562ecf0e29e5137d8c1e1e7309eb
cvennevik <cvennevik@gmail.com>
2022-07-23 09:37:28 +0200

    working

--- SlayTheSpireSolver/AI/ExpectedValue.cs
+++ SlayTheSpireSolver/AI/ExpectedValue.cs
@@ -18,19 +18,6 @@ public record ExpectedValue
     public double Minimum { get; }
     public double Estimate { get; }
 
-    public int CompareTo(ExpectedValue? other)
-    {
-        if (other == null) return 1;
-
-        var difference = Estimate - other.Estimate;
-        return difference switch
-        {
-            < 0 => -1,
-            0 => 0,
-            _ => 1
-        };
-    }
-
     public override string ToString()
     {
         return $"{{Minimum: {Minimum}, Estimate: {Estimate}}}";
