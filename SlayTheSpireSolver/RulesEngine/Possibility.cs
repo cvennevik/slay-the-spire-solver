23a5aee7775053d4f9fbfcb28a52bc870cd74691
cvennevik <cvennevik@gmail.com>
2022-07-23 21:26:00 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Possibility.cs
+++ SlayTheSpireSolver/RulesEngine/Possibility.cs
@@ -28,7 +28,7 @@ public record Possibility(GameState GameState, Probability Probability)
     {
         if (GameState.EffectStack.IsEmpty()) return new PossibilitySet(this);
 
-        var fullyResolvedPossibilities = new Dictionary<GameState, Probability>();
+        var fullyResolvedPossibilities = new List<Possibility>();
         var topEffectResolved = ResolveTopEffect();
 
         return topEffectResolved
