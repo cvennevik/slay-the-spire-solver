9d32bf3e513bcef88c53eaf31cc403594b172aeb
cvennevik <cvennevik@gmail.com>
2022-07-23 10:38:59 +0200

    working

---
+++ SlayTheSpireSolver.Benchmark/Benchmarks.cs
@@ -0,0 +1,13 @@
+using BenchmarkDotNet.Attributes;
+
+namespace SlayTheSpireSolver.Benchmark;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    [Benchmark]
+    public void MyFirstBenchmark()
+    {
+        
+    }
+}
