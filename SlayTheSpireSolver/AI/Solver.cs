b2fe43c9367ea8bf5dda23418e58c99d1b05c1ef
cvennevik <cvennevik@gmail.com>
2022-07-23 23:03:27 +0200

    working

--- SlayTheSpireSolver/AI/Solver.cs
+++ SlayTheSpireSolver/AI/Solver.cs
@@ -82,7 +82,7 @@ public class Solver
         var cutoffAction = actions.First();
         var cutoffExpectedValue = FindExpectedValue(cutoffAction, gameStateDepthLimit);
         var cutoffValue = cutoffExpectedValue.Minimum;
-        var result = actions
+        return actions
             .AsParallel()
             .Select(action => FindExpectedValue(action, gameStateDepthLimit, cutoffValue))
             .Append(cutoffExpectedValue)
