e7aba6ea6cc7ed848fcbc803ccd55e8c83a6e514
cvennevik <cvennevik@gmail.com>
2022-07-23 10:47:53 +0200

    working

--- SlayTheSpireSolver.Benchmark/Benchmarks.cs
+++ SlayTheSpireSolver.Benchmark/Benchmarks.cs
@@ -10,24 +10,27 @@ namespace SlayTheSpireSolver.Benchmark;
 [MemoryDiagnoser(false)]
 public class Benchmarks
 {
+    private readonly GameState _gameState = new()
+    {
+        PlayerHealth = 80,
+        BaseEnergy = 3,
+        Energy = 3,
+        EnemyParty = new[]
+        {
+            new JawWorm
+            {
+                Health = 30,
+                IntendedMove = new Chomp()
+            }
+        },
+        Hand = new Hand(new Strike(), new Strike(), new Strike(), new Bash(), new Defend()),
+        DrawPile = new DrawPile(new Defend(), new Defend(), new Defend(), new Strike(), new Strike()),
+        Turn = 1
+    };
+
     [Benchmark]
     public (PlayerAction, ExpectedValue) MyFirstBenchmark()
     {
-        var jawWorm = new JawWorm
-        {
-            Health = 30,
-            IntendedMove = new Chomp()
-        };
-        var gameState = new GameState
-        {
-            PlayerHealth = 80,
-            BaseEnergy = 3,
-            Energy = 3,
-            EnemyParty = new[] { jawWorm },
-            Hand = new Hand(new Strike(), new Strike(), new Strike(), new Bash(), new Defend()),
-            DrawPile = new DrawPile(new Defend(), new Defend(), new Defend(), new Strike(), new Strike()),
-            Turn = 1
-        };
-        return new Solver(3).FindBestAction(gameState);
+        return new Solver(3).FindBestAction(_gameState);
     }
 }
