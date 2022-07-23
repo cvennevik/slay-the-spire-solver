89003f319f60b690d9a2161a1b15a5b17db35e1b
cvennevik <cvennevik@gmail.com>
2022-07-23 10:41:13 +0200

    working

--- SlayTheSpireSolver.Benchmark/Benchmarks.cs
+++ SlayTheSpireSolver.Benchmark/Benchmarks.cs
@@ -1,4 +1,9 @@
 using BenchmarkDotNet.Attributes;
+using SlayTheSpireSolver.AI;
+using SlayTheSpireSolver.RulesEngine;
+using SlayTheSpireSolver.RulesEngine.Actions;
+using SlayTheSpireSolver.RulesEngine.Cards;
+using SlayTheSpireSolver.RulesEngine.Enemies.JawWorms;
 
 namespace SlayTheSpireSolver.Benchmark;
 
@@ -6,8 +11,23 @@ namespace SlayTheSpireSolver.Benchmark;
 public class Benchmarks
 {
     [Benchmark]
-    public void MyFirstBenchmark()
+    public (PlayerAction, ExpectedValue) MyFirstBenchmark()
     {
-        
+        var jawWorm = new JawWorm
+        {
+            Health = 30,
+            IntendedMove = new Chomp()
+        };
+        var gameState = new GameState
+        {
+            PlayerHealth = 80,
+            BaseEnergy = 3,
+            Energy = 3,
+            EnemyParty = new[] { jawWorm },
+            Hand = new Hand(new Strike(), new Strike(), new Strike(), new Bash(), new Defend()),
+            DrawPile = new DrawPile(new Defend(), new Defend(), new Defend(), new Strike(), new Strike()),
+            Turn = 1
+        };
+        return new Solver(1).FindBestAction(gameState);
     }
 }
