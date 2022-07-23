22027a4af6a3bfc08ffdb91ebf58e9b4f28b4416
cvennevik <cvennevik@gmail.com>
2022-07-23 11:08:39 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Values/Probability.cs
+++ SlayTheSpireSolver/RulesEngine/Values/Probability.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace SlayTheSpireSolver.RulesEngine.Values;
 
-public record Probability
+public readonly record struct Probability
 {
     public Probability(double value)
     {
