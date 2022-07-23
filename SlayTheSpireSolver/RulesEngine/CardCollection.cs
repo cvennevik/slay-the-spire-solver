e26b09cdc986824fd4ac4b0cab20f7836578eed8
cvennevik <cvennevik@gmail.com>
2022-07-23 20:12:22 +0200

    working

--- SlayTheSpireSolver/RulesEngine/CardCollection.cs
+++ SlayTheSpireSolver/RulesEngine/CardCollection.cs
@@ -139,4 +139,50 @@ internal abstract class CardCollectionTests<T> where T : CardCollection<T>, new(
         var collection = _type.CreateNew(new Strike()).Add(new Defend());
         Assert.AreEqual(_type.CreateNew(new Strike(), new Defend()), collection);
     }
+
+    [Test]
+    public void ThrowsExceptionWhenHandEmpty()
+    {
+        var hand = new Hand();
+        Assert.Throws<ArgumentException>(() => hand.Remove(new Strike()));
+    }
+
+    [Test]
+    public void RemoveStrikeCard()
+    {
+        var hand = new Hand(new Strike());
+        var newHand = hand.Remove(new Strike());
+        Assert.AreEqual(new Hand(), newHand);
+    }
+
+    [Test]
+    public void RemovesDefendCard()
+    {
+        var hand = new Hand(new Defend());
+        var newHand = hand.Remove(new Defend());
+        Assert.AreEqual(new Hand(), newHand);
+    }
+
+    [Test]
+    public void RemovesOnlyOneCopy()
+    {
+        var hand = new Hand(new Strike(), new Strike());
+        var newHand = hand.Remove(new Strike());
+        Assert.AreEqual(new Hand(new Strike()), newHand);
+    }
+
+    [Test]
+    public void RemovesCorrectCardType()
+    {
+        var hand = new Hand(new Strike(), new Defend());
+        var newHand = hand.Remove(new Strike());
+        Assert.AreEqual(new Hand(new Defend()), newHand);
+    }
+
+    [Test]
+    public void ThrowsExceptionWhenCardTypeNotInHand()
+    {
+        var hand = new Hand(new Defend());
+        Assert.Throws<ArgumentException>(() => hand.Remove(new Strike()));
+    }
 }
