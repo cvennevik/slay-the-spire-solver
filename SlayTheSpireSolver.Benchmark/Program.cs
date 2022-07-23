31f2b38370effd8b9debadd4c3172d54776a5225
cvennevik <cvennevik@gmail.com>
2022-07-23 10:37:06 +0200

    working

--- SlayTheSpireSolver.Benchmark/Program.cs
+++ SlayTheSpireSolver.Benchmark/Program.cs
@@ -1,5 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
