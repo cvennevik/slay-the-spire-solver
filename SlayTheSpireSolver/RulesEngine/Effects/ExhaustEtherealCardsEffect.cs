052c80df6b00e2456924e5ee95544b46c5949462
cvennevik <cvennevik@gmail.com>
2022-07-30 23:10:33 +0200

    working

---
+++ SlayTheSpireSolver/RulesEngine/Effects/ExhaustEtherealCardsEffect.cs
@@ -0,0 +1,9 @@
+namespace SlayTheSpireSolver.RulesEngine.Effects;
+
+public record ExhaustEtherealCardsEffect : Effect
+{
+    public override PossibilitySet Resolve(GameState gameState)
+    {
+        throw new NotImplementedException();
+    }
+}
