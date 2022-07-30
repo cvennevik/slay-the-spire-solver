e269df314f3043690cad8fea9c0ee762d0590fe9
cvennevik <cvennevik@gmail.com>
2022-07-30 22:50:28 +0200

    working

---
+++ SlayTheSpireSolver/RulesEngine/Cards/AscendersBane.cs
@@ -0,0 +1,22 @@
+using SlayTheSpireSolver.RulesEngine.Actions;
+using SlayTheSpireSolver.RulesEngine.Values;
+
+namespace SlayTheSpireSolver.RulesEngine.Cards;
+
+public record AscendersBane : Card
+{
+    public override Energy GetCost()
+    {
+        return 0;
+    }
+
+    public override IReadOnlyCollection<PlayCardAction> GetLegalActions(GameState gameState)
+    {
+        return Array.Empty<PlayCardAction>();
+    }
+
+    protected override string GetName()
+    {
+        return "Ascender's Bane";
+    }
+}
