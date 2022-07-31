80f7b733e25cf620416e277dff7c312be8b5be7b
cvennevik <cvennevik@gmail.com>
2022-07-31 09:57:49 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Relics/BurningBlood.cs
+++ SlayTheSpireSolver/RulesEngine/Relics/BurningBlood.cs
@@ -1,11 +1,6 @@
 namespace SlayTheSpireSolver.RulesEngine.Relics;
 
-public struct BurningBlood : Relic
+public record BurningBlood : Relic
 {
-    public string Name => "Burning Blood";
-
-    public int CompareTo(Relic? other)
-    {
-        throw new NotImplementedException();
-    }
+    public override string Name => "Burning Blood";
 }
--- SlayTheSpireSolver/RulesEngine/Relics/Relic.cs
+++ SlayTheSpireSolver/RulesEngine/Relics/Relic.cs
@@ -1,6 +1,13 @@
 namespace SlayTheSpireSolver.RulesEngine.Relics;
 
-public interface Relic : IComparable<Relic>
+public abstract record Relic : IComparable<Relic>
 {
-    string Name { get; }
+    public abstract string Name { get; }
+
+    public int CompareTo(Relic? other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        return string.Compare(Name, other.Name, StringComparison.Ordinal);
+    }
 }
