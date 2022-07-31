9be0d8f5f47b5f6733a67697979a218645d336a4
cvennevik <cvennevik@gmail.com>
2022-07-31 13:32:16 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/EndCombatEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/EndCombatEffect.cs
@@ -6,7 +6,7 @@ public record EndCombatEffect : Effect
 {
     public override PossibilitySet Resolve(GameState gameState)
     {
-        throw new NotImplementedException();
+        return gameState;
     }
 }
 
