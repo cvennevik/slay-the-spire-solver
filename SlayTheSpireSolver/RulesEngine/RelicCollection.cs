83ad732042b85c9c2c4e705f070a3c0eb738b94c
cvennevik <cvennevik@gmail.com>
2022-07-31 09:51:45 +0200

    working

--- SlayTheSpireSolver/RulesEngine/RelicCollection.cs
+++ SlayTheSpireSolver/RulesEngine/RelicCollection.cs
@@ -1,5 +1,13 @@
+using SlayTheSpireSolver.RulesEngine.Relics;
+
 namespace SlayTheSpireSolver.RulesEngine;
 
 public class RelicCollection
 {
+    private readonly Relic[] _relics;
+
+    public RelicCollection(params Relic[] relics)
+    {
+        _relics = relics;
+    }
 }
