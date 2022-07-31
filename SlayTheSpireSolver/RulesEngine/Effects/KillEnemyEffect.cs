07b7842b037091537b91c87f1b094a3c3449f451
cvennevik <cvennevik@gmail.com>
2022-07-31 13:47:13 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/KillEnemyEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/KillEnemyEffect.cs
@@ -10,6 +10,7 @@ public record KillEnemyEffect(EnemyId TargetId) : Effect
     {
         if (!gameState.EnemyParty.Has(TargetId)) return gameState;
         var newEnemyParty = gameState.EnemyParty.Remove(TargetId);
+        var newGameState = gameState with { EnemyParty = newEnemyParty };
         if (newEnemyParty.Any()) return gameState with { EnemyParty = newEnemyParty };
         return gameState with { EnemyParty = newEnemyParty };
     }
