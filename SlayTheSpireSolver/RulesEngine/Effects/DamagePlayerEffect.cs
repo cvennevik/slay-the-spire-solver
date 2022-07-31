6d5d62a055dee286ac130f3db2837e3c941f7a83
cvennevik <cvennevik@gmail.com>
2022-07-31 14:09:20 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
@@ -12,8 +12,7 @@ public record DamagePlayerEffect(Damage Damage) : Effect
         var remainingDamage = Damage - gameState.PlayerArmor;
         var remainingHealth = gameState.PlayerHealth - remainingDamage;
         var newGameState = gameState with { PlayerArmor = 0, PlayerHealth = remainingHealth };
-        if (remainingHealth <= 0) return newGameState;
-        return newGameState;
+        return remainingHealth <= 0 ? newGameState : newGameState;
     }
 }
 
