541d7d8d16bc711b2927504521c0da3a33c1a74d
cvennevik <cvennevik@gmail.com>
2022-07-31 13:56:19 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
@@ -24,11 +24,9 @@ internal class DamagePlayerEffectTests
     [TestCase(20, 0, 10, 10, 0)]
     [TestCase(20, 0, 5, 15, 0)]
     [TestCase(20, 0, 0, 20, 0)]
-    [TestCase(5, 0, 10, -5, 0)]
     [TestCase(10, 10, 5, 10, 5)]
     [TestCase(10, 10, 10, 10, 0)]
     [TestCase(10, 10, 11, 9, 0)]
-    [TestCase(10, 10, 25, -5, 0)]
     [TestCase(10, 10, 0, 10, 10)]
     public void DamagesPlayer(int initialHealth, int initialArmor, int damage, int expectedHealth, int expectedArmor)
     {
@@ -40,15 +38,8 @@ internal class DamagePlayerEffectTests
 
 
     [Test]
-    [TestCase(20, 0, 10, 10, 0)]
-    [TestCase(20, 0, 5, 15, 0)]
-    [TestCase(20, 0, 0, 20, 0)]
     [TestCase(5, 0, 10, -5, 0)]
-    [TestCase(10, 10, 5, 10, 5)]
-    [TestCase(10, 10, 10, 10, 0)]
-    [TestCase(10, 10, 11, 9, 0)]
     [TestCase(10, 10, 25, -5, 0)]
-    [TestCase(10, 10, 0, 10, 10)]
     public void KillsPlayer(int initialHealth, int initialArmor, int damage, int expectedHealth, int expectedArmor)
     {
         var damagePlayerEffect = new DamagePlayerEffect(damage);
