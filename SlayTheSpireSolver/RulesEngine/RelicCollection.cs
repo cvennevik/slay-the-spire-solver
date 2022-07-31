a70ae77e179cb58954031f549a20121ea8b74324
cvennevik <cvennevik@gmail.com>
2022-07-31 10:00:48 +0200

    working

--- SlayTheSpireSolver/RulesEngine/RelicCollection.cs
+++ SlayTheSpireSolver/RulesEngine/RelicCollection.cs
@@ -13,6 +13,22 @@ public class RelicCollection
         _relics = relics;
     }
 
+    public static bool operator ==(RelicCollection a, RelicCollection b)
+    {
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(RelicCollection a, RelicCollection b)
+    {
+        return !a.Equals(b);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj)) return true;
+        return obj is RelicCollection otherRelicCollection && _relics.SequenceEqual(otherRelicCollection._relics);
+    }
+
     public override string ToString()
     {
         return $"[{string.Join<Relic>(",", _relics)}]";
