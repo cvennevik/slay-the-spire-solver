5e06750ecff160b637ef695ad3a7ef9fb8446b86
cvennevik <cvennevik@gmail.com>
2022-07-31 13:56:01 +0200

    working

--- SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
+++ SlayTheSpireSolver/RulesEngine/Effects/DamagePlayerEffect.cs
@@ -30,10 +30,29 @@ internal class DamagePlayerEffectTests
     [TestCase(10, 10, 11, 9, 0)]
     [TestCase(10, 10, 25, -5, 0)]
     [TestCase(10, 10, 0, 10, 10)]
-    public void Test(int initialHealth, int initialArmor, int damage, int expectedHealth, int expectedArmor)
+    public void DamagesPlayer(int initialHealth, int initialArmor, int damage, int expectedHealth, int expectedArmor)
     {
         var damagePlayerEffect = new DamagePlayerEffect(damage);
-        var gameState = new GameState { PlayerHealth = initialHealth, PlayerArmor = initialArmor};
+        var gameState = new GameState { PlayerHealth = initialHealth, PlayerArmor = initialArmor };
+        var result = damagePlayerEffect.Resolve(gameState).Single().GameState;
+        Assert.AreEqual(new GameState { PlayerHealth = expectedHealth, PlayerArmor = expectedArmor }, result);
+    }
+
+
+    [Test]
+    [TestCase(20, 0, 10, 10, 0)]
+    [TestCase(20, 0, 5, 15, 0)]
+    [TestCase(20, 0, 0, 20, 0)]
+    [TestCase(5, 0, 10, -5, 0)]
+    [TestCase(10, 10, 5, 10, 5)]
+    [TestCase(10, 10, 10, 10, 0)]
+    [TestCase(10, 10, 11, 9, 0)]
+    [TestCase(10, 10, 25, -5, 0)]
+    [TestCase(10, 10, 0, 10, 10)]
+    public void KillsPlayer(int initialHealth, int initialArmor, int damage, int expectedHealth, int expectedArmor)
+    {
+        var damagePlayerEffect = new DamagePlayerEffect(damage);
+        var gameState = new GameState { PlayerHealth = initialHealth, PlayerArmor = initialArmor };
         var result = damagePlayerEffect.Resolve(gameState).Single().GameState;
         Assert.AreEqual(new GameState { PlayerHealth = expectedHealth, PlayerArmor = expectedArmor }, result);
     }
