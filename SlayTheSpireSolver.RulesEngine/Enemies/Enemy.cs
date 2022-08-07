20c1447d5dfb015cf8d67dd76ccfa93303dc904e
cvennevik <cvennevik@gmail.com>
2022-08-07 09:47:19 +0200

    working

--- SlayTheSpireSolver.RulesEngine/Enemies/Enemy.cs
+++ SlayTheSpireSolver.RulesEngine/Enemies/Enemy.cs
@@ -8,7 +8,7 @@ namespace SlayTheSpireSolver.RulesEngine.Enemies;
 public abstract record Enemy
 {
     public EnemyId Id { get; init; } = EnemyId.Default;
-    public Health Health { get; init; } = 1;
+    public Health Health { get; init; } = new(1);
     public Armor Armor { get; init; } = 0;
     public Strength Strength { get; init; } = 0;
     public Vulnerable Vulnerable { get; init; } = 0;
