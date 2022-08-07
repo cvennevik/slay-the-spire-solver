0a4bc5847d89ca850defe392be18892d9b04f85b
cvennevik <cvennevik@gmail.com>
2022-08-07 10:17:31 +0200

    working

--- SlayTheSpireSolver.RulesEngine/Values/Healing.cs
+++ SlayTheSpireSolver.RulesEngine/Values/Healing.cs
@@ -1,3 +1,12 @@
 namespace SlayTheSpireSolver.RulesEngine.Values;
 
-public readonly record struct Healing();
+public readonly record struct Healing
+{
+    public int Amount { get; }
+
+    public Healing(int amount)
+    {
+        if (amount < 0) throw new ArgumentException("Healing cannot be negative");
+        Amount = amount;
+    }
+};
